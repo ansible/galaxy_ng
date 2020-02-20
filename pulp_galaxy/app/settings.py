ff8d5bcdf5333b395cd05b3e3dcacf7732a7ee43
Alexander Saprykin <cutwatercore@gmail.com>
2020-02-20 14:58:19 +0100

    Cleanup code generated by pulp_template

--- pulp_galaxy/app/settings.py
+++
@@ -1,6 +0,0 @@
-"""
-Check `Plugin Writer's Guide`_ for more details.
-
-.. _Plugin Writer's Guide:
-    http://docs.pulpproject.org/en/3.0/nightly/plugins/plugin-writer/index.html
-"""
--- pulp_galaxy/tests/functional/api/__init__.py
+++
@@ -1,2 +0,0 @@
-# coding=utf-8
-"""Tests that communicate with galaxy plugin via the v3 API."""
