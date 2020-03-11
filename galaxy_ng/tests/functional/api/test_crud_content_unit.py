cutwater/pulp-galaxy

Tests boilerplate
[noissue]

pulp_galaxy/tests/functional/api/test_crud_content_unit.py
@@ -1,0 +1,110 @@
+# coding=utf-8
+"""Tests that perform actions over content unit."""
+import unittest
+
+from pulp_smash import utils
+from pulp_smash.pulp3.utils import delete_orphans
+
+from pulp_galaxy.tests.functional.utils import (
+    core_client,
+    gen_artifact,
+    gen_galaxy_client,
+    gen_galaxy_content_attrs,
+    monitor_task,
+    skip_if,
+)
+from pulp_galaxy.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+from pulpcore.client.pulpcore import ArtifactsApi
+# from pulpcore.client.pulp_galaxy import ContentGalaxyApi
+
+
+# Read the instructions provided below for the steps needed to enable this test (see: FIXME's).
+@unittest.skip("FIXME: plugin writer action required")
+class ContentUnitTestCase(unittest.TestCase):
+    """CRUD content unit.
+
+    This test targets the following issues:
+
+    * `Pulp #2872 <https://pulp.plan.io/issues/2872>`_
+    * `Pulp #3445 <https://pulp.plan.io/issues/3445>`_
+    * `Pulp Smash #870 <https://github.com/pulp/pulp-smash/issues/870>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variable."""
+        delete_orphans()
+        cls.content_unit = {}
+        cls.galaxy_content_api = ContentGalaxyApi(gen_galaxy_client())
+        cls.artifact = gen_artifact()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean class-wide variable."""
+        delete_orphans()
+
+    def test_01_create_content_unit(self):
+        """Create content unit."""
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        response = self.galaxy_content_api.create(**attrs)
+        created_resources = monitor_task(response.task)
+        content_unit = self.galaxy_content_api.read(created_resources[0])
+        self.content_unit.update(content_unit.to_dict())
+        for key, val in attrs.items():
+            with self.subTest(key=key):
+                self.assertEqual(self.content_unit[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_02_read_content_unit(self):
+        """Read a content unit by its href."""
+        content_unit = self.galaxy_content_api.read(self.content_unit["pulp_href"]).to_dict()
+        for key, val in self.content_unit.items():
+            with self.subTest(key=key):
+                self.assertEqual(content_unit[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_02_read_content_units(self):
+        """Read a content unit by its relative_path."""
+        # FIXME: "relative_path" is an attribute specific to the File plugin. It is only an
+        # example. You should replace this with some other field specific to your content type.
+        page = self.galaxy_content_api.list(relative_path=self.content_unit["relative_path"])
+        self.assertEqual(len(page.results), 1)
+        for key, val in self.content_unit.items():
+            with self.subTest(key=key):
+                self.assertEqual(page.results[0].to_dict()[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_03_partially_update(self):
+        """Attempt to update a content unit using HTTP PATCH.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.partial_update(self.content_unit["pulp_href"], attrs)
+        msg = "object has no attribute 'partial_update'"
+        self.assertIn(msg, exc.exception.args[0])
+
+    @skip_if(bool, "content_unit", False)
+    def test_03_fully_update(self):
+        """Attempt to update a content unit using HTTP PUT.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.update(self.content_unit["pulp_href"], attrs)
+        msg = "object has no attribute 'update'"
+        self.assertIn(msg, exc.exception.args[0])
+
+    @skip_if(bool, "content_unit", False)
+    def test_04_delete(self):
+        """Attempt to delete a content unit using HTTP DELETE.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.delete(self.content_unit["pulp_href"])
+        msg = "object has no attribute 'delete'"
+        self.assertIn(msg, exc.exception.args[0])

pulp_galaxy/tests/functional/api/test_download_content.py
@@ -1,0 +1,111 @@
+# coding=utf-8
+"""Tests that verify download of content served by Pulp."""
+import hashlib
+import unittest
+from random import choice
+from urllib.parse import urljoin
+
+from pulp_smash import config, utils
+from pulp_smash.pulp3.utils import download_content_unit, gen_distribution, gen_repo
+
+from pulp_galaxy.tests.functional.constants import GALAXY_FIXTURE_URL
+from pulp_galaxy.tests.functional.utils import (
+    gen_galaxy_client,
+    get_galaxy_content_paths,
+    gen_galaxy_remote,
+    monitor_task,
+)
+from pulp_galaxy.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+# from pulpcore.client.pulp_galaxy import (
+#     DistributionsGalaxyApi,
+#     PublicationsGalaxyApi,
+#     RepositoriesGalaxyApi,
+#     RepositorySyncURL,
+#     RemotesGalaxyApi,
+#     GalaxyGalaxyPublication,
+# )
+
+
+# Implement tests.functional.utils.get_galaxy_content_unit_paths(),
+# as well as sync and publish support before enabling this test.
+@unittest.skip("FIXME: plugin writer action required")
+class DownloadContentTestCase(unittest.TestCase):
+    """Verify whether content served by pulp can be downloaded."""
+
+    def test_all(self):
+        """Verify whether content served by pulp can be downloaded.
+
+        The process of publishing content is more involved in Pulp 3 than it
+        was under Pulp 2. Given a repository, the process is as follows:
+
+        1. Create a publication from the repository. (The latest repository
+           version is selected if no version is specified.) A publication is a
+           repository version plus metadata.
+        2. Create a distribution from the publication. The distribution defines
+           at which URLs a publication is available, e.g.
+           ``http://example.com/content/foo/`` and
+           ``http://example.com/content/bar/``.
+
+        Do the following:
+
+        1. Create, populate, publish, and distribute a repository.
+        2. Select a random content unit in the distribution. Download that
+           content unit from Pulp, and verify that the content unit has the
+           same checksum when fetched directly from Pulp-Fixtures.
+
+        This test targets the following issues:
+
+        * `Pulp #2895 <https://pulp.plan.io/issues/2895>`_
+        * `Pulp Smash #872 <https://github.com/pulp/pulp-smash/issues/872>`_
+        """
+        cfg = config.get_config()
+        client = gen_galaxy_client()
+        repo_api = RepositoriesGalaxyApi(client)
+        remote_api = RemotesGalaxyApi(client)
+        publications = PublicationsGalaxyApi(client)
+        distributions = DistributionsGalaxyApi(client)
+
+        repo = repo_api.create(gen_repo())
+        self.addCleanup(repo_api.delete, repo.pulp_href)
+
+        body = gen_galaxy_remote()
+        remote = remote_api.create(body)
+        self.addCleanup(remote_api.delete, remote.pulp_href)
+
+        # Sync a Repository
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        # Create a publication.
+        publish_data = GalaxyGalaxyPublication(repository=repo.pulp_href)
+        publish_response = publications.create(publish_data)
+        created_resources = monitor_task(publish_response.task)
+        publication_href = created_resources[0]
+        self.addCleanup(publications.delete, publication_href)
+
+        # Create a distribution.
+        body = gen_distribution()
+        body["publication"] = publication_href
+        distribution_response = distributions.create(body)
+        created_resources = monitor_task(distribution_response.task)
+        distribution = distributions.read(created_resources[0])
+        self.addCleanup(distributions.delete, distribution.pulp_href)
+
+        # Pick a content unit (of each type), and download it from both Pulp Fixtures…
+        unit_paths = [choice(paths) for paths in get_galaxy_content_paths(repo.to_dict()).values()]
+        fixtures_hashes = [
+            hashlib.sha256(utils.http_get(urljoin(GALAXY_FIXTURE_URL, unit_path[0]))).hexdigest()
+            for unit_path in unit_paths
+        ]
+
+        # …and Pulp.
+        pulp_hashes = []
+        cfg = config.get_config()
+        for unit_path in unit_paths:
+            content = download_content_unit(cfg, distribution.to_dict(), unit_path[1])
+            pulp_hashes.append(hashlib.sha256(content).hexdigest())
+
+        self.assertEqual(fixtures_hashes, pulp_hashes)

pulp_galaxy/tests/functional/api/test_download_policies.py
@@ -1,0 +1,99 @@
+# coding=utf-8
+"""Tests for Pulp`s download policies."""
+import unittest
+
+from pulp_smash.pulp3.utils import gen_repo, get_added_content_summary, get_content_summary
+
+from pulp_galaxy.tests.functional.constants import (
+    GALAXY_FIXTURE_SUMMARY,
+    DOWNLOAD_POLICIES,
+)
+from pulp_galaxy.tests.functional.utils import (
+    gen_galaxy_client,
+    gen_galaxy_remote,
+    monitor_task,
+    skip_if,
+)
+from pulp_galaxy.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+# from pulpcore.client.pulp_galaxy import (
+#     RepositoriesGalaxyApi,
+#     RepositorySyncURL,
+#     RemotesGalaxyApi,
+# )
+
+
+# Implement sync support before enabling this test.
+@unittest.skip("FIXME: plugin writer action required")
+class SyncDownloadPolicyTestCase(unittest.TestCase):
+    """Sync a repository with different download policies.
+
+    This test targets the following issue:
+
+    `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.client = gen_galaxy_client()
+        cls.DP_ON_DEMAND = "on_demand" in DOWNLOAD_POLICIES
+        cls.DP_STREAMED = "streamed" in DOWNLOAD_POLICIES
+
+    @skip_if(bool, "DP_ON_DEMAND", False)
+    def test_on_demand(self):
+        """Sync with ``on_demand`` download policy. See :meth:`do_test`."""
+        self.do_test("on_demand")
+
+    @skip_if(bool, "DP_STREAMED", False)
+    def test_streamed(self):
+        """Sync with ``streamend`` download policy.  See :meth:`do_test`."""
+        self.do_test("streamed")
+
+    def do_test(self, download_policy):
+        """Sync repositories with the different ``download_policy``.
+
+        Do the following:
+
+        1. Create a repository, and a remote.
+        2. Assert that repository version is None.
+        3. Sync the remote.
+        4. Assert that repository version is not None.
+        5. Assert that the correct number of possible units to be downloaded
+           were shown.
+        6. Sync the remote one more time in order to create another repository
+           version.
+        7. Assert that repository version is different from the previous one.
+        8. Assert that the same number of units are shown, and after the
+           second sync no extra units should be shown, since the same remote
+           was synced again.
+        """
+        repo_api = RepositoriesGalaxyApi(self.client)
+        remote_api = RemotesGalaxyApi(self.client)
+
+        repo = repo_api.create(gen_repo())
+        self.addCleanup(repo_api.delete, repo.pulp_href)
+
+        body = gen_galaxy_remote(**{"policy": download_policy})
+        remote = remote_api.create(body)
+        self.addCleanup(remote_api.delete, remote.pulp_href)
+
+        # Sync the repository.
+        self.assertEqual(repo.latest_version_href, f"{repo.pulp_href}versions/0/")
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        self.assertIsNotNone(repo.latest_version_href)
+        self.assertDictEqual(get_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)
+        self.assertDictEqual(get_added_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)
+
+        # Sync the repository again.
+        latest_version_href = repo.latest_version_href
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        self.assertEqual(latest_version_href, repo.latest_version_href)
+        self.assertDictEqual(get_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)

ansible/galaxy_ng

galaxy_ng/tests/functional/api/test_crud_content_unit.py
@@ -1,0 +1,109 @@
+"""Tests that perform actions over content unit."""
+import unittest
+
+from pulp_smash import utils
+from pulp_smash.pulp3.utils import delete_orphans
+
+from galaxy_ng.tests.functional.utils import (
+    core_client,
+    gen_artifact,
+    gen_galaxy_client,
+    gen_galaxy_content_attrs,
+    monitor_task,
+    skip_if,
+)
+from galaxy_ng.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+from pulpcore.client.pulpcore import ArtifactsApi
+from pulpcore.client.galaxy_ng import ApiGalaxyCollectionsApi
+
+
+# Read the instructions provided below for the steps needed to enable this test (see: FIXME's).
+@unittest.skip("FIXME: plugin writer action required")
+class ContentUnitTestCase(unittest.TestCase):
+    """CRUD content unit.
+
+    This test targets the following issues:
+
+    * `Pulp #2872 <https://pulp.plan.io/issues/2872>`_
+    * `Pulp #3445 <https://pulp.plan.io/issues/3445>`_
+    * `Pulp Smash #870 <https://github.com/pulp/pulp-smash/issues/870>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variable."""
+        delete_orphans()
+        cls.content_unit = {}
+        cls.galaxy_content_api = ApiGalaxyCollectionsApi(gen_galaxy_client())
+        cls.artifact = gen_artifact()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean class-wide variable."""
+        delete_orphans()
+
+    def test_01_create_content_unit(self):
+        """Create content unit."""
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        response = self.galaxy_content_api.create(**attrs)
+        created_resources = monitor_task(response.task)
+        content_unit = self.galaxy_content_api.read(created_resources[0])
+        self.content_unit.update(content_unit.to_dict())
+        for key, val in attrs.items():
+            with self.subTest(key=key):
+                self.assertEqual(self.content_unit[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_02_read_content_unit(self):
+        """Read a content unit by its href."""
+        content_unit = self.galaxy_content_api.read(self.content_unit["pulp_href"]).to_dict()
+        for key, val in self.content_unit.items():
+            with self.subTest(key=key):
+                self.assertEqual(content_unit[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_02_read_content_units(self):
+        """Read a content unit by its relative_path."""
+        # FIXME: "relative_path" is an attribute specific to the File plugin. It is only an
+        # example. You should replace this with some other field specific to your content type.
+        page = self.galaxy_content_api.list(relative_path=self.content_unit["relative_path"])
+        self.assertEqual(len(page.results), 1)
+        for key, val in self.content_unit.items():
+            with self.subTest(key=key):
+                self.assertEqual(page.results[0].to_dict()[key], val)
+
+    @skip_if(bool, "content_unit", False)
+    def test_03_partially_update(self):
+        """Attempt to update a content unit using HTTP PATCH.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.partial_update(self.content_unit["pulp_href"], attrs)
+        msg = "object has no attribute 'partial_update'"
+        self.assertIn(msg, exc.exception.args[0])
+
+    @skip_if(bool, "content_unit", False)
+    def test_03_fully_update(self):
+        """Attempt to update a content unit using HTTP PUT.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        attrs = gen_galaxy_content_attrs(self.artifact)
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.update(self.content_unit["pulp_href"], attrs)
+        msg = "object has no attribute 'update'"
+        self.assertIn(msg, exc.exception.args[0])
+
+    @skip_if(bool, "content_unit", False)
+    def test_04_delete(self):
+        """Attempt to delete a content unit using HTTP DELETE.
+
+        This HTTP method is not supported and a HTTP exception is expected.
+        """
+        with self.assertRaises(AttributeError) as exc:
+            self.galaxy_content_api.delete(self.content_unit["pulp_href"])
+        msg = "object has no attribute 'delete'"
+        self.assertIn(msg, exc.exception.args[0])

galaxy_ng/tests/functional/api/test_download_content.py
@@ -1,0 +1,101 @@
+"""Tests that verify download of content served by Pulp."""
+import hashlib
+import unittest
+from random import choice
+from urllib.parse import urljoin
+
+from pulp_smash import config, utils
+from pulp_smash.pulp3.utils import download_content_unit, gen_distribution, gen_repo
+
+from galaxy_ng.tests.functional.constants import GALAXY_FIXTURE_URL
+from galaxy_ng.tests.functional.utils import (
+    gen_galaxy_client,
+    get_galaxy_content_paths,
+    gen_galaxy_remote,
+    monitor_task,
+)
+from galaxy_ng.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+
+# Implement tests.functional.utils.get_galaxy_content_unit_paths(),
+# as well as sync and publish support before enabling this test.
+@unittest.skip("FIXME: plugin writer action required")
+class DownloadContentTestCase(unittest.TestCase):
+    """Verify whether content served by pulp can be downloaded."""
+
+    def test_all(self):
+        """Verify whether content served by pulp can be downloaded.
+
+        The process of publishing content is more involved in Pulp 3 than it
+        was under Pulp 2. Given a repository, the process is as follows:
+
+        1. Create a publication from the repository. (The latest repository
+           version is selected if no version is specified.) A publication is a
+           repository version plus metadata.
+        2. Create a distribution from the publication. The distribution defines
+           at which URLs a publication is available, e.g.
+           ``http://example.com/content/foo/`` and
+           ``http://example.com/content/bar/``.
+
+        Do the following:
+
+        1. Create, populate, publish, and distribute a repository.
+        2. Select a random content unit in the distribution. Download that
+           content unit from Pulp, and verify that the content unit has the
+           same checksum when fetched directly from Pulp-Fixtures.
+
+        This test targets the following issues:
+
+        * `Pulp #2895 <https://pulp.plan.io/issues/2895>`_
+        * `Pulp Smash #872 <https://github.com/pulp/pulp-smash/issues/872>`_
+        """
+        cfg = config.get_config()
+        client = gen_galaxy_client()
+        repo_api = RepositoriesGalaxyApi(client)
+        remote_api = RemotesGalaxyApi(client)
+        publications = PublicationsGalaxyApi(client)
+        distributions = DistributionsGalaxyApi(client)
+
+        repo = repo_api.create(gen_repo())
+        self.addCleanup(repo_api.delete, repo.pulp_href)
+
+        body = gen_galaxy_remote()
+        remote = remote_api.create(body)
+        self.addCleanup(remote_api.delete, remote.pulp_href)
+
+        # Sync a Repository
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        # Create a publication.
+        publish_data = GalaxyGalaxyPublication(repository=repo.pulp_href)
+        publish_response = publications.create(publish_data)
+        created_resources = monitor_task(publish_response.task)
+        publication_href = created_resources[0]
+        self.addCleanup(publications.delete, publication_href)
+
+        # Create a distribution.
+        body = gen_distribution()
+        body["publication"] = publication_href
+        distribution_response = distributions.create(body)
+        created_resources = monitor_task(distribution_response.task)
+        distribution = distributions.read(created_resources[0])
+        self.addCleanup(distributions.delete, distribution.pulp_href)
+
+        # Pick a content unit (of each type), and download it from both Pulp Fixtures…
+        unit_paths = [choice(paths) for paths in get_galaxy_content_paths(repo.to_dict()).values()]
+        fixtures_hashes = [
+            hashlib.sha256(utils.http_get(urljoin(GALAXY_FIXTURE_URL, unit_path[0]))).hexdigest()
+            for unit_path in unit_paths
+        ]
+
+        # …and Pulp.
+        pulp_hashes = []
+        cfg = config.get_config()
+        for unit_path in unit_paths:
+            content = download_content_unit(cfg, distribution.to_dict(), unit_path[1])
+            pulp_hashes.append(hashlib.sha256(content).hexdigest())
+
+        self.assertEqual(fixtures_hashes, pulp_hashes)

galaxy_ng/tests/functional/api/test_download_policies.py
@@ -1,0 +1,92 @@
+"""Tests for Pulp`s download policies."""
+import unittest
+
+from pulp_smash.pulp3.utils import gen_repo, get_added_content_summary, get_content_summary
+
+from galaxy_ng.tests.functional.constants import (
+    GALAXY_FIXTURE_SUMMARY,
+    DOWNLOAD_POLICIES,
+)
+from galaxy_ng.tests.functional.utils import (
+    gen_galaxy_client,
+    gen_galaxy_remote,
+    monitor_task,
+    skip_if,
+)
+from galaxy_ng.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+
+# Implement sync support before enabling this test.
+@unittest.skip("FIXME: plugin writer action required")
+class SyncDownloadPolicyTestCase(unittest.TestCase):
+    """Sync a repository with different download policies.
+
+    This test targets the following issue:
+
+    `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.client = gen_galaxy_client()
+        cls.DP_ON_DEMAND = "on_demand" in DOWNLOAD_POLICIES
+        cls.DP_STREAMED = "streamed" in DOWNLOAD_POLICIES
+
+    @skip_if(bool, "DP_ON_DEMAND", False)
+    def test_on_demand(self):
+        """Sync with ``on_demand`` download policy. See :meth:`do_test`."""
+        self.do_test("on_demand")
+
+    @skip_if(bool, "DP_STREAMED", False)
+    def test_streamed(self):
+        """Sync with ``streamend`` download policy.  See :meth:`do_test`."""
+        self.do_test("streamed")
+
+    def do_test(self, download_policy):
+        """Sync repositories with the different ``download_policy``.
+
+        Do the following:
+
+        1. Create a repository, and a remote.
+        2. Assert that repository version is None.
+        3. Sync the remote.
+        4. Assert that repository version is not None.
+        5. Assert that the correct number of possible units to be downloaded
+           were shown.
+        6. Sync the remote one more time in order to create another repository
+           version.
+        7. Assert that repository version is different from the previous one.
+        8. Assert that the same number of units are shown, and after the
+           second sync no extra units should be shown, since the same remote
+           was synced again.
+        """
+        repo_api = RepositoriesGalaxyApi(self.client)
+        remote_api = RemotesGalaxyApi(self.client)
+
+        repo = repo_api.create(gen_repo())
+        self.addCleanup(repo_api.delete, repo.pulp_href)
+
+        body = gen_galaxy_remote(**{"policy": download_policy})
+        remote = remote_api.create(body)
+        self.addCleanup(remote_api.delete, remote.pulp_href)
+
+        # Sync the repository.
+        self.assertEqual(repo.latest_version_href, f"{repo.pulp_href}versions/0/")
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        self.assertIsNotNone(repo.latest_version_href)
+        self.assertDictEqual(get_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)
+        self.assertDictEqual(get_added_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)
+
+        # Sync the repository again.
+        latest_version_href = repo.latest_version_href
+        sync_response = repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = repo_api.read(repo.pulp_href)
+
+        self.assertEqual(latest_version_href, repo.latest_version_href)
+        self.assertDictEqual(get_content_summary(repo.to_dict()), GALAXY_FIXTURE_SUMMARY)