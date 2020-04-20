5fc936b32fb5747bbce6eb2ccaad9d3b12597042
Adrian Likins <alikins@redhat.com>
2020-04-20 11:56:55 -0400

    Add URLs for the pulp based per-repo endpoints
    Add a url path that has a <path:path> path
    that points to the default 'golden' distro.
    
    This allows:
        api/automation-hub/v3/collections/my_ns/my_collection/
    
    To work as well as:
        api/automation-hub/ah-content/some_other_repo_distro_base_path/v3/collections/my_ny/my_collection/
    
    Change url resolv to ah-content/<str:path>/ was <path:path>.
    
    Since we only need to match to the str 'distro base_path'
    in the url, update the PEEL_API_ROOT to use <str:path>
    instead of <path:path> since the latter will match the
    remaining path params (ie, it matches
    for /api/automation-hub/v3/ah-content/golden-repo/collections/
    <path:path> would match 'golden-repo/collections/' and
    never match api/automation-hub/v3/ah-content/<path:path>/collections/
    
    NOTE: Need to revisit this using <path:path> to support 'path' being
    nested dirs.
    
    Add distro base_path to pulp viewset api root.
    
    'ansible-galaxy collection install' and
    'ansible-galaxy collection publish' work for the relocated
    urls now

--- galaxy_ng/app/api/urls.py
+++ galaxy_ng/app/api/urls.py
@@ -3,13 +3,37 @@ from django.urls import include, path
 from . import views
 from .ui import urls as ui_urls
 from .v3 import urls as v3_urls
-
+from .v3 import viewsets as v3_viewsets
 
 app_name = "api"
 urlpatterns = [
-    path("", views.ApiRootView.as_view(), name="root"),
-    path("v3/", include(v3_urls)),
     path("v3/_ui/", include(ui_urls)),
+
+    # A set of galaxy v3 API urls per Distribution.base_path
+    # which is essentially per Repository
+    path("content/<str:path>/v3/",
+         include(v3_urls, namespace='per_distribution')),
+
+    path("content/<str:path>/",
+         v3_viewsets.ApiRootView.as_view(),
+         name="root-per-distro"),
+
+    # This can be removed when ansible-galaxy stops appending '/api' to the
+    # urls.
+    path("content/<str:path>/api/",
+         v3_viewsets.SlashApiRedirectPerDistroView.as_view(),
+         name="api-redirect"),
+
+
+    # Set an instance of the v3 urls/viewsets at the same
+    # url path as the former galaxy_api.api.v3.viewsets
+    # ie (/api/automation-hub/v3/collections etc
+    # and pass in path param 'path' that actually means
+    # the base_path of the Distribution. For the default case,
+    # use the hard coded 'default' distro (ie, 'automation-hub')
+    path("v3/",
+         include(v3_urls, namespace='default_distribution'),
+         {'path': 'golden-distro-base-path'}),
 
     # This path is to redirect requests to /api/automation-hub/api/
     # to /api/automation-hub/
@@ -19,7 +43,13 @@ urlpatterns = [
     # galaxy server urls to try to find the API root. So add a redirect from
     # "/api" to actual API root at "/".
 
+    path("",
+         views.ApiRootView.as_view(),
+         name="root-api"),
+
     # This can be removed when ansible-galaxy stops appending '/api' to the
     # urls.
-    path("api/", views.SlashApiRedirectView.as_view()),
+    path("api/",
+         views.SlashApiRedirectView.as_view(),
+         name="api-redirect"),
 ]
