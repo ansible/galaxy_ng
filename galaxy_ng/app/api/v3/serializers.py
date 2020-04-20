e940820d545bd49970ece1d1c13d11c835070d3a
Adrian Likins <alikins@redhat.com>
2020-04-20 11:52:22 -0400

    Add CollectionVersion serial that takes repo/dist in download_url
    Use a GalaxyNGCollectionVersionSerializer to be used with
    the CollectionVersionsViewset.
    
    This serializer includes the Collections repo/dist base_path
    in the download_url path.
    
    The issue here is that the pulp_ansible ConllectionVersionSerializer
    doesn't include the distro base_path in the url, presumably
    because it was always empty before?
    
    Current pulp-content-app provides collection artifacts at
    urls like:
    
        http://localhost:24816/pulp/content/foo-distro-base-path/alikins.collection_inspect.0.0.182.tar.gz
    
    The 'foo-distro-base-path' is the base path for the
    'foo-distro' which is the distro associated with the
    'foo' Repository.
    
    Current pulp_ansible leaves out the 'foo-distro-base-path' even
    if the pulp config settings for CONTENT_ORIGIN and CONTENT_PATH_PREFIX
    are tweaked.
    
    So galaxy_ng.app.api.v3.serializers.GalaxyNGCollectionVersionSerializer
    is a workaround. Should probably get fixed in pulp_ansible.
    
    TODO: This serializer could be removed if pulp_ansible
    handles it in the future.
    See https://pulp.plan.io/issues/6510

--- galaxy_ng/app/api/v3/serializers.py
+++ galaxy_ng/app/api/v3/serializers.py
@@ -1,48 +1,23 @@
 import logging
-import mimetypes
 
-from rest_framework import serializers
-from rest_framework.exceptions import ValidationError, _get_error_details
+from django.conf import settings
 
-from galaxy_ng.app.api.ui.serializers.base import Serializer
-from galaxy_ng.app.api.utils import parse_collection_filename
+from pulp_ansible.app.galaxy.v3.serializers import CollectionVersionSerializer
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
-class CollectionSerializer(Serializer):
-    name = serializers.CharField(required=True)
-    namespace = serializers.CharField(required=True)
-    deprecated = serializers.BooleanField(required=False)
+class GalaxyNGCollectionVersionSerializer(CollectionVersionSerializer):
+    def get_download_url(self, obj):
+        """
+        Get artifact download URL.
+        """
 
+        host = settings.CONTENT_ORIGIN.strip("/")
+        prefix = settings.CONTENT_PATH_PREFIX.strip("/")
+        distro_base_path = self.context["path"]
+        filename_path = self.context["content_artifact"].relative_path.lstrip("/")
 
-class CollectionUploadSerializer(Serializer):
-    """
-    A serializer for the Collection One Shot Upload API.
-    """
+        download_url = f"{host}/{prefix}/{distro_base_path}/{filename_path}"
 
-    file = serializers.FileField(required=True)
-
-    sha256 = serializers.CharField(required=False, default=None)
-
-    def to_internal_value(self, data):
-        """Parse and validate collection filename."""
-        data = super().to_internal_value(data)
-
-        errors = {}
-
-        filename = data["file"].name
-
-        try:
-            filename_tuple = parse_collection_filename(filename)
-        except ValueError as exc:
-            errors['filename'] = _get_error_details(exc)
-            logger.error('CollectionUploadSerializer validation of filename failed: %s',
-                         errors['filename'])
-            raise ValidationError(errors)
-
-        data.update({
-            "filename": filename_tuple,
-            "mimetype": (mimetypes.guess_type(filename)[0] or 'application/octet-stream')
-        })
-        return data
+        return download_url
