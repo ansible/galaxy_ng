db747e6e8d93b4e97c05b453b183f38cd954de6a
David Newswanger <dnewswan@redhat.com>
2020-04-23 15:19:30 -0400

    Add serializer to groups.

--- galaxy_ng/app/api/ui/serializers/user.py
+++ galaxy_ng/app/api/ui/serializers/user.py
@@ -7,8 +7,18 @@ from rest_framework.exceptions import ValidationError
 from galaxy_ng.app.models import auth as auth_models
 
 
+class GroupSerializer(ModelSerializer):
+
+    class Meta:
+        model = auth_models.Group
+        fields = (
+            'id',
+            'name'
+        )
+
+
 class UserSerializer(ModelSerializer):
-    password_confirm = CharField(write_only=True, allow_blank=True)
+    password_confirm = CharField(write_only=True, allow_blank=True, required=False)
 
     class Meta:
         model = auth_models.User
@@ -23,21 +33,31 @@ class UserSerializer(ModelSerializer):
             'password_confirm'
         )
         extra_kwargs = {
-            'password': {'write_only': True, 'allow_blank': True}
+            'password': {'write_only': True, 'allow_blank': True, 'required': False}
         }
 
     def validate_password(self, password):
-        if (len(password) <= 10):
+        if len(password) <= 10:
             raise ValidationError(detail={
                 'password': "Password must be 10+ characters long"})
-        if (password != self.initial_data['password_confirm']):
+        if password != self.initial_data['password_confirm']:
             raise ValidationError(detail={
                 'password': "Passwords do no match"})
         return password
 
     def update(self, instance, data):
-        if (data['password']):
-            instance.set_password(data['password'])
+        if 'password' in data:
+            if data['password']:
+                instance.set_password(data['password'])
 
-        del data['password']
+            # password doesn't get set the same as other data, so delete it
+            # before the serializer saves
+            del data['password']
+
         return super().update(instance, data)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['groups'] = \
+            GroupSerializer(instance.groups.all(), many=True).data
+        return representation
