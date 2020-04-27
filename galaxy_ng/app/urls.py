5edd0fd93f43ff02fc0f931dfd08de11a481fbf6
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-04-27 11:54:29 -0300

    Introducing UI config
    - galaxy_ng/settings.py to set STATIC_URL = "/static/"
    - Add a new galaxy_ng view that 302 redirects / to /ui/

--- galaxy_ng/app/urls.py
+++ galaxy_ng/app/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.urls import include, path
 
 from galaxy_ng.app.api import urls as api_urls
+from galaxy_ng.ui import urls as ui_urls
 
 API_PATH_PREFIX = settings.GALAXY_API_PATH_PREFIX.strip('/')
 
 app_name = "galaxy"
 urlpatterns = [
+    path("", include(ui_urls)),
     path(f"{API_PATH_PREFIX}/", include(api_urls)),
     path('', include('django_prometheus.urls')),
 ]
