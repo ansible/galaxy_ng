b35501cb6065845c9cae824e5a3613d059f14426
Adrian Likins <alikins@redhat.com>
2020-05-14 10:55:07 -0400

    Use pulp viewsets for 'v3' and add per repo/distro v3 style endpoints (#84)
    This replaces most of galaxy_ng/app/api/v3/ with viewsets based on the pulp_ansible v3 viewsets eliminating the use of galaxy_pulp for those views.
    
    This also adds a new set of api endpoints that are exposed on urls based on each available
    pulp Distribution's base_path. ie, there is an api endpoint for each pulp Repository
    
    The new 'per Distribution' endpoints:
    
    /api/automation-hub/content/<str:path>/v3/collections/
    /api/automation-hub/content/<str:path>/v3/collections/<str:namespace>/<str:name>/
    /api/automation-hub/content/<str:path>/v3/collections/<str:namespace>/<str:name>/versions/	
    /api/automation-hub/content/<str:path>/v3/collections/
            <str:namespace>/<str:name>/versions/<str:version>/	
    /api/automation-hub/content/<str:path>/v3/imports/collections/<str:pk>/	
    /api/automation-hub/content/<str:path>/v3/artifacts/collections/	
    /api/automation-hub/content/<str:path>/v3/	
    /api/automation-hub/content/<str:path>/
    /api/automation-hub/content/<str:path>/api/
    
    The 'default distribution' endpoints are equivalent to the old 'v3/' endpoints 
    (excluding '_ui' for now). They both use the same viewset implementation,
    but the 'default distribution' sets a default distribution base_path.

--- galaxy_ng/app/api/urls.py
+++ galaxy_ng/app/api/urls.py
@@ -1,15 +1,63 @@
+from django.conf import settings
 from django.urls import include, path
 
 from . import views
 from .ui import urls as ui_urls
 from .v3 import urls as v3_urls
+from .v3 import viewsets as v3_viewsets
 
+DEFAULT_DISTRIBUTION_BASE_PATH = settings.GALAXY_API_DEFAULT_DISTRIBUTION_BASE_PATH.strip('/')
 
 app_name = "api"
+
+v3_urlpatterns = [
+    path("_ui/", include(ui_urls)),
+
+    # Set an instance of the v3 urls/viewsets at the same
+    # url path as the former galaxy_api.api.v3.viewsets
+    # ie (/api/automation-hub/v3/collections etc
+    # and pass in path param 'path' that actually means
+    # the base_path of the Distribution. For the default case,
+    # use the hard coded 'default' distro (ie, 'automation-hub')
+    path("",
+         # include((v3_urls, app_name), namespace='default-content'),
+         include((v3_urls, app_name), namespace='default-content'),
+         {'path': DEFAULT_DISTRIBUTION_BASE_PATH}),
+]
+
+content_v3_urlpatterns = [
+    # A set of galaxy v3 API urls per Distribution.base_path
+    # which is essentially per Repository
+    path("<str:path>/v3/",
+         # include((v3_urls, app_name))),
+         include(v3_urls)),
+]
+
+content_urlpatterns = [
+    path("content/",
+         include((content_v3_urlpatterns, app_name), namespace="v3")),
+
+    path("content/<str:path>/",
+         views.ApiRootView.as_view(),
+         name="root"),
+
+    # This can be removed when ansible-galaxy stops appending '/api' to the
+    # urls.
+    path("content/<str:path>/api/",
+         views.ApiRedirectView.as_view(),
+         name="api-redirect",
+         kwargs={"reverse_url_name": "galaxy:api:content:root"}),
+]
+
 urlpatterns = [
-    path("", views.ApiRootView.as_view(), name="root"),
-    path("v3/", include(v3_urls)),
-    path("v3/_ui/", include(ui_urls)),
+    path("v3/", include((v3_urlpatterns, app_name), namespace="v3")),
+
+    path("", include((content_urlpatterns, app_name), namespace='content')),
+
+    path("",
+         views.ApiRootView.as_view(),
+         name="root",
+         ),
 
     # This path is to redirect requests to /api/automation-hub/api/
     # to /api/automation-hub/
@@ -21,5 +69,8 @@ urlpatterns = [
 
     # This can be removed when ansible-galaxy stops appending '/api' to the
     # urls.
-    path("api/", views.SlashApiRedirectView.as_view()),
+    path("api/",
+         views.ApiRedirectView.as_view(),
+         name="api-redirect",
+         kwargs={"reverse_url_name": "galaxy:api:root"}),
 ]
