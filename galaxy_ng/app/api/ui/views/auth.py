066c9eabf70164c909caa5437b82ef974a1e10fd
Alexander Saprykin <osapryki@redhat.com>
2020-05-16 18:49:10 +0200

    Enforce CSRF protection for login view

--- galaxy_ng/app/api/ui/views/auth.py
+++ galaxy_ng/app/api/ui/views/auth.py
@@ -1,6 +1,9 @@
 from django.contrib import auth as django_auth
 from django.db import transaction
 from django.http import Http404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework.authentication import SessionAuthentication
 
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -19,9 +22,23 @@ __all__ = (
 )
 
 
+class _CSRFSessionAuthentication(SessionAuthentication):
+    """CSRF-enforcing version of a SessionAuthentication class."""
+
+    def authenticate(self, request):
+        user = getattr(request._request, "user", None)
+        self.enforce_csrf(request)
+        return user, None
+
+
 class LoginView(api_base.GenericAPIView):
     serializer_class = LoginSerializer
+    authentication_classes = (_CSRFSessionAuthentication,)
     permission_classes = (RestrictOnCloudDeployments,)
+
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request, *args, **kwargs):
+        return Response(status=http_code.HTTP_204_NO_CONTENT)
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=self.request.data)
--- galaxy_ng/tests/unit/api/test_api_ui_auth_views.py
+++ galaxy_ng/tests/unit/api/test_api_ui_auth_views.py
@@ -29,25 +29,34 @@ class TestLoginViewsStandalone(APITestCase):
             auth_models.User.objects.create_user(username="test2", password="test2-secret"),
         ]
 
-    def _test_login_user(self, username, password):
-        response: Response = self.client.post(
-            self.login_url, data={"username": username, "password": password}
-        )
-        self.assertEqual(response.status_code, http_code.HTTP_204_NO_CONTENT)
-        self.assertTrue("csrftoken" in response.cookies)
-        self.assertTrue("sessionid" in response.cookies)
-
-        response: Response = self.client.get(self.me_url)
-        self.assertEqual(response.status_code, http_code.HTTP_200_OK)
-        self.assertEqual(response.data["username"], username)
-
     def test_login(self):
-        self._test_login_user("test1", "test1-secret")
-        self._test_login_user("test2", "test2-secret")
+        self._test_login("test1", "test1-secret")
+        self._test_login("test2", "test2-secret")
+
+    def test_login_csrf_cookie_set_header_missing(self):
+        client = APIClient(enforce_csrf_checks=True)
+
+        # Ensure CSRF cookie is set
+        response = client.get(self.login_url)
+        self.assertTrue("csrftoken" in response.cookies)
+
+        self._test_login_permission_denied(
+            data={"username": "test1", "password": "test1-secret"},
+            expected_detail="CSRF Failed: CSRF token missing or incorrect.",
+            client=client
+        )
+
+    def test_login_csrf_cookie_missing(self):
+        client = APIClient(enforce_csrf_checks=True)
+        self._test_login_permission_denied(
+            data={"username": "test1", "password": "test1-secret"},
+            expected_detail="CSRF Failed: CSRF cookie not set.",
+            client=client
+        )
 
     def test_login_invalid_password(self):
         response: Response = self.client.post(
-            self.login_url, data={"username": "test1", "password": "invalid",}
+            self.login_url, data={"username": "test1", "password": "invalid", }
         )
         self.assertEqual(response.status_code, http_code.HTTP_403_FORBIDDEN)
 
@@ -56,28 +65,9 @@ class TestLoginViewsStandalone(APITestCase):
 
     def test_login_wrong_password(self):
         response: Response = self.client.post(
-            self.login_url, data={"username": "test2", "password": "test1-secret",}
+            self.login_url, data={"username": "test2", "password": "test1-secret", }
         )
         self.assertEqual(response.status_code, http_code.HTTP_403_FORBIDDEN)
-
-    def _test_login_validation_error(
-        self, data, expected_code, expected_detail, expected_parameter
-    ):
-        response: Response = self.client.post(self.login_url, data=data)
-
-        self.assertEqual(response.status_code, http_code.HTTP_400_BAD_REQUEST)
-        expected_response_data = {
-            "errors": [
-                {
-                    "status": "400",
-                    "code": expected_code,
-                    "title": "Invalid input.",
-                    "detail": expected_detail,
-                    "source": {"parameter": expected_parameter},
-                }
-            ]
-        }
-        self.assertDictEqual(response.data, expected_response_data)
 
     def test_login_no_username(self):
         data = {"password": "test1-secret"}
@@ -117,7 +107,7 @@ class TestLoginViewsStandalone(APITestCase):
 
     def test_logout(self):
         response: Response = self.client.post(
-            self.login_url, data={"username": "test1", "password": "test1-secret",}
+            self.login_url, data={"username": "test1", "password": "test1-secret", }
         )
         self.assertEqual(response.status_code, http_code.HTTP_204_NO_CONTENT)
 
@@ -126,6 +116,53 @@ class TestLoginViewsStandalone(APITestCase):
 
         response: Response = self.client.get(self.me_url)
         self.assertEqual(response.status_code, http_code.HTTP_403_FORBIDDEN)
+
+    def _test_login(self, username, password, client=None):
+        client = client or self.client
+        response: Response = client.post(
+            self.login_url, data={"username": username, "password": password}
+        )
+        self.assertEqual(response.status_code, http_code.HTTP_204_NO_CONTENT)
+        self.assertTrue("csrftoken" in response.cookies)
+        self.assertTrue("sessionid" in response.cookies)
+
+        response: Response = client.get(self.me_url)
+        self.assertEqual(response.status_code, http_code.HTTP_200_OK)
+        self.assertEqual(response.data["username"], username)
+
+    def _test_login_permission_denied(self, data, expected_detail, client=None):
+        client = client or self.client
+        response: Response = client.post(self.login_url, data)
+        self.assertEqual(response.status_code, http_code.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data, {
+            "errors": [
+                {
+                    "status": "403",
+                    "code": "permission_denied",
+                    "detail": expected_detail,
+                    "title": "You do not have permission to perform this action.",
+                }
+            ]
+        })
+
+    def _test_login_validation_error(
+            self, data, expected_code, expected_detail, expected_parameter
+    ):
+        response: Response = self.client.post(self.login_url, data=data)
+
+        self.assertEqual(response.status_code, http_code.HTTP_400_BAD_REQUEST)
+        expected_response_data = {
+            "errors": [
+                {
+                    "status": "400",
+                    "code": expected_code,
+                    "title": "Invalid input.",
+                    "detail": expected_detail,
+                    "source": {"parameter": expected_parameter},
+                }
+            ]
+        }
+        self.assertDictEqual(response.data, expected_response_data)
 
 
 @override_settings(GALAXY_DEPLOYMENT_MODE=DeploymentMode.STANDALONE.value)
