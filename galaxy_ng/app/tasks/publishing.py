029675f3dafc05c94bffcbed43ed55e4060e9fe9
Adrian Likins <alikins@redhat.com>
2020-07-28 15:40:12 -0400

    Use PulpTemporaryFile
    Find import CollectionVersions vis Task CreatedResources
    
    Since PulpTemporaryFile doesn't have a reference to the
    Artifact we can't lookup the created CollectionVersion by
    the Artifact id.
    
    So lookup the current task, find it's CreatedResources, and
    grabbed the CollectionVersions.
    
    Note: In theory, since there is a list of imported CollectionVersions
    now, a single import could in the future created multiple CVs.
    
    This requires:
    - pulpcore >= 3.6.0
    - pulp_ansible > 0.2.0b15
    
    Issue: #330
    Issue: #333

--- galaxy_ng/app/tasks/publishing.py
+++ galaxy_ng/app/tasks/publishing.py
@@ -1,9 +1,11 @@
 import logging
 
 from django.conf import settings
-from pulpcore.plugin.models import ContentArtifact
+from django.contrib.contenttypes.models import ContentType
+
+from pulpcore.plugin.models import Task
 from pulpcore.plugin.tasking import enqueue_with_reservation
-from pulp_ansible.app.models import AnsibleRepository
+from pulp_ansible.app.models import AnsibleDistribution, AnsibleRepository, CollectionVersion
 from pulp_ansible.app.tasks.collections import import_collection
 
 from .promotion import add_content_to_repository, remove_content_from_repository
@@ -16,7 +18,22 @@ GOLDEN_NAME = settings.GALAXY_API_DEFAULT_DISTRIBUTION_BASE_PATH
 STAGING_NAME = settings.GALAXY_API_STAGING_DISTRIBUTION_BASE_PATH
 
 
-def import_and_move_to_staging(artifact_pk, **kwargs):
+def get_created_collection_versions():
+    current_task = Task.current()
+    created_resources = current_task.created_resources.filter(
+        content_type_id=ContentType.objects.get_for_model(CollectionVersion))
+
+    # TODO: replace with values_list
+    created_collection_versions = []
+    for created_resource in created_resources:
+        collection_version = created_resource.content_object
+
+        created_collection_versions.append(collection_version)
+
+    return created_collection_versions
+
+
+def import_and_move_to_staging(temp_file_pk, **kwargs):
     """Import collection version and move to staging repository.
 
     Custom task to call pulp_ansible's import_collection() task then
@@ -25,28 +42,33 @@ def import_and_move_to_staging(artifact_pk, **kwargs):
     This task will not wait for the enqueued tasks to finish.
     """
     inbound_repository_pk = kwargs.get('repository_pk')
-    import_collection(artifact_pk=artifact_pk, repository_pk=inbound_repository_pk)
+    import_collection(temp_file_pk=temp_file_pk,
+                      repository_pk=inbound_repository_pk)
 
-    content_artifact = ContentArtifact.objects.get(artifact_id=artifact_pk)
-    collection_version = content_artifact.content.ansible_collectionversion
-
-    # enqueue task to add collection_version to staging repo
     try:
-        staging_repo = AnsibleRepository.objects.get(name=STAGING_NAME)
+        staging_repo = AnsibleDistribution.objects.get(name=STAGING_NAME).repository
     except AnsibleRepository.DoesNotExist:
         raise RuntimeError(f'Could not find staging repository: "{STAGING_NAME}"')
-    locks = [staging_repo]
-    task_args = (collection_version.pk, staging_repo.pk)
-    enqueue_with_reservation(add_content_to_repository, locks, args=task_args)
 
-    # enqueue task to remove collection_verion from inbound repo
     inbound_repo = AnsibleRepository.objects.get(pk=inbound_repository_pk)
-    locks = [inbound_repo]
-    task_args = (collection_version.pk, inbound_repository_pk)
-    enqueue_with_reservation(remove_content_from_repository, locks, args=task_args)
+
+    inbound_locks = [inbound_repo]
+    staging_locks = [staging_repo]
+
+    created_collection_versions = get_created_collection_versions()
+
+    for collection_version in created_collection_versions:
+        # enqueue task to add collection_version to staging repo
+        add_task_args = (collection_version.pk, staging_repo.pk)
+        enqueue_with_reservation(add_content_to_repository, staging_locks, args=add_task_args)
+
+        # enqueue task to remove collection_verion from inbound repo
+        remove_task_args = (collection_version.pk, inbound_repository_pk)
+        enqueue_with_reservation(remove_content_from_repository,
+                                 inbound_locks, args=remove_task_args)
 
 
-def import_and_auto_approve(artifact_pk, **kwargs):
+def import_and_auto_approve(temp_file_pk, **kwargs):
     """Import collection version and automatically approve.
 
     Custom task to call pulp_ansible's import_collection() task
@@ -54,25 +76,33 @@ def import_and_auto_approve(artifact_pk, **kwargs):
     manual approval action needs to occur.
     """
     inbound_repository_pk = kwargs.get('repository_pk')
-    import_collection(artifact_pk=artifact_pk, repository_pk=inbound_repository_pk)
-    content_artifact = ContentArtifact.objects.get(artifact_id=artifact_pk)
-    collection_version = content_artifact.content.ansible_collectionversion
+    import_collection(temp_file_pk=temp_file_pk, repository_pk=inbound_repository_pk)
 
-    # FIXME: remove when no longer using certification flag
-    collection_version.certification = VERSION_CERTIFIED
-    collection_version.save()
-
-    # enqueue task to add collection_version to golden repo
     try:
-        golden_repo = AnsibleRepository.objects.get(name=GOLDEN_NAME)
+        golden_repo = AnsibleDistribution.objects.get(name=GOLDEN_NAME).repository
     except AnsibleRepository.DoesNotExist:
-        raise RuntimeError(f'Could not find golden repository: "{GOLDEN_NAME}"')
-    locks = [golden_repo]
-    task_args = (collection_version.pk, golden_repo.pk)
-    enqueue_with_reservation(add_content_to_repository, locks, args=task_args)
+        raise RuntimeError(f'Could not find staging repository: "{GOLDEN_NAME}"')
 
-    # enqueue task to remove collection_verion from inbound repo
     inbound_repo = AnsibleRepository.objects.get(pk=inbound_repository_pk)
-    locks = [inbound_repo]
-    task_args = (collection_version.pk, inbound_repository_pk)
-    enqueue_with_reservation(remove_content_from_repository, locks, args=task_args)
+
+    remove_locks = [inbound_repo]
+    add_locks = [golden_repo]
+
+    created_collection_versions = get_created_collection_versions()
+
+    for collection_version in created_collection_versions:
+        collection_version.certification = VERSION_CERTIFIED
+        collection_version.save()
+
+        # enqueue task to add collection_version to golden repo
+        add_task_args = (collection_version.pk, golden_repo.pk)
+        enqueue_with_reservation(add_content_to_repository, add_locks, args=add_task_args)
+
+        # enqueue task to remove collection_verion from inbound repo
+        remove_task_args = (collection_version.pk, inbound_repository_pk)
+        enqueue_with_reservation(remove_content_from_repository,
+                                 remove_locks, args=remove_task_args)
+
+        log.info('Imported and auto approved collection artifact %s to repository %s',
+                 collection_version.relative_path,
+                 golden_repo.latest_version())
--- galaxy_ng/tests/unit/app/test_tasks.py
+++ galaxy_ng/tests/unit/app/test_tasks.py
@@ -3,23 +3,34 @@ import os
 import tempfile
 from unittest import mock
 
-from django.test import TestCase
-from pulpcore.plugin.models import Artifact, ContentArtifact
-from pulp_ansible.app.models import Collection, CollectionVersion, AnsibleRepository
+from django.conf import settings
+from django.test import TestCase, override_settings
+
+from pulpcore.plugin.models import Artifact, PulpTemporaryFile, ContentArtifact
+from pulp_ansible.app.models import (
+    Collection, CollectionVersion, AnsibleRepository, AnsibleDistribution
+)
 
 from galaxy_ng.app.tasks import import_and_auto_approve, import_and_move_to_staging
 
 log = logging.getLogger(__name__)
 
+golden_name = settings.GALAXY_API_DEFAULT_DISTRIBUTION_BASE_PATH
+staging_name = settings.GALAXY_API_STAGING_DISTRIBUTION_BASE_PATH
 
+
+@override_settings(WORKING_DIRECTORY=tempfile.mkdtemp(suffix='galaxy_ng_unittest'))
 class TestTaskPublish(TestCase):
-    artifact_path = os.path.join(tempfile.gettempdir(), 'artifact-tmp')
+    # artifact_path = os.path.join(tempfile.gettempdir(), 'artifact-tmp')
+    artifact_path = os.path.join(settings.WORKING_DIRECTORY, 'artifact-tmp')
 
     def setUp(self):
         with open(self.artifact_path, 'w') as f:
             f.write('Temp Artifact File')
-        self.artifact = Artifact.init_and_validate(self.artifact_path)
-        self.artifact.save()
+        self.pulp_temp_file = PulpTemporaryFile.init_and_validate(self.artifact_path)
+        self.pulp_temp_file.save()
+
+        self.artifact = Artifact.from_pulp_temporary_file(self.pulp_temp_file)
 
         collection = Collection.objects.create(namespace='my_ns', name='my_name')
         self.collection_version = CollectionVersion.objects.create(collection=collection)
@@ -31,14 +42,26 @@ class TestTaskPublish(TestCase):
         )
         content_artifact.save()
 
+    @mock.patch('galaxy_ng.app.tasks.publishing.get_created_collection_versions')
     @mock.patch('galaxy_ng.app.tasks.publishing.import_collection')
     @mock.patch('galaxy_ng.app.tasks.publishing.enqueue_with_reservation')
-    def test_import_and_auto_approve(self, mocked_enqueue, mocked_import):
-        inbound_repo = AnsibleRepository.objects.create(name='the_incoming_repo')
-        golden_repo = AnsibleRepository.objects.create(name='automation-hub')
+    def test_import_and_auto_approve(self, mocked_enqueue, mocked_import, mocked_get_created):
+        inbound_repo = AnsibleRepository.objects.create(name=staging_name)
+        golden_repo = AnsibleRepository.objects.create(name=golden_name)
+
+        golden_dist = AnsibleDistribution(name=golden_name, base_path=golden_name)
+        staging_dist = AnsibleDistribution(name=staging_name, base_path=staging_name)
+        golden_dist.repository = golden_repo
+        staging_dist.repository = inbound_repo
+        golden_dist.save()
+        staging_dist.save()
 
         self.assertTrue(self.collection_version.certification == 'needs_review')
-        import_and_auto_approve(self.artifact.pk, repository_pk=inbound_repo.pk)
+
+        mocked_get_created.return_value = [self.collection_version]
+
+        import_and_auto_approve(self.pulp_temp_file.pk, repository_pk=inbound_repo.pk)
+
         self.collection_version.refresh_from_db()
         self.assertTrue(self.collection_version.certification == 'certified')
         self.assertTrue(mocked_import.call_count == 1)
@@ -47,22 +70,37 @@ class TestTaskPublish(TestCase):
         # test cannot find golden repo
         golden_repo.name = 'a_different_name_for_golden'
         golden_repo.save()
-        golden_errmsg = 'Could not find golden repository: "automation-hub"'
-        with self.assertRaisesMessage(RuntimeError, golden_errmsg):
+        mocked_get_created.side_effect = AnsibleDistribution.DoesNotExist
+        with self.assertRaises(AnsibleDistribution.DoesNotExist):
             import_and_auto_approve(self.artifact.pk, repository_pk=inbound_repo.pk)
 
+    @mock.patch('galaxy_ng.app.tasks.publishing.get_created_collection_versions')
     @mock.patch('galaxy_ng.app.tasks.publishing.import_collection')
     @mock.patch('galaxy_ng.app.tasks.publishing.enqueue_with_reservation')
-    def test_import_and_move_to_staging(self, mocked_enqueue, mocked_import):
-        inbound_repo = AnsibleRepository.objects.create(name='the_incoming_repo')
-        staging_repo = AnsibleRepository.objects.create(name='staging')
-        import_and_move_to_staging(self.artifact.pk, repository_pk=inbound_repo.pk)
+    def test_import_and_move_to_staging(self, mocked_enqueue, mocked_import, mocked_get_created):
+        inbound_name = 'the_incoming_repo'
+        inbound_repo = AnsibleRepository.objects.create(name=inbound_name)
+        staging_repo = AnsibleRepository.objects.create(name=staging_name)
+        inbound_repo.save()
+        staging_repo.save()
+
+        inbound_dist = AnsibleDistribution(name=inbound_name, base_path=inbound_name)
+        staging_dist = AnsibleDistribution(name=staging_name, base_path=staging_name)
+        inbound_dist.repository = inbound_repo
+        staging_dist.repository = staging_repo
+        inbound_dist.save()
+        staging_dist.save()
+
+        mocked_get_created.return_value = [self.collection_version]
+
+        import_and_move_to_staging(self.pulp_temp_file.pk, repository_pk=inbound_repo.pk)
+
         self.assertTrue(mocked_import.call_count == 1)
         self.assertTrue(mocked_enqueue.call_count == 2)
 
         # test cannot find staging repo
         staging_repo.name = 'a_different_name_for_staging'
         staging_repo.save()
-        staging_errmsg = 'Could not find staging repository: "staging"'
-        with self.assertRaisesMessage(RuntimeError, staging_errmsg):
-            import_and_move_to_staging(self.artifact.pk, repository_pk=inbound_repo.pk)
+        mocked_get_created.side_effect = AnsibleDistribution.DoesNotExist
+        with self.assertRaises(AnsibleDistribution.DoesNotExist):
+            import_and_move_to_staging(self.pulp_temp_file.pk, repository_pk=inbound_repo.pk)
