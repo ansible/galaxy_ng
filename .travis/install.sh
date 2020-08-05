00a2841fe08347742c0b110ba9af9a2583453d6a
Alexander Saprykin <osapryki@redhat.com>
2020-08-05 15:11:43 +0200

    Backport PR #321 to stable branch (#360)
    * Revert "Updating and fixing the CI (#322) (#358)"
    
    This reverts commit bbb925be4023502d08e7984a62f26b1c9befe7fb.
    No-Issue
    
    * Updating and fixing the CI
    
    - Making build errors easier to see
    - Removing pulp_ansible from "Required PR"
    
    No-Issue
    
    * OpenAPI v3 with drf-spectacular
    
    No-Issue
    
    Co-authored-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -44,12 +44,6 @@ else
   # Fallback
   TAG=$(git rev-parse --abbrev-ref HEAD | tr / _)
 fi
-
-if [ -e $TRAVIS_BUILD_DIR/../pulp_ansible ]; then
-  PULP_ANSIBLE=./pulp_ansible
-else
-  PULP_ANSIBLE=git+https://github.com/pulp/pulp_ansible.git@master
-fi
 if [ -n "$TRAVIS_TAG" ]; then
   # Install the plugin only and use published PyPI packages for the rest
   # Quoting ${TAG} ensures Ansible casts the tag as a string.
@@ -62,8 +56,6 @@ plugins:
     source: pulpcore
   - name: galaxy_ng
     source: ./galaxy_ng
-  - name: pulp_ansible
-    source: pulp_ansible
 services:
   - name: pulp
     image: "pulp:${TAG}"
@@ -80,8 +72,6 @@ plugins:
     source: ./pulpcore
   - name: galaxy_ng
     source: ./galaxy_ng
-  - name: pulp_ansible
-    source: $PULP_ANSIBLE
 services:
   - name: pulp
     image: "pulp:${TAG}"
