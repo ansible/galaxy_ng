3c3924f67a6a2016410a82ab9fb14f476560b2d1
Chris Houseknecht <chousekn@redhat.com>
2020-08-19 11:54:42 -0400

    Pinning to latest pulpcore, pulp-ansible and pulp-container (#381)
    Closes-Issue: #380

--- .travis/install.sh
+++ .travis/install.sh
@@ -45,11 +45,6 @@ else
   TAG=$(git rev-parse --abbrev-ref HEAD | tr / _)
 fi
 
-if [ -e $TRAVIS_BUILD_DIR/../pulp_ansible ]; then
-  PULP_ANSIBLE=./pulp_ansible
-else
-  PULP_ANSIBLE=git+https://github.com/pulp/pulp_ansible.git@master
-fi
 if [ -n "$TRAVIS_TAG" ]; then
   # Install the plugin only and use published PyPI packages for the rest
   # Quoting ${TAG} ensures Ansible casts the tag as a string.
@@ -81,7 +76,7 @@ plugins:
   - name: galaxy_ng
     source: ./galaxy_ng
   - name: pulp_ansible
-    source: $PULP_ANSIBLE
+    source: ./pulp_ansible
 services:
   - name: pulp
     image: "pulp:${TAG}"
---
+++ CHANGES/380.feature
@@ -0,0 +1,1 @@
+Pin to pulpcore 3.6.0, pulp-ansible 0.2.0 and pulp-container 2.0.0
