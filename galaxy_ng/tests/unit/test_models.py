6de151dcf0b344cd00d050cc2110b883dc79aeaa
Andrew Crosby <acrosby@redhat.com>
2020-09-02 13:13:30 -0400

    Create namespaces on content sync via django signal
    Closes-Issue: #404
    
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy_ng/tests/unit/test_models.py
+++ galaxy_ng/tests/unit/test_models.py
@@ -1,9 +1,29 @@
 from django.test import TestCase
+from pulp_ansible.app.models import Collection
+
+from galaxy_ng.app.models import Namespace
 
 
-class TestNothing(TestCase):
-    """Test Nothing (placeholder)."""
+class TestSignalCreateNamespace(TestCase):
+    namespace_name = 'my_test_ns'
 
-    def test_nothing_at_all(self):
-        """Test that the tests are running and that's it."""
-        self.assertTrue(True)
+    def test_new_collection_create_namespace(self):
+        self.assertFalse(Namespace.objects.filter(name=self.namespace_name))
+        Collection.objects.create(
+            name='my_collection',
+            namespace=self.namespace_name,
+        )
+        self.assertTrue(Namespace.objects.filter(name=self.namespace_name))
+
+    def test_existing_namespace_not_changed(self):
+        description = 'Namespace created not by signal'
+        Namespace.objects.create(
+            name=self.namespace_name,
+            description=description,
+        )
+        Collection.objects.create(
+            name='my_collection',
+            namespace=self.namespace_name,
+        )
+        namespace = Namespace.objects.get(name=self.namespace_name)
+        self.assertEquals(namespace.description, description)
