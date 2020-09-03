c278483f391e4167253eea1526397e62b6328fed
Andrew Crosby <acrosby@redhat.com>
2020-09-03 13:10:51 -0400

    Add docs_blob to v3 api for collection versions (#408)
    Closes-Issue: #403
    
    Add endpoint with docs_blob for a CollectionVersion:
    v3/collections/<namespace>/<collection>/versions/<version_number>/docs-blob/
    
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy_ng/app/api/v3/urls.py
+++ galaxy_ng/app/api/v3/urls.py
@@ -39,6 +39,11 @@ urlpatterns = [
         name='collection-versions-detail',
     ),
     path(
+        'collections/<str:namespace>/<str:name>/versions/<str:version>/docs-blob/',
+        viewsets.CollectionVersionDocsViewSet.as_view({'get': 'retrieve'}),
+        name='collection-versions-detail-docs',
+    ),
+    path(
         'imports/collections/<str:pk>/',
         viewsets.CollectionImportViewSet.as_view({'get': 'retrieve'}),
         name='collection-import',
