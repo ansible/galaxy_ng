7b846d052536d60616f35cabd931d6826e5af4a8
Bruno Rocha <rochacbruno@gmail.com>
2020-10-08 18:24:35 -0300

    Fix travis check_pull_request issue message script
    Change custom_check_pull_request to point to Jira issues.
    
    Issue: AAH-44

--- .travis/custom_check_pull_request.py
+++ .travis/custom_check_pull_request.py
@@ -34,10 +34,10 @@ CHANGELOG_REQUIRED_LABELS = set(ISSUE_LABELS) - {
 
 NO_ISSUE_REGEX = re.compile(r"^\s*{}\s*$".format(NO_ISSUE), re.MULTILINE)
 ISSUE_LABEL_REGEX = re.compile(
-    r"^\s*({}):\s+#(\d+)\s*$".format("|".join(ISSUE_LABELS)), re.MULTILINE,
+    r"^\s*({}):\s+AAH-(\d+)\s*$".format("|".join(ISSUE_LABELS)), re.MULTILINE,
 )
 
-GITHUB_URL = "https://github.com"
+JIRA_URL = "https://issues.redhat.com/rest/api/latest/issue/AAH-{issue}"
 
 
 def git_list_commits(commit_range):
@@ -56,23 +56,23 @@ def git_commit_message(commit_sha):
 def check_changelog_record(issue):
     reconrds_count = len(glob.glob(f"CHANGES/{issue}.*"))
     if reconrds_count == 0:
-        LOG.error(f"Missing change log entry for issue #{issue}.")
+        LOG.error(f"Missing change log entry for issue AAH-{issue}.")
         return False
     if reconrds_count == 1:
         return True
-    LOG.error(f"Multiple change log records found for issue #{issue}.")
-    return False
+    LOG.info(f"Multiple change log records found for issue AAH-{issue}.")
+    return True
 
 
-def check_issue_exists(repo_slug, issue):
-    response = requests.head(f"{GITHUB_URL}/{repo_slug}/issues/{issue}")
+def check_issue_exists(issue):
+    response = requests.head(JIRA_URL.format(issue=issue))
     if not response.ok:
-        LOG.error(f"Referenced issue #{issue} not found in repository {repo_slug}.")
+        LOG.error(f"Referenced issue AAH-{issue} not found in Jira.")
         return False
     return True
 
 
-def check_commit(commit_sha, repo_slug):
+def check_commit(commit_sha):
     commit_message = git_commit_message(commit_sha)
     issue_labels = ISSUE_LABEL_REGEX.findall(commit_message)
     if not issue_labels:
@@ -83,7 +83,7 @@ def check_commit(commit_sha, repo_slug):
 
     ok = True
     for label, issue in issue_labels:
-        if not check_issue_exists(repo_slug, issue):
+        if not check_issue_exists(issue):
             ok = False
         if label in CHANGELOG_REQUIRED_LABELS and not check_changelog_record(issue):
             ok = False
@@ -94,14 +94,14 @@ def main():
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
     # pull_request = os.environ["TRAVIS_PULL_REQUEST"]
-    repo_slug = os.environ["TRAVIS_REPO_SLUG"]
+    # repo_slug = os.environ["TRAVIS_REPO_SLUG"]
     commit_range = os.environ["TRAVIS_COMMIT_RANGE"].split("...")
     commit_list = git_list_commits(commit_range)
 
     ok = True
     for commit_sha in commit_list:
         LOG.debug(f"Checking commit {commit_sha[:8]} ...")
-        if not check_commit(commit_sha, repo_slug):
+        if not check_commit(commit_sha):
             ok = False
 
     # TODO: Validate pull request message.
