69a692460e4aeec204742b00aa781dffbce745a2
Andrew Crosby <acrosby@redhat.com>
2020-10-26 14:37:41 -0400

    Re-add explictly IsAuthenticated permission to root endpoints
    No-Issue

--- galaxy_ng/app/api/ui/viewsets/root.py
+++ galaxy_ng/app/api/ui/viewsets/root.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from galaxy_ng.app.api import base as api_base
@@ -5,7 +6,7 @@ from galaxy_ng.app import constants
 
 
 class APIRootView(api_base.ViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
         return Response({
