6c2271a57c49fb94a6662c0d137dc4724bb94082
Bruno Rocha <rochacbruno@users.noreply.github.com>
2020-11-19 13:56:25 -0300

    API to return 409 in case of existing group/namespace (#572)
    - In case of existing group or namespace API returns 409 conflict
    - Fixed error message that was not well formatted on PR #562
    - Added unittest for namespace
    
    Issue: AAH-152
    (cherry picked from commit 829fa348f425b9820f62d93e127a1e188a07ef28)

--- galaxy_ng/app/api/ui/viewsets/group.py
+++ galaxy_ng/app/api/ui/viewsets/group.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django_filters import filters
 from django_filters.rest_framework import filterset, DjangoFilterBackend
 
@@ -5,6 +6,7 @@ from pulpcore.app import viewsets
 from galaxy_ng.app.access_control import access_policy
 
 from galaxy_ng.app.api.base import LocalSettingsMixin
+from galaxy_ng.app.exceptions import ConflictError
 from django.contrib.auth.models import Group
 
 
@@ -26,6 +28,15 @@ class GroupViewSet(LocalSettingsMixin, viewsets.GroupViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = GroupFilter
     permission_classes = [access_policy.GroupAccessPolicy]
+
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        name = request.data['name']
+        if Group.objects.filter(name=name).exists():
+            raise ConflictError(
+                detail={'name': f'A group named {name} already exists.'}
+            )
+        return super().create(request, *args, **kwargs)
 
 
 class GroupModelPermissionViewSet(LocalSettingsMixin, viewsets.GroupModelPermissionViewSet):
