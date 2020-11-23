65980e0fd40c3c15a9528850b7a13e10b01dca5e
Andrew Crosby <acrosby@redhat.com>
2020-11-23 10:17:48 -0500

    Upgrade to pulp_ansible 0.5.2 (#579)
    Issue: AAH-177

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ requirements = [
     "Django~=2.2.3",
     "galaxy-importer==0.2.11",
     "pulpcore>=3.7,<3.9",
-    "pulp-ansible==0.5.1",
+    "pulp-ansible==0.5.2",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
 ]
