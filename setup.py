009171d5ece87e0478cc2570bcdf34e620092b69
Andrew Crosby <acrosby@redhat.com>
2021-02-08 14:03:04 -0500

    Update django dependency
    Issue: AAH-299

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class BuildPyCommand(_BuildPyCommand):
 
 
 requirements = [
-    "Django~=2.2.3",
+    "Django~=2.2.18",
     "galaxy-importer==0.2.14",
     "pulpcore>=3.7,<3.9",
     "pulp-ansible==0.5.5",
