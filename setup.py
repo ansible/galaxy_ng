eac58dcbdf11d4bec83f50f554c88eff095e5629
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-17 16:28:41 -0300

    Just for testing the change
    Enable pulpcore from PR
    Required PR: https://github.com/pulp/pulpcore/pull/1130
    No-Issue

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class BuildPyCommand(_BuildPyCommand):
 requirements = [
     "Django~=2.2.18",
     "galaxy-importer==0.2.15",
-    "pulpcore<3.11,>=3.10",
+    "pulpcore",
     "pulp-ansible==0.7.0",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
