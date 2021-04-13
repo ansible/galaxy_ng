ansible/galaxy_ng

commit bd84c7afb82fef2b282000c92f8f1b4809514334

Replace PULP_REDIS* env variables with PULP_REDIS_URL env variable (#715)
* Replace current PULP_REDIS* env variables with PULP_REDIS_URL env variable to accommodate PULP_REDIS_SSL

* Conditionally use redis:// or rediss:// based on PULP_REDIS_SSL

* Include PULP_REDIS_PASSWORD if set

Issue: AAH-486

docker/entrypoint.sh
@@ -96,10 +96,31 @@ run_manage() {
 }
 
 
+redis_connection_hack() {
+    if [[ "$PULP_REDIS_SSL" == "true" ]]; then
+        protocol="rediss://"
+    else
+        protocol="redis://"
+    fi
+
+    if [[ -z "$PULP_REDIS_PASSWORD" ]]; then
+        password=""
+    else
+        password=":${PULP_REDIS_PASSWORD}@"
+    fi
+
+    PULP_REDIS_URL="${protocol}${password}${PULP_REDIS_HOST:-localhost}:${PULP_REDIS_PORT:-6379}/0"
+    unset PULP_REDIS_HOST PULP_REDIS_PORT PULP_REDIS_PASSWORD
+    export PULP_REDIS_URL
+}
+
 main() {
     if [[ "$#" -eq 0 ]]; then
         exec "/bin/bash"
     fi
+
+    # TODO: remove once Pulp recognizes REDIS_SSL parameter when building settings for RQ in pulpcore/rqconfig.py
+    redis_connection_hack
 
     case "$1" in
         'run')

commit 76f1d53a8e14e62922ce52d272d473967deb8c04

Fix docker entrypoint errors introduced by #715 (#721)
* Fixed unset variable and tcp port unavailable errors
* Improve password check
* Avoid reassigning variables
Issue: AAH-486

docker/entrypoint.sh
@@ -72,6 +72,10 @@ run_service() {
     wait-for-tcp "${PULP_DB_HOST:-localhost}" "${PULP_DB_PORT:-5432}"
     wait-for-tcp "${PULP_REDIS_HOST:-localhost}" "${PULP_REDIS_PORT:-6379}"
 
+    # TODO: remove once Pulp recognizes REDIS_SSL parameter when building 
+    # settings for RQ in pulpcore/rqconfig.py
+    redis_connection_hack
+
     if [[ "$WITH_DEV_INSTALL" -eq "1" ]]; then
         install_local_deps
     fi
@@ -97,13 +101,13 @@ run_manage() {
 
 
 redis_connection_hack() {
-    if [[ "$PULP_REDIS_SSL" == "true" ]]; then
+    if [[ ${PULP_REDIS_SSL:-false} = "true" ]]; then
         protocol="rediss://"
     else
         protocol="redis://"
     fi
 
-    if [[ -z "$PULP_REDIS_PASSWORD" ]]; then
+    if [[ -z "${PULP_REDIS_PASSWORD:-}" ]]; then
         password=""
     else
         password=":${PULP_REDIS_PASSWORD}@"
@@ -118,9 +122,6 @@ main() {
     if [[ "$#" -eq 0 ]]; then
         exec "/bin/bash"
     fi
-
-    # TODO: remove once Pulp recognizes REDIS_SSL parameter when building settings for RQ in pulpcore/rqconfig.py
-    redis_connection_hack
 
     case "$1" in
         'run')