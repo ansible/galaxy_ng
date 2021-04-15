2403412583e79911317989fa4f341040eb1b2700
Andrew Crosby <acrosby@redhat.com>
2021-04-15 16:15:19 -0400

    Add feature flag to enable/disable execution environments (#728)
    * Add GALAXY_FEATURE_FLAGS dictionary to settings.py
    * Add _ui/v1/feature-flags/ to display feature flag setting
    * Conditionally add execution environment routes based on feature flag
    
    Issue: AAH-298

--- galaxy_ng/app/api/ui/views/__init__.py
+++ galaxy_ng/app/api/ui/views/__init__.py
@@ -2,9 +2,13 @@ from .auth import (
     LoginView,
     LogoutView,
 )
+from .feature_flags import FeatureFlagsView
 
 __all__ = (
     # auth
     "LoginView",
     "LogoutView",
+
+    # feature_flags
+    "FeatureFlagsView",
 )
