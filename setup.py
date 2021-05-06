b85213346bd5f9fddf82838cea50ab4a5bdab089
Adrian Likins <alikins@redhat.com>
2021-05-06 10:32:53 -0400

    Temporarily pin django to 2.2.20 (#754) (#758)
    Pin django to 2.2.20 until https://pulp.plan.io/issues/8691
    and/or https://code.djangoproject.com/ticket/32718 is resolved.
    This fixes CI build issues for now.
    
    Issue: AAH-584
    
    (cherry picked from commit 57f3922f3fe7719c5da5519e2b0dafca3ccfb8e5)

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class BuildPyCommand(_BuildPyCommand):
 
 
 requirements = [
-    "Django~=2.2.18",
+    "Django==2.2.20",
     "galaxy-importer==0.3.0",
     "pulpcore<3.12,>=3.11",
     "pulp-ansible==0.7.1",
