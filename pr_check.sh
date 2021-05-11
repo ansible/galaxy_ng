122241ba64f54f8759016907a19c163647061fd6
Brian McLaughlin <bmclaugh@redhat.com>
2021-05-11 09:31:40 -0400

    Update pr_check Issue: AAH-579

--- pr_check.sh
+++ pr_check.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# TODO(cutwater): This is dummy script to be called by Jenkins CI.
-# 		  To be implemented.
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="automation-hub"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="automation-hub"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/automation-hub-galaxy-ng"
+
+# IQE_PLUGINS=""
+# IQE_MARKER_EXPRESSION="ephemeral"
+# IQE_FILTER_EXPRESSION=""
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+export IMAGE_TAG="pr-$IMAGE_TAG"
+
+source $CICD_ROOT/build.sh
+# source $APP_ROOT/unit_test.sh
+# source $CICD_ROOT/deploy_ephemeral_env.sh
+# source $CICD_ROOT/smoke_test.sh
+
+# overriding IMAGE_TAG defined by boostrap.sh, for now
+
+# source smoke_test.sh
 
 
 # Need to make a dummy results file to make tests pass
