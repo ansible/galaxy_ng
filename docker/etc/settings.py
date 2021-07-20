11a4af9875ddc177833aef11e05e4c584ce3a233
Andrew Crosby <acrosby@redhat.com>
2021-07-20 00:06:37 -0400

    Revert "Fix typo in AWS S3 configuration for Clowder (#843)"
    This reverts commit f7a7419747d08034979cdddb8392d0231047208f.

--- docker/etc/settings.py
+++ docker/etc/settings.py
@@ -49,7 +49,7 @@ if clowder_config and clowder_config.isClowderEnabled():
         'PASSWORD': LocalConfig.database.password
     }
     # AWS S3 configuration
-    AWS_S3_ENDPOINT_URL = 'http://{}:{}'.format(
+    AWS_S3_ENTRYPOINT_URL = 'http://{}:{}'.format(
         LocalConfig.objectStore.hostname,
         LocalConfig.objectStore.port
     )
