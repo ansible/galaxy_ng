6930adbdfbde688eaf2eee320da933b738eaa5e5
Adrian Likins <alikins@redhat.com>
2021-07-22 08:53:01 -0400

    Create per namespace inbound repos on all namespace creation (#816)
    This will create a new Repository and Distribution for every
    Namespace in Galaxy.
    
    Add delete() to Namespace model.
    Remove delete() from NamespaceManager.
    Previous unit test for delete was using a query_set.delete()
    which does a bulk delete, and not using the NamespaceManager.delete()
    
    Update unit tests.
    
    Issue: AAH-739
    (cherry picked from commit eaea077d1157d13fdb253c459608832736621dda)

--- galaxy_ng/app/models/namespace.py
+++ galaxy_ng/app/models/namespace.py
@@ -9,7 +9,6 @@ from pulp_ansible.app.models import AnsibleRepository, AnsibleDistribution
 from galaxy_ng.app.access_control import mixins
 from galaxy_ng.app.constants import INBOUND_REPO_NAME_FORMAT
 
-
 __all__ = ("Namespace", "NamespaceLink")
 
 
@@ -48,9 +47,11 @@ class NamespaceManager(models.Manager):
             create_inbound_repo(obj.name)
         return super().bulk_create(objs, **kwargs)
 
-    def delete(self):
-        delete_inbound_repo(self.name)
-        return super().delete()
+    def get_or_create(self, *args, **kwargs):
+        ns, created = super().get_or_create(*args, **kwargs)
+        if created:
+            create_inbound_repo(kwargs['name'])
+        return ns, created
 
 
 class Namespace(LifecycleModel, mixins.GroupModelPermissionsMixin, AutoDeleteObjPermsMixin):
@@ -95,6 +96,10 @@ class Namespace(LifecycleModel, mixins.GroupModelPermissionsMixin, AutoDeleteObj
             NamespaceLink(name=link["name"], url=link["url"], namespace=self)
             for link in links
         )
+
+    def delete(self, *args, **kwargs):
+        delete_inbound_repo(self.name)
+        return super().delete(*args, **kwargs)
 
     class Meta:
         permissions = (
