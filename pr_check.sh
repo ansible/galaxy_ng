07d9c98b57080dc13cdc4e49a340daa159f803af
Andrew Crosby <acrosby@redhat.com>
2021-08-03 13:15:44 -0400

    testing
    No-Issue

--- pr_check.sh
+++ pr_check.sh
@@ -16,6 +16,9 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s "$CICD_URL/bootstrap.sh" > .cicd_bootstrap.sh
 source .cicd_bootstrap.sh
 
+
+
+
 export IMAGE_TAG="pr-$IMAGE_TAG"
 
 source "$CICD_ROOT/build.sh"
