611093c6a8369952e1a21b1062b5af2b7398216d
Bruno Rocha <rochacbruno@gmail.com>
2021-09-02 15:47:05 +0100

    Ensure retain_repo_versions==1 for repositories
    - A migration to update existing repos
    - A Signal to handle new repos
    
    Issue: AAH-708

--- galaxy_ng/tests/unit/test_models.py
+++ galaxy_ng/tests/unit/test_models.py
@@ -5,6 +5,36 @@ from pulp_ansible.app.models import AnsibleDistribution, AnsibleRepository, Coll
 from galaxy_ng.app.models import Namespace
 
 
+class TestSignalCreateRepository(TestCase):
+    def test_create_repository_ensure_retain_repo_versions(self):
+        """On creation retain_repo_versions is set to 1 if omited"""
+        repo_name = INBOUND_REPO_NAME_FORMAT.format(namespace_name="test")
+        repository = AnsibleRepository.objects.create(name=repo_name)
+        self.assertEqual(repository.name, repo_name)
+        self.assertEqual(repository.retain_repo_versions, 1)
+
+    def test_when_set_not_changed_retain_repo_versions(self):
+        """On creation retain_repo_versions is not changed when explicit set"""
+        repo_name = INBOUND_REPO_NAME_FORMAT.format(namespace_name="test2")
+        repository = AnsibleRepository.objects.create(name=repo_name, retain_repo_versions=99)
+        self.assertEqual(repository.name, repo_name)
+        self.assertEqual(repository.retain_repo_versions, 99)
+
+    def test_update_do_not_change_retain_repo_versions(self):
+        """On update retain_repo_versions is not changed when already exists"""
+        # Create repo setting retain_repo_versions
+        repo_name = INBOUND_REPO_NAME_FORMAT.format(namespace_name="test3")
+        repository = AnsibleRepository.objects.create(name=repo_name, retain_repo_versions=99)
+        self.assertEqual(repository.name, repo_name)
+        self.assertEqual(repository.retain_repo_versions, 99)
+        # Update the name of the repo
+        AnsibleRepository.objects.filter(pk=repository.pk).update(name="test3_2")
+        updated = AnsibleRepository.objects.get(pk=repository.pk)
+        # Ensure name changed but retain_repo_versions did not
+        self.assertEqual(updated.name, "test3_2")
+        self.assertEqual(updated.retain_repo_versions, 99)
+
+
 class TestSignalCreateNamespace(TestCase):
     namespace_name = 'my_test_ns'
 
