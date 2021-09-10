4e39e29b1ffd120cc8e71acbf7b7298b4fa2a742
David Newswanger <dnewswan@redhat.com>
2021-09-10 16:10:00 -0400

    Release 4.4.0a1
    No-Issue

--- galaxy_ng/app/__init__.py
+++ galaxy_ng/app/__init__.py
@@ -6,7 +6,7 @@ class PulpGalaxyPluginAppConfig(PulpPluginAppConfig):
 
     name = "galaxy_ng.app"
     label = "galaxy"
-    version = "4.4.0dev"
+    version = "4.4.0a1"
 
     def ready(self):
         super().ready()
