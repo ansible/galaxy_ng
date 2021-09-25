6ec991378aadeaea80394468c01c4552e395cc40
Bruno Rocha <rochacbruno@users.noreply.github.com>
2021-09-25 14:24:30 +0100

    Update galaxy_ng/app/viewsets.py
    No-Issue
    
    Co-authored-by: David Davis <daviddavis@users.noreply.github.com>

--- galaxy_ng/app/viewsets.py
+++ galaxy_ng/app/viewsets.py
@@ -17,7 +17,7 @@ def __getattr__(self, name):
     This file is necessary to prevent the DRF web API browser from breaking on all of the
     pulp/api/v3/repositories/ endpoints.
     Pulp expects models it manages to come with a viewset attached to them. Since the
-    ContainerRegistryRemote model is a pulp remote, pulp expects it to have a pulp
+    ContainerRegistryRemote model is a pulp remote, pulp expects it to have a NamedModelViewSet
     viewset attached to it. Pulp associates viewsets with models by looking in
     <plugin_name>.app.viewsets. Since galaxy_ng stores it's viewsets in a different
     module, this file is necesary for pulp tbe able to associate the ContainerRegistryRemote
