eb0a1a63e17db8a0a17408ed52393f31bb3adc05
Brian McLaughlin <bmclaugh@redhat.com>
2021-10-21 14:29:17 -0400

    Adding pulp_container to requirements Issue: AAH-906

--- galaxy_ng/tests/functional/cli/test_container_repository_tags.py
+++ galaxy_ng/tests/functional/cli/test_container_repository_tags.py
@@ -36,6 +36,7 @@ class ContainerRepositoryTagsTestCase(TestCaseUsingBindings, rbac_base.BaseRegis
         cls.registry_name = urlparse(cls.cfg.get_base_url()).netloc
         admin_user, admin_password = cls.cfg.pulp_auth
         cls.user_admin = {"username": admin_user, "password": admin_password}
+        cls._pull(f"{DOCKERHUB_PULP_FIXTURE_1}:manifest_a")
 
     def test_list_container_repository_tags(self):
         image_name = "foo/bar"
