9d5fcefed4848cda7040a07c81a4e22e5a156acc
Brian McLaughlin <bmclaugh@redhat.com>
2021-12-10 15:31:30 -0500

    Verify uploaded collection is logged in api access log Updated test_upload_collection to cleanup uploaded collection Issue: AAH-1018

--- galaxy_ng/tests/functional/cli/test_collection_upload.py
+++ galaxy_ng/tests/functional/cli/test_collection_upload.py
@@ -1,7 +1,8 @@
-"""Tests that Collections can be uploaded to  Pulp with the ansible-galaxy CLI."""
+"""Tests that Collections can be uploaded to Pulp with the ansible-galaxy CLI."""
 
 import subprocess
 import tempfile
+from unittest.case import skip
 
 from pulp_smash.pulp3.bindings import delete_orphans
 from pulp_smash.utils import http_get
@@ -44,3 +45,54 @@ class UploadCollectionTestCase(TestCaseUsingBindings):
         self.assertEqual(collection.namespace, "pulp")
         self.assertEqual(collection.name, "squeezer")
         self.assertEqual(collection.highest_version["version"], "0.0.9")
+
+        # Cleanup
+        self.delete_collection(collection.namespace, collection.name)
+        self.delete_namespace(collection.namespace)
+
+    @skip("Requires GALAXY_ENABLE_API_ACCESS_LOG to be enabled")
+    def test_uploaded_collection_logged(self):
+        """Test whether a Collection uploaded via ansible-galaxy is logged correctly in API Access Log."""
+        delete_orphans()
+
+        # Create namespace if it doesn't exist
+        data = str(self.namespace_api.list().data)
+        if "pulp" not in data:
+            self.namespace_api.create(namespace={"name": "pulp", "groups": []})
+
+        # Preapare ansible.cfg for ansible-galaxy CLI
+        self.update_ansible_cfg("inbound-pulp")
+
+        # In a temp dir, publish a collection using ansible-galaxy CLI
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            content = http_get("https://galaxy.ansible.com/download/pulp-squeezer-0.0.9.tar.gz")
+            collection_path = f"{tmp_dir}/pulp-squeezer-0.0.9.tar.gz"
+            with open(collection_path, "wb") as f:
+                f.write(content)
+
+            cmd = "ansible-galaxy collection publish -vvv -c {}".format(collection_path)
+
+            subprocess.run(cmd.split())
+
+            cmd = f"docker cp galaxy_ng_worker_1:/var/log/galaxy_api_access.log {tmp_dir}"
+
+            subprocess.run(cmd.split())
+
+            with open(f"{tmp_dir}/galaxy_api_access.log") as f:
+                log_contents = f.readlines()
+            
+        # Verify that the collection was published
+        collections = self.collections_api.list("published")
+        self.assertEqual(collections.meta.count, 1)
+
+        # Verify that the colletion publishing was logged in the api access log
+        collection = self.collections_api.read(path="published", namespace="pulp", name="squeezer")
+        for line in log_contents:
+            if "INFO: Collection uploaded: " in line:
+                self.assertIn(collection.namespace, line)
+                self.assertIn(collection.name, line)
+                self.assertIn(collection.highest_version["version"], line)
+
+        # Cleanup
+        self.delete_collection(collection.namespace, collection.name)
+        self.delete_namespace(collection.namespace)
