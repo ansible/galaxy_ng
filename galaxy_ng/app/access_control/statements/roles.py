c5f07847bcbfe02ef481be0e0891b7c3081f4cf3
Brian McLaughlin <bmclaugh@redhat.com>
2022-01-11 16:49:43 -0500

    Added roles: galaxy.collection_admin, galaxy.execution_environment_admin Issue: AAH-1092

--- galaxy_ng/app/access_control/statements/roles.py
+++ galaxy_ng/app/access_control/statements/roles.py
@@ -1,4 +1,27 @@
 VIEWSETS = {
+    "CollectionViewSet": {
+        "LOCKED_ROLES": {
+            "galaxy.collection_admin": [
+                "galaxy.change_namespace",
+                "galaxy.delete_namespace",
+                "galaxy.view_namespace",
+                "galaxy.upload_to_namespace",
+                "ansible.delete_collection",
+            ],
+        }
+    },
+    "ContainerRepositoryViewSet": {
+        "LOCKED_ROLES": {
+            "galaxy.execution_environment_admin": [
+                "container.delete_containerrepository",
+                "container.namespace_change_containerdistribution",
+                "container.namespace_modify_content_containerpushrepository",
+                "container.namespace_push_containerdistribution",
+                "container.add_containernamespace",
+                "container.change_containernamespace",
+            ],
+        }
+    },
     "NamespaceViewSet": {
         "LOCKED_ROLES": {
             "galaxy.namespace_owner": [
