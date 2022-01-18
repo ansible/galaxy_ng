162f7f7edfd6ef34fad91169d82cf60d22abda05
James Tanner <tanner.jc@gmail.com>
2022-01-18 11:07:17 -0500

    Test test test
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/ephemeral/smoke_test.sh
+++ dev/ephemeral/smoke_test.sh
@@ -2,9 +2,13 @@
 
 echo "SMOKE TEST!"
 
-# Need to fix the user first ...
+echo "Set project to ${NAMESPACE}"
 oc project ${NAMESPACE}
+
+echo "Find galaxy-api pod"
 AH_API_POD=$(oc get pod -l pod=automation-hub-galaxy-api -o custom-columns=POD:.metadata.name --no-headers | head -1)
+
+echo "Fixing keycloak user permissions"
 oc exec -i $AH_API_POD /entrypoint.sh manage shell < dev/ephemeral/fixuser.py
 
 # What is the username?
@@ -21,6 +25,7 @@ export HUB_API_ROOT="https://front-end-aggregator-${NAMESPACE}.apps.c-rh-c-eph.8
 echo "HUB_API_ROOT: ${HUB_API_ROOT}"
 
 
+echo "Starting sleep cycle ..."
 for X in $(seq 1500 -1 0); do
     echo "SLEEP ${X}"
     sleep 1
