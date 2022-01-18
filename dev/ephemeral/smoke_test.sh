d7d870fae7e253103304838d9421e96fc9dbdadd
James Tanner <tanner.jc@gmail.com>
2022-01-18 16:53:35 -0500

    Test test test
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/ephemeral/smoke_test.sh
+++ dev/ephemeral/smoke_test.sh
@@ -36,7 +36,7 @@ ${VENV_PATH}/bin/pip install --upgrade pip wheel
 ${VENV_PATH}/bin/pip install -r galaxy_ng/tests/integration/requirements.txt
 
 echo "Running pytest ..."
-${VENV_PATH}/bin/pytest --capture=no -m "not standalone_only" -v galaxy_ng/tests/integration
+${VENV_PATH}/bin/pytest --capture=no -m "not standalone_only" -v galaxy_ng/tests/integration || echo "TESTS FAILED!!!"
 
 
 echo ""
