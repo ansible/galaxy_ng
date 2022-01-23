5798ba9f8cb8b0fbe4a12b34c005c61e56e9fcc5
James Tanner <tanner.jc@gmail.com>
2022-01-23 13:24:10 -0500

    remove sleep and capture exit code
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/ephemeral/smoke_test.sh
+++ dev/ephemeral/smoke_test.sh
@@ -50,8 +50,7 @@ ${VENV_PATH}/bin/pip install --upgrade pip wheel
 ${VENV_PATH}/bin/pip install -r integration_requirements.txt
 
 echo "Running pytest ..."
-${VENV_PATH}/bin/pytest --capture=no -m "not standalone_only" -v galaxy_ng/tests/integration || echo "TESTS FAILED!!!"
-
+${VENV_PATH}/bin/pytest --capture=no -m "not standalone_only" -v galaxy_ng/tests/integration
 
 #echo ""
 #echo "##################################################"
@@ -60,8 +59,8 @@ ${VENV_PATH}/bin/pytest --capture=no -m "not standalone_only" -v galaxy_ng/tests
 #echo ""
 #oc logs $AH_API_POD
 
-echo "Starting sleep cycle for 10000s... "
-for X in $(seq 10000 -1 0); do
-    #echo "SLEEP ${X}"
-    sleep 1
-done
+#echo "Starting sleep cycle for 10000s... "
+#for X in $(seq 10000 -1 0); do
+#    #echo "SLEEP ${X}"
+#    sleep 1
+#done
