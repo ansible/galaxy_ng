7898197dc38fd52e870ad352d96de8ffa641a08e
James Tanner <tanner.jc@gmail.com>
2022-01-25 11:42:09 -0500

    Set content origin
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -24,6 +24,7 @@ source "$CICD_ROOT/build.sh"
 # NOTE(cutwater): See https://issues.redhat.com/browse/RHCLOUD-14977
 source ${CICD_ROOT}/_common_deploy_logic.sh
 export NAMESPACE=$(bonfire namespace reserve)
+export CONTENT_ORIGIN="https://front-end-aggregator-${NAMESPACE}.apps.c-rh-c-eph.8p0c.p1.openshiftapps.com"
 bonfire deploy \
     ${APP_NAME} \
     --source=appsre \
@@ -34,7 +35,8 @@ bonfire deploy \
     --timeout ${DEPLOY_TIMEOUT} \
     ${COMPONENTS_ARG} \
     ${COMPONENTS_RESOURCES_ARG} \
-    --set-parameter ${COMPONENT_NAME}/IMPORTER_JOB_NAMESPACE=${NAMESPACE}
+    --set-parameter ${COMPONENT_NAME}/IMPORTER_JOB_NAMESPACE=${NAMESPACE} \
+    --set-parameter ${COMPONENT_NAME}/CONTENT_ORIGIN="${CONTENT_ORIGIN}"
 # END WORKAROUND
 
 # source $CICD_ROOT/smoke_test.sh
