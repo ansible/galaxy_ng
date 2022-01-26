cfab784a92e878eb4de57aebd8f92579b0aa4bf5
James Tanner <tanner.jc@gmail.com>
2022-01-26 11:34:12 -0500

    NEED MORE INPUT
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/v3/views/server_info.py
+++ galaxy_ng/app/api/v3/views/server_info.py
@@ -4,13 +4,19 @@ import os
 from django.http import HttpResponse
 from django.conf import settings
 
+from galaxy_ng.app.constants import DeploymentMode
+
+
 def server_info(*args, **kwargs):
     ds = {
         'settings.ANSIBLE_API_HOSTNAME': settings.ANSIBLE_API_HOSTNAME,
         'settings.GALAXY_API_PATH_PREFIX': settings.GALAXY_API_PATH_PREFIX,
         'settings.X_PULP_CONTENT_HOST': settings.X_PULP_CONTENT_HOST,
         'settings.X_PULP_CONTENT_PORT': settings.X_PULP_CONTENT_PORT,
-        'settings.CONTENT_PATH_PREFIX': settings.CONTENT_PATH_PREFIX
+        'settings.CONTENT_PATH_PREFIX': settings.CONTENT_PATH_PREFIX,
+        'settings.GALAXY_DEPLOYMENT_MODE': settings.GALAXY_DEPLOYMENT_MODE,
+        'DeploymentMode.INSIGHTS.value': DeploymentMode.INSIGHTS.value,
+        'settings.GALAXY_DEPLOYMENT_MODE == DeploymentMode.INSIGHTS.value': settings.GALAXY_DEPLOYMENT_MODE == DeploymentMode.INSIGHTS.value
     }
 
     for k, v in os.environ.items():
