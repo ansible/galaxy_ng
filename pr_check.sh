e6676b8ad1c0779c4c21dfe970caf4214bb9caac
James Tanner <tanner.jc@gmail.com>
2022-01-26 14:51:56 -0500

    Configure minio routes and settings for ephemeral stacks
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -24,6 +24,7 @@ source "$CICD_ROOT/build.sh"
 # NOTE(cutwater): See https://issues.redhat.com/browse/RHCLOUD-14977
 source ${CICD_ROOT}/_common_deploy_logic.sh
 export NAMESPACE=$(bonfire namespace reserve)
+export CONTENT_ORIGIN="https://front-end-aggregator-${NAMESPACE}.apps.c-rh-c-eph.8p0c.p1.openshiftapps.com"
 bonfire deploy \
     ${APP_NAME} \
     --source=appsre \
@@ -34,8 +35,21 @@ bonfire deploy \
     --timeout ${DEPLOY_TIMEOUT} \
     ${COMPONENTS_ARG} \
     ${COMPONENTS_RESOURCES_ARG} \
-    --set-parameter ${COMPONENT_NAME}/IMPORTER_JOB_NAMESPACE=${NAMESPACE}
+    --set-parameter ${COMPONENT_NAME}/IMPORTER_JOB_NAMESPACE=${NAMESPACE} \
+    --set-parameter ${COMPONENT_NAME}/CONTENT_ORIGIN="${CONTENT_ORIGIN}"
 # END WORKAROUND
+
+# Fix the routing for minio and artifact urls
+oc project ${NAMESPACE}
+oc create route edge minio --service=env-${NAMESPACE}-minio --insecure-policy=Redirect
+MINIO_ROUTE=$(oc get route minio -o jsonpath='https://{.spec.host}{"\n"}')
+oc patch clowdapp automation-hub \
+    --type=json \
+    -p '[{"op": "add", "path": "/spec/deployments/2/podSpec/env/-", "value": {
+                "name": "PULP_AWS_S3_ENDPOINT_URL",
+                "value": "'"${MINIO_ROUTE}"'"
+            }
+        }]'
 
 # source $CICD_ROOT/smoke_test.sh
 
