ffd37d6767c02bd3d41885d10fc7569f2c8bf3ed
James Tanner <tanner.jc@gmail.com>
2022-02-08 13:43:58 -0500

    Add integration tests for collection[verison] deletion.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/conftest.py
+++ galaxy_ng/tests/integration/conftest.py
@@ -27,6 +27,9 @@ stage_health: For checking stage?
 namespace: Tests that manipulate namespaces.
 certification: Related to certification.
 collection_move: Related to moving collection versions.
+collection_delete: Deletes collections.
+collection_version_delete: Deletes collections versions.
+delete: Tests for deleting objects.
 move: Related to the move api.
 """
 
