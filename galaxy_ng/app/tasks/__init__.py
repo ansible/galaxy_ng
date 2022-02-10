0608454e7f0b31c6527f988ea293a580a9520a66
Andrew Crosby <acrosby@redhat.com>
2022-02-10 10:52:50 -0500

    Combine the copy and remove tasks (#1122)
    Ensure copy and remove tasks are run in sequence by combining from 2
    tasks into 1, where they share the same locks. Avoids scenario where 1
    task can fail and the other can succeed.
    
    Issue: AAH-1349

--- galaxy_ng/app/tasks/__init__.py
+++ galaxy_ng/app/tasks/__init__.py
@@ -1,8 +1,8 @@
-from .registry_sync import launch_container_remote_sync, sync_all_repos_in_registry  # noqa: F401
 from .deletion import delete_collection, delete_collection_version  # noqa: F401
-from .promotion import call_copy_task, call_remove_task  # noqa: F401
-from .publishing import import_and_auto_approve, import_and_move_to_staging  # noqa: F401
-from .synclist import curate_all_synclist_repository, curate_synclist_repository  # noqa: F401
 from .index_registry import index_execution_environments_from_redhat_registry  # noqa: F401
+from .promotion import call_move_content_task  # noqa: F401
+from .publishing import import_and_auto_approve, import_and_move_to_staging  # noqa: F401
+from .registry_sync import launch_container_remote_sync, sync_all_repos_in_registry  # noqa: F401
+from .synclist import curate_all_synclist_repository, curate_synclist_repository  # noqa: F401
 
 # from .synchronizing import synchronize  # noqa
