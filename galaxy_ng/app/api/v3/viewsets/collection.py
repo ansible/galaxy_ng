96aca17b17969a1f8ecf38bae063b3ab3442efa0
Andrew Crosby <acrosby@redhat.com>
2022-02-16 12:58:09 -0500

    Attempt to use CONTENT_ORIGIN+redirect in eph env

--- galaxy_ng/app/api/v3/viewsets/collection.py
+++ galaxy_ng/app/api/v3/viewsets/collection.py
@@ -371,6 +371,15 @@ class CollectionArtifactDownloadView(api_base.APIView):
         prefix = settings.CONTENT_PATH_PREFIX.strip('/')
         distribution = self._get_ansible_distribution(self.kwargs['path'])
 
+        # NOTE: attempt to use CONTENT_ORIGIN+redirect in eph env
+        url = '{host}/{prefix}/{distro_base_path}/{filename}'.format(
+            host=settings.CONTENT_ORIGIN.strip("/"),
+            prefix=prefix,
+            distro_base_path=distro_base_path,
+            filename=filename,
+        )
+        return redirect(distribution.content_guard.cast().preauthenticate_url(url))
+
         if settings.GALAXY_DEPLOYMENT_MODE == DeploymentMode.INSIGHTS.value:
             url = 'http://{host}:{port}/{prefix}/{distro_base_path}/{filename}'.format(
                 host=settings.X_PULP_CONTENT_HOST,
