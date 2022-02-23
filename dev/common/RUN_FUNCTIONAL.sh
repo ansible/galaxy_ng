38e3cca00b76c2bd696c56f4c6b612b6598b0c08
James Tanner <tanner.jc@gmail.com>
2022-02-22 22:36:25 -0500

    Add a simple wrapper script to run functional tests on an ubuntu vagrant box
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/common/RUN_FUNCTIONAL.sh
+++ dev/common/RUN_FUNCTIONAL.sh
@@ -1,7 +1,38 @@
 #!/bin/bash -x
 
-set -e
+##################################################
+#   HOW TO USE ...
+##################################################
 
+# These tests expect to be run from inside the vagrant box 
+# configuration shown below or on a raw ubuntu 2004 machine.
+
+VAGRANTFILE=$(cat << EOF
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "generic/ubuntu2004"
+  config.vm.synced_folder ".", "/app", type: "nfs", nfs_udp: false
+
+  config.vm.provider :libvirt do |libvirt|
+    libvirt.cpus = 4
+    libvirt.memory = 8092
+  end
+
+end
+EOF
+)
+
+# 1) mount or copy the galaxy_ng checkout to /app/galaxy_ng
+# 2) cd /app/galaxy_ng
+# 3) sudo dev/common/RUN_FUNCTIONAL.sh
+
+##################################################
+#   BEGIN TESTS ...
+##################################################
+
+set -e
 
 export TEST="pulp"
 export PY_COLORS=1
@@ -21,6 +52,17 @@ export APP_PATH=/app
 export GALAXY_PATH=$APP_PATH/galaxy_ng
 export DEBIAN_FRONTEND=noninteractive
 
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+if [[ "$(cat /etc/issue)" != "Ubuntu 20.04"* ]]; then
+  echo "Please run this on an Ubuntu 20.04 system"
+  exit 1
+fi
+
 apt -y update
 apt -y install git jq python3-pip docker.io libpq-dev
 RC=$?
@@ -28,6 +70,14 @@ if [[ $RC != 0 ]]; then
     exit $RC
 fi
 
+# Make sure docker was installed and running ...
+docker ps -a
+RC=$?
+if [[ $RC != 0 ]]; then
+    exit $RC
+fi
+
+# /usr/bin/python is required for most things to work ...
 if [[ ! -f /usr/bin/python3 ]]; then
     echo "python3 not found"
     exit 1
@@ -37,6 +87,7 @@ if [ ! -L /usr/local/bin/python ]; then
 fi
 which python || (echo "python symlinking did not work" && exit 1)
 
+# Clean up any previously created checkouts or the scripts will abort.
 if [ -d $APP_PATH/pulp-smash ]; then
     rm -rf $APP_PATH/pulp-smash
 fi
@@ -55,12 +106,6 @@ fi
 if [ -d $APP_PATH/galaxy-importer ]; then
     rm -rf $APP_PATH/galaxy-importer
 fi
-
-#which docker
-#docker ps -a
-#exit 0
-
-#git clone https://github.com/pulp/pulp-openapi-generator /pulp-openapi-generator
 
 pip install -U pip wheel
 pip install httpie
@@ -106,6 +151,12 @@ RC=$?
 if [[ $RC != 0 ]]; then
     exit $RC
 fi
+
+echo "------------------------------------------- get registry cert"
+rm -rf /etc/docker/certs.d/pulp*
+mkdir -p /etc/docker/certs.d/pulp:443
+openssl s_client -connect pulp:443 2>/dev/null </dev/null |  \
+    sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /etc/docker/certs.d/pulp\:443/ca.crt
 
 echo "------------------------------------------- script.sh"
 bash -x $GALAXY_PATH/.github/workflows/scripts/script.sh
