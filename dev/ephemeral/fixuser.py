83f77f3f0e22b88189f78c023ff7fd227422c992
Andrew Crosby <acrosby@redhat.com>
2022-03-18 15:30:31 -0400

    Remove escalated privledges from integration test user
    Issue: AAH-1449

--- dev/ephemeral/fixuser.py
+++ dev/ephemeral/fixuser.py
@@ -1,6 +1,9 @@
-from galaxy_ng.app.models.auth import User
+from galaxy_ng.app.models.auth import Group, User
+
+pe_group = Group.objects.get(name="system:partner-engineers")
 
 user = User.objects.create_user('jdoe', password='bar')
-user.is_superuser = True
-user.is_staff = True
+# user.is_superuser = True
+# user.is_staff = True
+user.groups.add(pe_group)
 user.save()
