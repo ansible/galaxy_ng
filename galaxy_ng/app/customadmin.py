b37dbf6c2513ba96e247483bdc209ae114f8b9c3
Bruno Rocha <rochacbruno@users.noreply.github.com>
2022-03-29 15:02:45 +0100

    Remove customadmin (#1184)
    Issue: AAH-1478

--- galaxy_ng/app/customadmin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from pulpcore.app.admin import TaskAdmin
-from pulpcore.plugin.models import Task
-
-
-site = admin.AdminSite(name="galaxy-admin")
-site.register(Task, TaskAdmin)
