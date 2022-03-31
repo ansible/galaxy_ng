a5f59be598a6d67fac553b43095c83cf2a55bfeb
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-03-31 21:52:52 +0100

    Bump pulp_ansible requirement
    No-Issue

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ requirements = [
     "Django~=2.2.23",
     "galaxy-importer==0.3.4",
     "pulpcore<3.12,>=3.11.2",
-    "pulp-ansible==0.7.4",
+    "pulp-ansible==0.7.5",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
     # pulp-container 2.6 requires pulpcore >=3.12.1
