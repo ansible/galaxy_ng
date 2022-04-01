f5dda3492245ac11e0b94cc4a4ac25399abe43a8
David Newswanger <dnewswan@redhat.com>
2022-04-01 14:25:07 -0400

    Fix requirements

--- setup.py
+++ setup.py
@@ -90,8 +90,7 @@ class BuildPyCommand(_BuildPyCommand):
 requirements = [
     "galaxy-importer==0.4.2",
     "pulpcore>=3.18.1,<3.19.0",
-    # "pulp-ansible>=0.13.0",
-    "pulp-ansible @ git+https://github.com/pulp/pulp_ansible@main#egg=pulp-ansible",  # noqa E501
+    "pulp-ansible@git+https://github.com/pulp/pulp_ansible@main#egg=pulp-ansible",  # noqa E501
     "django-prometheus>=2.0.0",
     "drf-spectacular",
     "pulp-container>=2.10.2,<2.11.0",
