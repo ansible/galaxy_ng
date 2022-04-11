550efdbf69b06d825771a0ac578e6ed277bac1b9
David Newswanger <dnewswan@redhat.com>
2022-04-11 14:18:54 -0400

    Revert RBAC changes.
    Revert "Create galaxy_ng specific Roles (#1058)"
    
    This reverts commit 3fefab0d379a5710a235e482c60567d24c85628d.
    
    No-Issue

--- galaxy_ng/tests/integration/conftest.py
+++ galaxy_ng/tests/integration/conftest.py
@@ -35,7 +35,6 @@ move: Related to the move api.
 synclist: Related to synclist object and synclist repo.
 openapi: Checks the openapi schema and routes.
 openapi_generate_bindings: Verifies pulp client bindings generator
-role: Related to RBAC Roles
 """
 
 
