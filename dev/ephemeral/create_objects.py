959cc305b1f314dee52ff9c23f93cff215dd2e63
James Tanner <tanner.jc@gmail.com>
2022-04-25 11:08:50 -0400

    Add suggestions
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/ephemeral/create_objects.py
+++ dev/ephemeral/create_objects.py
@@ -2,7 +2,4 @@ from galaxy_ng.app.models import Namespace
 
 print('creating autohubtest2+3 namespace(s)')
 for nsname in ['autohubtest2', 'autohubtest3']:
-    ns = Namespace.objects.filter(name=nsname).first()
-    if not ns:
-        ns = Namespace.objects.create(name=nsname)
-        ns.save()
+    ns = Namespace.objects.get_or_create(name=nsname)
--- dev/ephemeral/fixuser.py
+++ dev/ephemeral/fixuser.py
@@ -1,9 +1,12 @@
 from galaxy_ng.app.models.auth import User
 
-user = User.objects.filter(username='jdoe').first()
-if user is None:
-    user = User.objects.create_user('jdoe', password='bar')
-
+user = User.objects.get_or_create(
+    username='jdoe',
+    defaults=dict(
+        username='jdoe',
+        password='bar'
+    )
+)
 user.is_superuser = True
 user.is_staff = True
 user.save()
