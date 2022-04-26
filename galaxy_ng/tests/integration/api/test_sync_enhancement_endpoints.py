210c88209a78c6552c78c5d687a78ad34b5631ec
David Newswanger <dnewswan@redhat.com>
2022-04-26 12:50:17 -0400

    Fix not found on content/ urls

--- galaxy_ng/tests/integration/api/test_sync_enhancement_endpoints.py
+++ galaxy_ng/tests/integration/api/test_sync_enhancement_endpoints.py
@@ -15,7 +15,6 @@ def test_pulp_sync_enhancement_endpoints(ansible_config):
 
     # verify that the repo metadate endpoint works
     results = client("/api/automation-hub/v3/")
-
     assert "published" in results
 
     # verify that the unpaginated endpoints are disabled
@@ -25,4 +24,13 @@ def test_pulp_sync_enhancement_endpoints(ansible_config):
 
     with pytest.raises(GalaxyError) as ctx:
         client("/api/automation-hub/v3/collection_versions/all/", method="GET")
+    assert ctx.value.http_code == 404
+
+    # verify that the content/ prefix works correctly unpaginated endpoints are disabled
+    with pytest.raises(GalaxyError) as ctx:
+        client("/api/automation-hub/content/published/v3/collections/all/", method="GET")
+    assert ctx.value.http_code == 404
+
+    with pytest.raises(GalaxyError) as ctx:
+        client("/api/automation-hub/content/published/v3/collection_versions/all/", method="GET")
     assert ctx.value.http_code == 404
