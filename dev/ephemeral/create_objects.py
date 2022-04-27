af98960f922525c000150c3a607e4fc9fe0ade0d
James Tanner <tanner.jc@gmail.com>
2022-04-27 09:42:31 -0400

    Make the ephemeral django scripts idempotent.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/ephemeral/create_objects.py
+++ dev/ephemeral/create_objects.py
@@ -2,5 +2,4 @@ from galaxy_ng.app.models import Namespace
 
 print('creating autohubtest2+3 namespace(s)')
 for nsname in ['autohubtest2', 'autohubtest3']:
-    ns = Namespace.objects.create(name=nsname)
-    ns.save()
+    ns, _ = Namespace.objects.get_or_create(name=nsname)
