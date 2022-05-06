fe8ea8f1082b563558c279bf826aeecb97f4bb69
Andrew Crosby <acrosby@redhat.com>
2022-05-05 20:53:57 -0400

    Remove mgmt cmd --point-each-to-synclist-repo option as it changes all

--- galaxy_ng/app/management/commands/update-synclist-distros.py
+++ galaxy_ng/app/management/commands/update-synclist-distros.py
@@ -8,67 +8,25 @@ log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    """This command modifies AnsibleDistributions in format of #####-synclists
-    to change which repository they point to.
+    """This command updates all AnsibleDistribution in the format of #####-synclists
+    to point to the published repo.
 
     Example:
-
-    django-admin update-synclist-distros --point-each-to-default-repo
-    django-admin update-synclist-distros --point-each-to-synclist-repo
+    django-admin update-synclist-distros
     """
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--point-each-to-default-repo",
-            action="store_true",
-            help="Update all AnsibleDistribution in format of #####-synclists "
-            "to point to published repo",
-            default=False,
-            required=False,
-        )
-        parser.add_argument(
-            "--point-each-to-synclist-repo",
-            action="store_true",
-            help="Update all AnsibleDistribution in format of #####-synclists "
-            "to point to repo of the same name ('12345-synclist' for example)",
-            default=False,
-            required=False,
-        )
-
     def handle(self, *args, **options):
-        log.debug("options: %s", repr(options))
+        log.info(
+            "Updating all AnsibleDistribution in the format of #####-synclists "
+            "to point to the published repo"
+        )
 
-        if options["point_each_to_default_repo"]:
-            log.info(
-                "Updating all AnsibleDistribution in format of #####-synclists "
-                "to point to published repo"
-            )
+        published_repo = AnsibleRepository.objects.get(name="published")
 
-            published_repo = AnsibleRepository.objects.get(name="published")
-
-            with transaction.atomic():
-                synclist_distros = AnsibleDistribution.objects.filter(
-                    base_path__endswith="-synclist"
-                )
-                for distro in synclist_distros:
-                    log.debug("distro: %s", distro)
+        with transaction.atomic():
+            synclist_distros = AnsibleDistribution.objects.filter(base_path__endswith="-synclist")
+            for distro in synclist_distros:
+                if distro.repository.pk != published_repo.pk:
                     distro.repository = published_repo
                     distro.save()
-
-        if options["point_each_to_synclist_repo"]:
-            log.info(
-                "Updating all AnsibleDistribution in format of #####-synclists "
-                "to point to repo of the same name"
-            )
-
-            with transaction.atomic():
-                synclist_distros = AnsibleDistribution.objects.filter(
-                    base_path__endswith="-synclist"
-                )
-                for distro in synclist_distros:
-                    log.debug("distro: %s", distro)
-                    synclist_repo, _ = AnsibleRepository.objects.get_or_create(
-                        name=distro.base_path
-                    )
-                    distro.repository = synclist_repo
-                    distro.save()
+                    log.info("distro edited: %s", distro.name)
