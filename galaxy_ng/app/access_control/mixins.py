3bb5e274d8dc80f0de6abb97c1510404dca09ec2
David Newswanger <dnewswan@redhat.com>
2022-05-16 14:30:42 -0400

    Fix groups on namespaces and my namespaces.
    No-Issue

--- galaxy_ng/app/access_control/mixins.py
+++ galaxy_ng/app/access_control/mixins.py
@@ -1,16 +1,44 @@
+from collections import defaultdict
+
 from django.db import transaction
 from django.core.exceptions import BadRequest
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 
 from rest_framework.exceptions import ValidationError
 
 from pulpcore.plugin.util import (
     assign_role,
     remove_role,
-    get_groups_with_perms_attached_roles
+    # get_groups_with_perms_attached_roles,
 )
 
+from pulpcore.plugin.models.role import GroupRole
+
 from django_lifecycle import hook
+
+
+# TODO: Replace this with the version from pulpcore once 
+# https://github.com/pulp/pulpcore/pull/2728 is merged and released
+def get_groups_with_perms_attached_roles(obj, only_with_perms_in=None):
+    ctype = ContentType.objects.get_for_model(obj)
+    perms = Permission.objects.filter(content_type__pk=ctype.id)
+    if only_with_perms_in:
+        codenames = [
+            split_perm[-1]
+            for split_perm in (perm.split(".", maxsplit=1) for perm in only_with_perms_in)
+            if len(split_perm) == 1 or split_perm[0] == ctype.app_label
+        ]
+        perms = perms.filter(codename__in=codenames)
+    group_roles = GroupRole.objects.filter(role__permissions__in=perms).filter(
+        Q(content_type=ctype, object_id=obj.pk)
+    )
+    res = defaultdict(set)
+    for group_role in group_roles:
+        res[group_role.group].add(group_role.role.name)
+    return {k: list(v) for k, v in res.items()}
 
 
 class GroupModelPermissionsMixin:
