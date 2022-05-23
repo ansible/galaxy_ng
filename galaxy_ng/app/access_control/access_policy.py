312ac2f77cb40b7086e5d82445314d26331812a6
Brian McLaughlin <bmclaugh@redhat.com>
2022-05-23 11:17:40 -0400

    Update pulp_container to 2.12 (#1272)
    Update pulp_container to 2.12.0, pulpcore to 3.19, update CI

--- galaxy_ng/app/access_control/access_policy.py
+++ galaxy_ng/app/access_control/access_policy.py
@@ -7,7 +7,6 @@ from rest_framework.exceptions import NotFound
 
 from pulpcore.plugin.access_policy import AccessPolicyFromDB
 
-from pulp_container.app.access_policy import NamespacedAccessPolicyMixin
 from pulp_container.app import models as container_models
 
 from galaxy_ng.app import models
@@ -280,8 +279,43 @@ class ContainerRegistryRemoteAccessPolicy(AccessPolicyBase):
     NAME = "ContainerRegistryRemoteViewSet"
 
 
-class ContainerRemoteAccessPolicy(AccessPolicyBase, NamespacedAccessPolicyMixin):
+class ContainerRemoteAccessPolicy(AccessPolicyBase):
     NAME = "ContainerRemoteViewSet"
+
+    # Copied from pulp_container/app/global_access_conditions.py
+    def has_namespace_or_obj_perms(self, request, view, action, permission):
+        """
+        Check if a user has a namespace-level perms or object-level permission
+        """
+        ns_perm = "container.namespace_{}".format(permission.split(".", 1)[1])
+        if self.has_namespace_obj_perms(request, view, action, ns_perm):
+            return True
+        else:
+            return request.user.has_perm(permission) or request.user.has_perm(
+                permission, view.get_object()
+            )
+
+    # Copied from pulp_container/app/global_access_conditions.py
+    def has_namespace_obj_perms(self, request, view, action, permission):
+        """
+        Check if a user has object-level perms on the namespace associated with the distribution
+        or repository.
+        """
+        if request.user.has_perm(permission):
+            return True
+        obj = view.get_object()
+        if type(obj) == container_models.ContainerDistribution:
+            namespace = obj.namespace
+            return request.user.has_perm(permission, namespace)
+        elif type(obj) == container_models.ContainerPushRepository:
+            for dist in obj.distributions.all():
+                if request.user.has_perm(permission, dist.cast().namespace):
+                    return True
+        elif type(obj) == container_models.ContainerPushRepositoryVersion:
+            for dist in obj.repository.distributions.all():
+                if request.user.has_perm(permission, dist.cast().namespace):
+                    return True
+        return False
 
     def has_distro_permission(self, request, view, action, permission):
         class FakeView:
