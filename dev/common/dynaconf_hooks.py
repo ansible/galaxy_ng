9ca07c0fda4c639b5f2afa001436e5aaf3404ca1
Bruno Rocha <rochacbruno@users.noreply.github.com>
2022-05-31 16:44:51 +0100

    [dev env] Reload API when .compose.env is changed (#1284)
    When developer makes changes on .compose.env or dev/common/settings.py
    the API container reloads the gunicorn process and settings are
    reflected imeddiatelly.
    
    No-Issue

---
+++ dev/common/dynaconf_hooks.py
@@ -0,0 +1,18 @@
+from dynaconf.utils.parse_conf import parse_conf_data
+from dynaconf.vendor.dotenv import dotenv_values
+
+
+def post(settings):
+    """This is file is meant to be used only on dev environment
+
+    The main goal is to:
+    - load `.compose.env` file values and override the settings with those values
+      whenever the application restarts
+    - The docker API container is set to restart when .compose.env changes.
+    """
+    data = {
+        k[5:]: parse_conf_data(v, tomlfy=True, box_settings=settings)
+        for k, v in dotenv_values("/src/galaxy_ng/.compose.env").items()
+        if k.startswith("PULP_")
+    }
+    return data
