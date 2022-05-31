fd0cbe4bf30bcb00ff33926784241d667c1a939b
David Newswanger <dnewswan@redhat.com>
2022-05-31 15:09:03 -0400

    Add a rediirect for the /pulp/ api endpoints when they are not set to the default.
    Issue: AAH-1646

--- galaxy_ng/app/views.py
+++ galaxy_ng/app/views.py
@@ -1,5 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.conf import settings
+from galaxy_ng.app.api import base as api_base
 
 
 def health_view(request):
     return HttpResponse('OK')
+
+
+class HttpResponsePermanentRedirect308(HttpResponsePermanentRedirect):
+    status_code = 308
+
+
+class PulpAPIRedirectView(api_base.APIView):
+    permission_classes = []
+
+    def get(self, request, **kwargs):
+        api_path = kwargs.get("api_path", None)
+
+        url = f"/{settings.API_ROOT.strip('/')}/api/v3/"
+
+        if api_path:
+            url = f"{url}{api_path.strip('/')}/"
+
+        args = request.META.get("QUERY_STRING", "")
+        if args:
+            url = "%s?%s" % (url, args)
+
+        # Returning 308 instead of 302 since 308 requires that clients maintain the
+        # same method as the original request.
+        return HttpResponsePermanentRedirect308(url)
---
+++ galaxy_ng/tests/integration/api/test_pulp_redirect.py
@@ -0,0 +1,31 @@
+from ..utils import get_client
+
+
+def test_pul_api_redirect(ansible_config, artifact):
+    """Test that /pulp/ is redirecting to /api/galaxy/pulp/"""
+
+    config = ansible_config("ansible_partner")
+
+    api_client = get_client(
+        config=config,
+        request_token=True,
+        require_auth=True
+    )
+
+    # verify api root works
+    response = api_client('/pulp/api/v3/')
+    assert "users" in response
+
+    # verify a couple of different paths work
+    response = api_client('/pulp/api/v3/status/')
+    assert "versions" in response
+
+    response = api_client('/pulp/api/v3/distributions/ansible/ansible/')
+    assert response["count"] > 0
+
+    # verify query params work
+    response = api_client('/pulp/api/v3/distributions/ansible/ansible/?name=published')
+    assert response["count"] == 1
+
+    # verify the hrefs are not returning the old url
+    assert not response["results"][0]["pulp_href"].startswith("/pulp/")
