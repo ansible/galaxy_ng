7ac013e56414a05f682aa6bf20f9e320b9db9275
Andrew Crosby <acrosby@redhat.com>
2022-06-16 14:11:06 -0400

    Use v3/excludes to exclude content from sync (#1311)
    Issue: AAH-1583

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -29,7 +29,7 @@ TAG=ci_build
 if [ -e $REPO_ROOT/../pulp_ansible ]; then
   PULP_ANSIBLE=./pulp_ansible
 else
-  PULP_ANSIBLE=git+https://github.com/pulp/pulp_ansible.git@0.10.2
+  PULP_ANSIBLE=git+https://github.com/pulp/pulp_ansible.git@0.10.3
 fi
 
 if [ -e $REPO_ROOT/../pulp_container ]; then
