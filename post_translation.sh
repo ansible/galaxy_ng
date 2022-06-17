c90ccb9fe285e7789cb5e6768289324a1d014837
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-17 17:57:47 -0400

    Create post_translation.sh

---
+++ post_translation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd _clones/pinakes/
+
+# Rename the zh_cn folder 
+mv translations/zh_cn translations/zh
+
+# Create a directory for api (locale)
+# rm -rf locale
+mkdir locale
+
+# Copy all subdirectories to locale
+cp -r translations/ locale/
+
+# Loop over each directory and create another directory LC_Messages
+# Move django.po files to LC_Messages and remove messages.po
+cd locale/
+for d in */ ; do
+    dir=${d%*/}
+    mkdir $dir/LC_MESSAGES
+    mv $dir/django.po $dir/LC_MESSAGES/
+done
+
+cd ..
+# echo $(pwd)
+
+
+# echo $pwd
+
+# cd to repository
+
+# cd _clones/
+
+galaxy_ng_api_path="galaxy_ng/locale" # locale will be dropped here
+
+rsync -av locale/ $galaxy_ng_api_path
+
+rm -rf translations/
+rm -rf locale/
