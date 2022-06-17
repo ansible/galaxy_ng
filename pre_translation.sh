76f93d63a1355a85e341696c8ce47bce2f813f47
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 23:23:40 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -9,9 +9,9 @@ cp .compose.env.example .compose.env
 ./compose run --rm --user=0 \
  -v $(pwd)/_translations:/translations \
  api \
- bash -c "cd /app/galaxy_ng/ && django-admin makemessages -l en_us --keep-pot"
+ bash -c "cd galaxy_ng/ && django-admin makemessages -l en_us --keep-pot"
  
 cd ..
 
 # Move files to Translations folder
-mv app/galaxy_ng/locale/django.pot translations/
+mv galaxy_ng/locale/django.pot translations/
