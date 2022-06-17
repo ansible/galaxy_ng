d82e57a081098a8e96afb2104ce38c9b320f1a47
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 21:14:14 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -3,14 +3,10 @@ cd _clones
 
 # Extract Strings from UI
 ./compose run --rm --user=0 \
--v _translations:/translations \
-ui \
 sh -c "npm run gettext:extract && cp /hub/app/locale/en.po translations/en.po"
 
 # Extract Strings from API
  ./compose run --rm --user=0 \
--v _translations:/translations \
-api \
 bash -c "cd /app/galaxy_ng && django-admin makemessages -l en_us --keep-pot && cp /app/galaxy_ng/locale/django.pot translations/django.po"
 
 # Terminate docker
