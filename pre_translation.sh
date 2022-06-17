e8f0e2ce795b4fd980d97572253ea4a8a07487b7
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 23:07:49 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -1,12 +1,17 @@
 # Change Directory to clones
 cd _clones
 
-# Extract Strings from UI
-./compose run --rm --user=0 \
-sh -c "npm run gettext:extract && cp /hub/app/locale/en.po translations/en.po"
+# Build Hub Development Environment
+cp .compose.env.example .compose.env
+./compose build
 
 # Extract Strings from API
- ./compose run --rm --user=0 \
-bash -c "cd /app/galaxy_ng && django-admin makemessages -l en_us --keep-pot && cp /app/galaxy_ng/locale/django.pot translations/django.po"
+./compose run --rm --user=0 \
+ -v _translations:/translations \
+ api \
+ bash -c "cd galaxy_ng/ && django-admin makemessages -l en_us --keep-pot"
+ 
+cd ..
 
-# Terminate docker
+# Move files to Translations folder
+mv galaxy_ng/locale/django.pot translations/
