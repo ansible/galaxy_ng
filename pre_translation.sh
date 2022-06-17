d5b562c2e6bffaf86f8d0ae0d493c79f398e0c84
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-17 05:25:29 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -9,7 +9,7 @@ cp .compose.env.example .compose.env
 ./compose run --rm --user=0 \
  -v $(pwd)/translations:/translations \
  api \
- bash -c "cd galaxy_ng/ && django-admin makemessages -l en_us --keep-pot"
+ bash -c "django-admin makemessages -l en_us --keep-pot"
  
 # cd ..
 
