959ca76169c68164abe8aaab05dfcabc73c0d6cc
jctanner <tanner.jc@gmail.com>
2022-07-14 16:05:37 -0400

    Flask proxy insights (#1369)
    Add a proxy to simulate the insights platform for the local compose stack.
    Add a github action workflow to test insights mode with the proxy.
    
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

---
+++ dev/insights/RUN_INTEGRATION.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Expected to be called by:
+# - GitHub Actions ci_standalone.yml for DeploymentMode.STANDALONE
+# - Developer Env makefile commands for DeploymentMode.STANDALONE
+# - TODO: Ephemeral Env pr_check.sh (merge smoke_test.sh into this) for DeploymentMode.INSIGHTS
+
+set -e
+
+export HUB_API_ROOT="http://localhost:8080/api/automation-hub/"
+export HUB_AUTH_URL="http://localhost:8080/auth/realms/redhat-external/protocol/openid-connect/token"
+export HUB_USE_MOVE_ENDPOINT="true"
+unset HUB_TOKEN
+
+which virtualenv || pip install --user virtualenv
+
+VENVPATH=/tmp/gng_testing
+PIP=${VENVPATH}/bin/pip
+
+if [[ ! -d $VENVPATH ]]; then
+    virtualenv $VENVPATH
+    $PIP install --retries=0 --verbose --upgrade pip wheel
+fi
+source $VENVPATH/bin/activate
+echo "PYTHON: $(which python)"
+
+pip install -r integration_requirements.txt
+pip show epdb || pip install epdb
+
+# when running user can specify extra pytest arguments such as
+# export HUB_LOCAL=1
+# dev/common/RUN_INTEGRATION.sh --pdb -sv --log-cli-level=DEBUG "-m standalone_only" -k mytest
+
+pytest --capture=no --tb=short -m "not standalone_only" $@ -v galaxy_ng/tests/integration
+RC=$?
+
+exit $RC
