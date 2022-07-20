4e30ef17db18028508984a98d3db773925308483
Andrew Crosby <acrosby@redhat.com>
2022-07-20 15:56:33 -0400

    Update pulp dependencies from commit hashes to pinned versions
    No-Issue

--- setup.py
+++ setup.py
@@ -110,11 +110,11 @@ def _format_pulp_requirement(plugin, specifier=None, ref=None, gh_namespace="pul
 
 requirements = [
     "galaxy-importer==0.4.5",
-    _format_pulp_requirement("pulpcore", ref="6e44fb2fe609f92dc1f502b19c67abd08879148f"),
-    _format_pulp_requirement("pulp-ansible", ref="06a7cc92876eec90bd79b1fffed794dda60a7333"),
+    "pulpcore==3.20.0",
+    "pulp-ansible==0.14.0",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
-    _format_pulp_requirement("pulp-container", ref="20bec438da54ddd6da4102f046f12c9f796a2ce9"),
+    "pulp-container==2.13.0",
     "django-automated-logging==6.1.3",
     "social-auth-core>=3.3.1,<4.0.0",
     "social-auth-app-django>=3.1.0,<4.0.0",
