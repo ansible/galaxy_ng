72220717892a583909dd5f02d08edca4e2fb0c59
Adrian Likins <alikins@redhat.com>
2022-08-10 11:09:44 -0400

    Rev the pinned vers of pulp* in setup.py
    No-Issue

--- setup.py
+++ setup.py
@@ -87,12 +87,12 @@ class BuildPyCommand(_BuildPyCommand):
 
 
 requirements = [
-    "galaxy-importer==0.4.2",
-    "pulpcore>=3.18.1,<3.19.0",
-    "pulp-ansible>=0.13.0,<0.14.0",
+    "galaxy-importer==0.4.5",
+    "pulpcore>=3.18.6,<3.19.0",
+    "pulp-ansible>=0.13.2,<0.14.0",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
-    "pulp-container>=2.10.2,<2.11.0",
+    "pulp-container>=2.10.5,<2.11.0",
     "django-automated-logging==6.1.3",
     "social-auth-core>=3.3.1,<4.0.0",
     "social-auth-app-django>=3.1.0,<4.0.0",
