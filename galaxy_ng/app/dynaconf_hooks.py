e1baf9ad765e72212775f3d8b93276f967f75151
Bruno Rocha <rochacbruno@users.noreply.github.com>
2022-08-18 11:08:34 +0100

    [Backport to stable-4.5] [bugfix] Fix signing feature flags for UI (#1417)
    Issue: AAH-1690

--- galaxy_ng/app/dynaconf_hooks.py
+++ galaxy_ng/app/dynaconf_hooks.py
@@ -1,9 +1,9 @@
 import json
-import ldap
-import pkg_resources
 import os
 from typing import Any, Dict
 
+import ldap
+import pkg_resources
 from django_auth_ldap.config import LDAPSearch
 from dynaconf import Dynaconf
 
@@ -15,6 +15,9 @@ def post(settings: Dynaconf) -> Dict[str, Any]:
 
     settings: A read-only copy of the django.conf.settings
     returns: a dictionary to be merged to django.conf.settings
+
+    NOTES:
+        Feature flags must be loaded directly on `app/api/ui/views/feature_flags.py` view.
     """
     data = {"dynaconf_merge": False}
     # existing keys will be merged if dynaconf_merge is set to True
@@ -24,7 +27,6 @@ def post(settings: Dynaconf) -> Dict[str, Any]:
     data.update(configure_logging(settings))
     data.update(configure_keycloak(settings))
     data.update(configure_cors(settings))
-    data.update(configure_feature_flags(settings))
     data.update(configure_pulp_ansible(settings))
     data.update(configure_authentication_classes(settings))
     data.update(configure_authentication_backends(settings))
@@ -270,16 +272,6 @@ def configure_cors(settings: Dynaconf) -> Dict[str, Any]:
     if settings.get("GALAXY_ENABLE_CORS", default=False):
         corsmiddleware = ["galaxy_ng.app.common.openapi.AllowCorsMiddleware"]
         data["MIDDLEWARE"] = corsmiddleware + settings.get("MIDDLEWARE", [])
-    return data
-
-
-def configure_feature_flags(settings: Dynaconf) -> Dict[str, Any]:
-    """Adds conditional feature flags"""
-    data = {}
-    data["GALAXY_FEATURE_FLAGS__collection_signing"] = settings.get(
-        "GALAXY_COLLECTION_SIGNING_SERVICE") is not None
-    data["GALAXY_FEATURE_FLAGS__collection_auto_sign"] = settings.get(
-        "GALAXY_AUTO_SIGN_COLLECTIONS", False)
     return data
 
 
