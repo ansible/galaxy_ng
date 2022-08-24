880f1f039a092f901e26f75f6ec0be7a81a126c2
James Tanner <tanner.jc@gmail.com>
2022-08-24 11:33:44 -0400

    Add more commentary.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/v1/models.py
+++ galaxy_ng/app/api/v1/models.py
@@ -5,6 +5,24 @@ from galaxy_ng.app.models import Namespace
 
 
 class LegacyNamespace(models.Model):
+    """
+    A legacy namespace, aka a github username.
+
+    Namespaces in the galaxy_ng sense are very restrictive
+    with their character sets. This is primarily due to how
+    collection namespaces and names must be pythonic and
+    importable module names. Legacy roles had no such
+    restrictions and were 1:1 with whatever github allowed
+    for a username.
+
+    This model exists for a few reasons:
+        1) enable an endpoint for v1/users with no sql hacks
+        2) enable the ui to list namespaces with avatar icons
+        3) to map the legacy namespace to a new namespace
+           which can have galaxy_ng style permission management
+        4) to define what users aside from the creator can
+           "own" the roles under the namespace.
+    """
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -30,6 +48,26 @@ class LegacyNamespace(models.Model):
 
 
 class LegacyRole(models.Model):
+    """
+    A legacy v1 role, which is just an index for github.
+
+    These are not "content" in the pulp sense. They are simply
+    an index of repositories on github.com that have the shape
+    of a standalone role. Nothing is stored on disk or served
+    out to the client from the server besides json metadata.
+
+    Sometimes they have versions and sometimes not, hence
+    there is no LegacyRoleVersion model.
+
+    Rather than make many many fields and many many models
+    to encapsulate the various type of data for a role, this model
+    uses a json field to store everything. It is effectively
+    mimic'ig a NOSQL database in that regard. Instead of
+    adding new fields here as requirements change, store the new
+    data in the full_metadata field and alter the serializer
+    to expose that new data or to munge old data. For example,
+    the docs_blob is a key inside full_metadata.
+    """
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
