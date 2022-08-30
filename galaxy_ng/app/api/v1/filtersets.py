29731c930a5d4d5735ef6d530d831274a0e7be4e
James Tanner <tanner.jc@gmail.com>
2022-08-29 22:46:19 -0400

    Add filterset for roles viewsets.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/v1/filtersets.py
+++ galaxy_ng/app/api/v1/filtersets.py
@@ -3,6 +3,7 @@ from django_filters import filters
 from django_filters.rest_framework import filterset
 
 from galaxy_ng.app.api.v1.models import LegacyNamespace
+from galaxy_ng.app.api.v1.models import LegacyRole
 
 
 class LegacyNamespaceFilter(filterset.FilterSet):
@@ -48,3 +49,36 @@ class LegacyUserFilter(LegacyNamespaceFilter):
     def username_filter(self, queryset, name, value):
         username = self.request.query_params.get('username')
         return queryset.filter(name=username)
+
+
+class LegacyRoleFilter(filterset.FilterSet):
+
+    github_user = filters.CharFilter(method='github_user_filter')
+    keywords = filters.CharFilter(method='keywords_filter')
+
+    sort = filters.OrderingFilter(
+        fields=(
+            ('created', 'full_metadata__created'),
+            # ('name', 'name')
+        )
+    )
+
+    class Meta:
+        model = LegacyRole
+        fields = ['created', 'name']
+
+    def github_user_filter(self, queryset, name, value):
+        return queryset.filter(namespace__name=value)
+
+    def keywords_filter(self, queryset, name, value):
+
+        keywords = self.request.query_params.getlist('keywords')
+
+        for keyword in keywords:
+            queryset = queryset.filter(
+                Q(namespace__name__contains=keyword)
+                | Q(name__contains=keyword)
+                | Q(full_metadata__description__contains=keyword)
+            )
+
+        return queryset
--- galaxy_ng/app/api/v1/viewsets/roles.py
+++ galaxy_ng/app/api/v1/viewsets/roles.py
@@ -1,8 +1,7 @@
 import logging
 
-from django.conf import settings
-from django.db.models import Q
 from django.http import HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
 
 from drf_spectacular.utils import extend_schema_field
 
@@ -21,7 +20,6 @@ from galaxy_ng.app.api.v1.tasks import (
     legacy_role_import,
 )
 from galaxy_ng.app.api.v1.models import (
-    LegacyNamespace,
     LegacyRole
 )
 from galaxy_ng.app.api.v1.serializers import (
@@ -31,6 +29,7 @@ from galaxy_ng.app.api.v1.serializers import (
 )
 
 from galaxy_ng.app.api.v1.viewsets.tasks import LegacyTasksViewset
+from galaxy_ng.app.api.v1.filtersets import LegacyRoleFilter
 
 
 logger = logging.getLogger(__name__)
@@ -46,6 +45,10 @@ class LegacyRoleBaseViewSet(viewsets.ModelViewSet, LegacyTasksViewset):
     """Base class for legacy roles."""
 
     queryset = LegacyRole.objects.all().order_by('full_metadata__created')
+    # queryset = LegacyRole.objects.all()
+    ordering_fields = ('full_metadata__created')
+    ordering = ('full_metadata__created')
+
     serializer = LegacyRoleSerializer
     serializer_class = LegacyRoleSerializer
     # permission_classes = [AllowAny]
@@ -53,91 +56,9 @@ class LegacyRoleBaseViewSet(viewsets.ModelViewSet, LegacyTasksViewset):
     pagination_class = LegacyRolesSetPagination
     authentication_classes = [BasicAuthentication, SessionAuthentication, TokenAuthentication]
 
-    def get_queryset(self):
-
-        logger.debug(f'QUERY_PARAMS: {self.request.query_params}')
-
-        keywords = None
-        if self.request.query_params.get('keywords'):
-            keywords = self.request.query_params.get('keywords').rstrip('/')
-
-        order_by = 'full_metadata__created'
-        if self.request.query_params.get('order_by'):
-            order_by = self.request.query_params.get('order_by').rstrip('/')
-            order_by = f'full_metadata__{order_by}'
-
-        github_user = None
-        for keyword in ['owner__username', 'github_user', 'namespace']:
-            if self.request.query_params.get(keyword):
-                github_user = self.request.query_params[keyword]
-                if github_user is not None:
-                    github_user = github_user.rstrip('/')
-                break
-
-        namespace = None
-        if github_user:
-            # if github_user.isdigit():
-            #    namespace = LegacyNamespace.objects.filter(pk=int(github_user)).first()
-            # else:
-            #    namespace = LegacyNamespace.objects.filter(name=github_user).first()
-            namespace = LegacyNamespace.objects.filter(name=github_user).first()
-
-        name = self.request.query_params.get('name')
-        if name is not None:
-            name = name.rstrip('/')
-
-        if github_user and name:
-            logger.debug('FILTER BY USER AND NAME')
-            return LegacyRole.objects.filter(namespace=namespace, name=name).order_by(order_by)
-
-        elif github_user:
-            logger.info(f'FILTER BY USER: {github_user}')
-            logger.info(f'FILTER BY NAMESPACE: {namespace}')
-            return LegacyRole.objects.filter(namespace=namespace).order_by(order_by)
-
-        if keywords:
-            return LegacyRole.objects.filter(
-                Q(namespace__name__contains=keywords)
-                | Q(name__contains=keywords)
-                | Q(full_metadata__description__contains=keywords)
-            ).order_by(order_by)
-
-        # galaxy cli uses autocomplete kwargs for searching ...
-        # FIXME: no support for platforms yet. More research and planning needed.
-        autocomplete_kwargs = {
-            'autocomplete': None,
-            'platforms_autocomplete': None,
-            'tags_autocomplete': None,
-            'username_autocomplete': None,
-        }
-        for key in autocomplete_kwargs.keys():
-            autocomplete_kwargs[key] = self.request.GET.get(key)
-        if autocomplete_kwargs.values():
-            # assemble a list of filter arguments which will be
-            # an AND operation by default for each item.
-            filter_args = []
-            if autocomplete_kwargs['autocomplete']:
-                filter_args.append(
-                    Q(namespace__name__contains=autocomplete_kwargs['autocomplete'])
-                    | Q(name__contains=autocomplete_kwargs['autocomplete'])
-                )
-            if autocomplete_kwargs['username_autocomplete']:
-                filter_args.append(
-                    Q(namespace__name__contains=autocomplete_kwargs['username_autocomplete'])
-                )
-            if autocomplete_kwargs['tags_autocomplete']:
-                if ' ' in autocomplete_kwargs['tags_autocomplete']:
-                    tags = autocomplete_kwargs['tags_autocomplete']
-                    tags = tags.split(' ')
-                    filter_args.extend([Q(full_metadata__tags__contains=x) for x in tags])
-                else:
-                    filter_args.append(
-                        Q(full_metadata__tags__contains=autocomplete_kwargs['tags_autocomplete'])
-                    )
-
-            return LegacyRole.objects.filter(*filter_args).order_by(order_by)
-
-        return LegacyRole.objects.all().order_by(order_by)
+    # filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = LegacyRoleFilter
 
 
 class LegacyRolesViewSet(LegacyRoleBaseViewSet):
@@ -162,11 +83,6 @@ class LegacyRoleImportsViewSet(LegacyRoleBaseViewSet):
         """Import a new role or new role version."""
         data = request.data
 
-        logger.debug(f'github_user: {data.get("github_user")}')
-        logger.debug(f'github_repo: {data.get("github_repo")}')
-        logger.debug(f'github_reference: {data.get("github_reference")}')
-        logger.debug(f'alternate_role_name: {data.get("alternate_role_name")}')
-
         kwargs = {
             'github_user': data.get('github_user'),
             'github_repo': data.get('github_repo'),
@@ -176,18 +92,6 @@ class LegacyRoleImportsViewSet(LegacyRoleBaseViewSet):
         error = self._validate_create_kwargs(kwargs)
         if error:
             return HttpResponse(str(error), status=403)
-
-        logger.debug(f'SETTINGS: {settings}')
-        for x in dir(settings):
-            if 'auth' in x.lower():
-                logger.debug(f'SETTING {x}: {getattr(settings, x)}')
-        logger.debug(f'REQUEST HEADERS: {self.request.headers}')
-        logger.debug(f'REQUEST USER: {self.request.user}')
-        logger.debug(
-            'REQUEST USER IS AUTHENTICATED:'
-            + f' {self.request.user.is_authenticated}'
-        )
-        logger.debug(f'REQUEST USER USERNAME: {self.request.user.username}')
 
         task_id = self.legacy_dispatch(legacy_role_import, kwargs=kwargs)
 
