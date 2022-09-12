21a70c9a68645a77f672869da9e8873ebce728d9
James Tanner <tanner.jc@gmail.com>
2022-09-12 15:34:11 -0400

    Refactor.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/api/test_container_ldap.py
+++ galaxy_ng/tests/integration/api/test_container_ldap.py
@@ -28,30 +28,5 @@ def test_push_container_as_ldap_user(ansible_config):
     subprocess.check_call(["docker", "login", "-u", username, "-p", password, "localhost:5001"])
 
     # Push image to local registry
-    pid = subprocess.run(["docker", "push", "localhost:5001/alpine:latest"], stderr=subprocess.PIPE)
-    assert pid.returncode == 0, pid.stderr
-
-    '''
-    # Get an API client running with admin user credentials
-    client = get_client(config=ansible_config("admin"), request_token=True, require_auth=True)
-
-    # Get the pulp_href for the pushed image
-    image = client(
-        "/api/automation-hub/pulp/api/v3/repositories/container/container-push/?name=alpine"
-    )
-    container_href = image["results"][0]["pulp_href"]
-
-    # fetch the container
-    repo = client(container_href)
-    latest_version_href = repo["latest_version_href"]
-
-    # Check the image is signed on the latest version
-    latest_version = client(latest_version_href)
-    # assert latest_version["content_summary"]["added"]["container.signature"]["count"] > 0
-    import epdb; epdb.st()
-
-    # Check the sign state is set on the UI API
-    ee = client("/api/automation-hub/_ui/v1/execution-environments/repositories/?name=alpine")
-    # assert ee["data"][0]["pulp"]["repository"]["sign_state"] == "signed"
-    import epdb; epdb.st()
-    '''
+    pid = subprocess.run(["docker", "push", "-d", "localhost:5001/alpine:latest"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    assert pid.returncode == 0, pid.stdout
