443f3807e00b68b311247b6b0df53abcfb40afaa
James Tanner <tanner.jc@gmail.com>
2022-09-15 09:46:15 -0400

    Skip test if not ldap auth.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/api/test_container_ldap.py
+++ galaxy_ng/tests/integration/api/test_container_ldap.py
@@ -11,9 +11,19 @@ import pytest
 from galaxy_ng.tests.integration.utils import get_client
 
 
+@pytest.fixture(scope="function")
+def settings(ansible_config):
+    config = ansible_config("admin")
+    api_client = get_client(config, request_token=False, require_auth=True)
+    return api_client("/api/automation-hub/_ui/v1/settings/")
+
+
 @pytest.mark.ldap
 @pytest.mark.standalone_only
-def test_push_container_as_ldap_user(ansible_config):
+def test_push_container_as_ldap_user(ansible_config, settings):
+
+    if not settings.get("GALAXY_AUTH_LDAP_ENABLED"):
+        pytest.skip("GALAXY_AUTH_LDAP_ENABLED is not enabled")
 
     # get an ldap user
     config = ansible_config("ldap")
