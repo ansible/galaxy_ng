2b142cf34dc1d254b2d63512109b13143d2ed788
Mike DePaulo <mikedep333@redhat.com>
2022-10-03 16:12:22 -0400

    Update CI from plugin-template
    Fixes CI
    
    [noissue]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 set -mveuo pipefail
+shopt -s expand_aliases
 source .github/workflows/scripts/utils.sh
 cmd_prefix bash -c "django-admin compilemessages"
 
 echo "machine pulp
 login admin
 password password
-" > ~/.netrc
+" > ~pulp/.netrc
 
-chmod og-rw ~/.netrc
+chmod og-rw ~pulp/.netrc
