5d5b9ae265577d3e00ca4be3213564196b00fed7
Bruno Rocha <rochacbruno@gmail.com>
2022-10-10 11:52:48 +0100

    Upgrade to Pulp Container 2.14.1
    Issue: AAH-1950

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -121,7 +121,7 @@ fi
 
 cd ..
 
-git clone --depth=1 https://github.com/pulp/pulp_container.git --branch 2.13.1
+git clone --depth=1 https://github.com/pulp/pulp_container.git --branch 2.14.1
 cd pulp_container
 
 if [ -n "$PULP_CONTAINER_PR_NUMBER" ]; then
