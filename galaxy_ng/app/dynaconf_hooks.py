8defea2e20aa148d8da6c3ea2b1e160724de8cb5
jctanner <tanner.jc@gmail.com>
2022-10-11 13:50:32 -0400

    Bypass social_core's randomized username creation when a user exists. (#1493)
    * Bypass social_core's randomized username creation when a user exists.
    * Add test for social auth user fix.
    * Lint fixes.
    
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/dynaconf_hooks.py
+++ galaxy_ng/app/dynaconf_hooks.py
@@ -222,6 +222,22 @@ def configure_socialauth(settings: Dynaconf) -> Dict[str, Any]:
             "rest_framework.authentication.BasicAuthentication",
         ]
 
+        # Override the get_username step so that social users
+        # are associated to existing users instead of creating
+        # a whole new randomized username.
+        data['SOCIAL_AUTH_PIPELINE'] = [
+            'social_core.pipeline.social_auth.social_details',
+            'social_core.pipeline.social_auth.social_uid',
+            'social_core.pipeline.social_auth.auth_allowed',
+            'social_core.pipeline.social_auth.social_user',
+            # 'social_core.pipeline.user.get_username',
+            'galaxy_ng.social.pipeline.user.get_username',
+            'social_core.pipeline.user.create_user',
+            'social_core.pipeline.social_auth.associate_user',
+            'social_core.pipeline.social_auth.load_extra_data',
+            'social_core.pipeline.user.user_details'
+        ]
+
     return data
 
 
---
+++ galaxy_ng/social/pipeline/user.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+
+def get_username(strategy, details, backend, user=None, *args, **kwargs):
+    return {'username': details['username']}
