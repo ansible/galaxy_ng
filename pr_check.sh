0e44cc16cfc2915b15ebdda7e58998c498d471a7
Daniel Rodowicz <drodowic@redhat.com>
2022-11-15 12:45:10 -0500

    convert pr_check.sh to use frontend operator (#1526)
    * convert pr_check.sh to use frontend operator
    * setup ephemeral for frontend operator
    
    Issue: AAH-1956

--- pr_check.sh
+++ pr_check.sh
@@ -26,10 +26,14 @@ if [ $skip_pr_check == "true" ]; then exit 0; fi
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="automation-hub,gateway,insights-ephemeral"  # name of app-sre "application" folder this component lives in
+APP_NAME="automation-hub"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="automation-hub"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/automation-hub-galaxy-ng"
 COMPONENTS_W_RESOURCES="all"  # components which should preserve resource settings (optional, default: none)
+
+export IMAGE_FRONTEND="quay.io/cloudservices/ansible-hub-ui"
+export IMAGE_FRONTEND_SHA1=$(curl -s https://api.github.com/repos/ansible/ansible-hub-ui/commits/master | jq -r '.sha')
+export IMAGE_FRONTEND_TAG=$(echo ${IMAGE_FRONTEND_SHA1} | head -c7)
 
 # IQE_PLUGINS=""
 # IQE_MARKER_EXPRESSION="ephemeral"
@@ -47,48 +51,31 @@ source "$CICD_ROOT/build.sh"
 # NOTE(cutwater): See https://issues.redhat.com/browse/RHCLOUD-14977
 source ${CICD_ROOT}/_common_deploy_logic.sh
 export NAMESPACE=$(bonfire namespace reserve)
-export CONTENT_ORIGIN="https://front-end-aggregator-${NAMESPACE}.apps.c-rh-c-eph.8p0c.p1.openshiftapps.com"
 bonfire deploy \
     ${APP_NAME} \
     --source=appsre \
     --ref-env insights-stage \
     --set-template-ref ${COMPONENT_NAME}=${GIT_COMMIT} \
     --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --set-template-ref automation-hub-frontend=${IMAGE_FRONTEND_SHA1} \
+    --set-image-tag ${IMAGE_FRONTEND}=${IMAGE_FRONTEND_TAG} \
+    --frontends=true \
     --namespace ${NAMESPACE} \
     --timeout ${DEPLOY_TIMEOUT} \
     ${COMPONENTS_ARG} \
     ${COMPONENTS_RESOURCES_ARG} \
     --set-parameter ${COMPONENT_NAME}/IMPORTER_JOB_NAMESPACE=${NAMESPACE} \
-    --set-parameter ${COMPONENT_NAME}/CONTENT_ORIGIN="${CONTENT_ORIGIN}" \
     --set-parameter ${COMPONENT_NAME}/ENABLE_SIGNING="0" \
     --set-parameter ${COMPONENT_NAME}/GALAXY_SIGNATURE_UPLOAD_ENABLED="false" \
     --set-parameter ${COMPONENT_NAME}/GALAXY_REQUIRE_SIGNATURE_FOR_APPROVAL="false" \
     --set-parameter ${COMPONENT_NAME}/GALAXY_AUTO_SIGN_COLLECTIONS="false" \
     --set-parameter ${COMPONENT_NAME}/GALAXY_COLLECTION_SIGNING_SERVICE="''"
 # END WORKAROUND
+bonfire namespace describe ${NAMESPACE}
 oc project ${NAMESPACE}
 
-UI_REF="qa-beta"
-echo "patching the frontend-aggregator to use ${UI_REF}"
-oc patch cm aggregator-app-config --type merge --patch "{\"data\": {\"app-config.yml\": \"${COMPONENT_NAME}:\n  commit: ${UI_REF}\n\"}}"
-oc rollout restart deployment/front-end-aggregator
-oc rollout status deployment/front-end-aggregator
-oc rollout restart deployment/mocks
-oc rollout status deployment/mocks
-FE_POD=$(oc get pod -l app=front-end-aggregator -o json | jq -r '.items[] | select( .metadata.deletionTimestamp == null ) | .metadata.name')
-oc exec ${FE_POD} -- /bin/bash -c "sed -i 's/--omit-dir-times/--omit-dir-times --omit-link-times/' /www/src/git_helper.sh"
-oc exec ${FE_POD} -- /bin/bash -c "/www/src/git_helper.sh config https://github.com/RedHatInsights/cloud-services-config prod-beta"
-
-# Fix the routing for minio and artifact urls
-oc create route edge minio --service=env-${NAMESPACE}-minio --insecure-policy=Redirect
-MINIO_ROUTE=$(oc get route minio -o jsonpath='https://{.spec.host}{"\n"}')
-oc patch clowdapp automation-hub \
-    --type=json \
-    -p '[{"op": "add", "path": "/spec/deployments/2/podSpec/env/-", "value": {
-                "name": "PULP_AWS_S3_ENDPOINT_URL",
-                "value": "'"${MINIO_ROUTE}"'"
-            }
-        }]'
+dev/ephemeral/patch_ephemeral.sh
+dev/ephemeral/create_keycloak_users.sh
 
 # source $CICD_ROOT/smoke_test.sh
 # source smoke_test.sh
