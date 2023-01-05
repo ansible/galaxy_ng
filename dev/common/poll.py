4edae3c82bf1cbd30c9cc5d7e98900b290fb1767
James Tanner <tanner.jc@gmail.com>
2023-01-05 17:20:06 -0500

    Do it the easy way.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- dev/common/poll.py
+++ dev/common/poll.py
@@ -11,85 +11,15 @@ import yaml
 from json import JSONEncoder
 
 
-class DynaConfEncoder(JSONEncoder):
-    '''Special encoder for a dynaconf structure'''
-    def default(self, o):
-        if isinstance(o, PosixPath):
-            return o.path
-        return o.__dict__
-
-
-class PosixPath:
-    '''Evalable class for dynaconf posixpath thingies'''
-    def __init__(self, path):
-        self.path = path
-
-    def toJSON(self):
-        return self.path
-
-
-def parse_dynaconf_list(rawtxt):
-    '''Parse the magical dynaconf list output'''
-
-    ds = {}
-
-    # get rid of color codes ...
-    rawtxt = rawtxt.replace('\x1b[37m\x1b[100m', '')
-    rawtxt = rawtxt.replace('\x1b[97m\x1b[104m\x1b[1m', '')
-    rawtxt = rawtxt.replace('\x1b[97m\x1b[45m', '')
-    rawtxt = rawtxt.replace('\x1b[0m\x1b[97m\x1b[100m', '')
-    rawtxt = rawtxt.replace('\x1b[0m', '')
-    rawtxt = rawtxt.replace('Working in development environment', '')
-    rawtxt = rawtxt.replace('Django app detected', '')
-
-    # split all the key&vals
-    lines = rawtxt.split('\n')
-    bits = ['']
-    for idx, x in enumerate(lines):
-        if '<' in x and '>' in x:
-            bits.append('')
-        bits[-1] += x
-
-    # parse all the key&vals
-    bits = [x.strip() for x in bits if x.strip()]
-    for bit in bits:
-        var_name = bit.split('<', 1)[0]
-        var_type = bit.replace(var_name, '').split('>', 1)[0].replace('<', '')
-
-        var_raw = bit.split('>', 1)[-1].strip()
-
-        val = None
-        if var_type in ['str']:
-            val = var_raw.replace("'", '')
-        elif var_type == 'NoneType':
-            val = None
-        elif var_type in ['int', 'list', 'bool', 'dict', 'PosixPath']:
-            val = eval(var_raw)
-        else:
-            raise Exception(f'{var_name} is {var_type} which is not yet parseable')
-        ds[var_name] = {
-            'name': var_name,
-            'type': var_type,
-            'value': val
-        }
-
-    # force back to stdlib types
-    ds = json.dumps(ds, cls=DynaConfEncoder)
-    ds = json.loads(ds)
-
-    return ds
-
-
 def get_dynaconf_variable(varname):
-    '''Run the dynaconf list command and get a specific key'''
-    cmd = 'dynaconf list'
+    '''Run the dynaconf get subcommand for a specific key'''
+    cmd = f'dynaconf get {varname}'
     cmd = f'./compose exec api bash -c "{cmd}"'
     pid = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout = pid.stdout.decode('utf-8')
-    parsed = parse_dynaconf_list(stdout)
-    if varname not in parsed:
+    if pid.returncode != 0:
         return None
-    return parsed[varname]['value']
+    stdout = pid.stdout.decode('utf-8')
+    return stdout.strip()
 
 
 def get_compose_config():
