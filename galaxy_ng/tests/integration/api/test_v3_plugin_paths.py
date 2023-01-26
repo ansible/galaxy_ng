45d21f6178c7d0b37e70b21640dede1c225ed414
Andrew Crosby <acrosby@redhat.com>
2023-01-26 17:09:08 -0500

    Add simple check that pulp_labels exists with correct type

--- galaxy_ng/tests/integration/api/test_v3_plugin_paths.py
+++ galaxy_ng/tests/integration/api/test_v3_plugin_paths.py
@@ -181,6 +181,9 @@ def test_api_v3_plugin_execution_environments_repositories(ansible_config, local
     # assert new namespace was created
     assert ns_name == ns_detail_resp['name']
 
+    # assert pulp_labels dictionary is in response
+    assert type(repository_resp['pulp']['repository']['pulp_labels']) == dict
+
     # delete the respository
     delete_repository_resp = api_client(
         f'{api_prefix}/v3/plugin/execution-environments/repositories/{name}/',
