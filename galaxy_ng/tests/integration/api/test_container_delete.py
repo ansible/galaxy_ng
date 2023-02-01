313ef257ed0c453034fb7bd5d79342d3bc01e262
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-02-01 13:10:18 -0500

    deal with linter errors
    Issue: AAH-2024

--- galaxy_ng/tests/integration/api/test_container_delete.py
+++ galaxy_ng/tests/integration/api/test_container_delete.py
@@ -1,14 +1,10 @@
-from galaxy_ng.tests.integration.api.rbac_actions.utils import ensure_test_container_is_pulled
 import requests
 import subprocess
-from galaxy_ng.tests.integration.utils import get_client
-
 
 
 def delete_ee_and_content(user, password, ansible_config):
     config = ansible_config("admin")
     api_prefix = config.get("api_prefix").rstrip("/")
-    api_client = get_client(config, request_token=False, require_auth=True)
 
     # Pull alpine image
     subprocess.check_call(["podman", "pull", "alpine"])
