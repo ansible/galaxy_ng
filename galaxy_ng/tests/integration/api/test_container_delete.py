29b765cd1059cdcd71ccd5928ad734065115d229
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-02-02 16:12:59 -0500

    fix url to distribution
    Issue: AAH-2024

--- galaxy_ng/tests/integration/api/test_container_delete.py
+++ galaxy_ng/tests/integration/api/test_container_delete.py
@@ -28,13 +28,13 @@ def test_delete_ee_and_content(ansible_config):
 
     # Go to the container distribution list and select the distribution via the base path.
     distro_response = client(
-        f"{api_prefix}/api/automation-hub/pulp/api/v3/distributions/"
+        f"{api_prefix}/pulp/api/v3/distributions/"
         "container/container/?base_path=alpine",
     )
     assert distro_response["results"][0]["base_path"] == 'alpine'
 
     # Grab the repository href from the json and make get request
-    repo_href = distro_response["results"][0]["repository"]
+    repo_href = (distro_response["results"][0]["repository"]).replace("/api/automation-hub", "")
     repo_response = client(f"{api_prefix}{repo_href}")
 
     # Grab <latest_version_href> field from this Container Push Repo Instance
@@ -42,15 +42,15 @@ def test_delete_ee_and_content(ansible_config):
 
     # Filter List Content List by the latest version found above
     content_list = client(
-        f"{api_prefix}/api/automation-hub/pulp/api/v3/content/?repository_version={latest_version}",
+        f"{api_prefix}/pulp/api/v3/content/?repository_version={latest_version}",
     )
 
     # View content before deleting
     assert content_list["results"].length > 0
 
     # Delete repository, contents, and artifacts
-    delete_response = client(f"{api_prefix}/api/\
-        automation-hub/v3/plugin/execution-environments/repositories/alpine/", method='DELETE')
+    delete_response = client(f"{api_prefix}/api/automation-hub/v3/"
+                             "plugin/execution-environments/repositories/alpine/", method='DELETE')
     assert delete_response.status_code == 200
 
     # Ensure content list is empty
