23bb36080af033641bd7dafa401b763b62f442c1
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-02-02 10:57:42 -0500

    fix syntax errors in test
    Issue: AAH-2024

--- galaxy_ng/tests/integration/api/test_container_delete.py
+++ galaxy_ng/tests/integration/api/test_container_delete.py
@@ -1,44 +1,56 @@
-import requests
 import subprocess
+import pytest
+from ..utils import get_client
 
 
-def delete_ee_and_content(user, password, ansible_config):
+@pytest.mark.standalone_only
+def test_delete_ee_and_content(ansible_config):
     config = ansible_config("admin")
     api_prefix = config.get("api_prefix").rstrip("/")
 
     # Pull alpine image
-    subprocess.check_call(["podman", "pull", "alpine"])
-
+    subprocess.check_call(["docker", "pull", "alpine"])
     # Tag the image
-    subprocess.check_call(["podman", "tag", "alpine", "localhost:5001/alpine:latest"])
+    subprocess.check_call(["docker", "tag", "alpine", "localhost:5001/alpine:latest"])
+
+    # Login to local registy with tls verify disabled
+    subprocess.check_call(["docker", "login", "-u", "admin", "-p", "admin", "localhost:5001"])
+
+    # Push image to local registry
+    subprocess.check_call(["docker", "push", "localhost:5001/alpine:latest"])
+
+    # Get an API client running with admin user credentials
+    client = get_client(
+        config=ansible_config("admin"),
+        request_token=True,
+    )
+    api_prefix = client.config.get("api_prefix").rstrip("/")
 
     # Go to the container distribution list and select the distribution via the base path.
-    distro_response = requests.get(
-        f"{api_prefix}/api/automation-hub/pulp/api/v3/distributions/\
-            container/container/?base_path=alpine",
-        auth=(user['username'], password),
+    distro_response = client(
+        f"{api_prefix}/api/automation-hub/pulp/api/v3/distributions/"
+        "container/container/?base_path=alpine",
     )
     assert distro_response["results"][0]["base_path"] == 'alpine'
 
     # Grab the repository href from the json and make get request
     repo_href = distro_response["results"][0]["repository"]
-    repo_response = requests.get(f"{api_prefix}{repo_href}")
+    repo_response = client(f"{api_prefix}{repo_href}")
 
     # Grab <latest_version_href> field from this Container Push Repo Instance
     latest_version = repo_response["latest_version_href"]
 
     # Filter List Content List by the latest version found above
-    content_list = requests.get(
+    content_list = client(
         f"{api_prefix}/api/automation-hub/pulp/api/v3/content/?repository_version={latest_version}",
-        auth=(user['username'], password),
     )
 
     # View content before deleting
     assert content_list["results"].length > 0
 
     # Delete repository, contents, and artifacts
-    delete_response = requests.delete(f"{api_prefix}/api/\
-        automation-hub/v3/plugin/execution-environments/repositories/alpine/")
+    delete_response = client(f"{api_prefix}/api/\
+        automation-hub/v3/plugin/execution-environments/repositories/alpine/", method='DELETE')
     assert delete_response.status_code == 200
 
     # Ensure content list is empty
