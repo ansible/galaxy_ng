9804fa1760356d314a68f7645cfe712fc2376e45
Christian <ctorrens@redhat.com>
2023-03-14 22:35:33 +0100

    WIP method to verify Issue: AAH-2125

--- galaxy_ng/tests/integration/api/test_repo_management.py
+++ galaxy_ng/tests/integration/api/test_repo_management.py
@@ -30,6 +30,7 @@ def create_repo_and_dist(client, repo_name):
     ansible_distribution_path = "/api/automation-hub/pulp/api/v3/distributions/ansible/ansible/"
     logger.debug(f"creating repo {repo_name}")
     repo_res = create_repository(client, repo_name)
+    logger.debug(f"Repository creation response {repo_res}")
     dist_data = {"base_path": repo_name, "name": repo_name, "repository": repo_res['pulp_href']}
     logger.debug(f"creating dist with this data {dist_data}")
     task_resp = client.post(ansible_distribution_path, dist_data)
@@ -52,11 +53,26 @@ def edit_results_for_verification(results):
         new_results.append(new_result)
     return new_results
 
+def verify_repo_data(expected_repos, actual_repos):
+    def is_dict_included(dict1, dict2):
+        # Check if all key-value pairs in dict1 are present in dict2
+        for key, value in dict1.items():
+            if key not in dict2 or dict2[key] != value:
+                return False
+        return True
+    for expected_repo in expected_repos:
+        found = False
+        for actual_repo in actual_repos:
+            if is_dict_included(expected_repo, actual_repo):
+                found = True
+        if not found:
+            return False
+    return True
+
 
 @pytest.mark.min_hub_version("4.6dev")  # set correct min hub version
 class TestRM:
-
-    @pytest.mark.rm
+    #@pytest.mark.rm
     @pytest.mark.standalone_only
     def test_create_repository(self, galaxy_client):
         """
@@ -68,7 +84,7 @@ class TestRM:
         repos = get_all_repositories(gc)
         assert repo_exists(test_repo_name, repos)
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_delete_repository(self, galaxy_client):
         """
@@ -85,7 +101,7 @@ class TestRM:
 
     # http://localhost:5001/pulp_ansible/galaxy/default/api/v3/plugin/ansible/search/collection-versions/?repository=published&name=collection_dep_a_ejrhoshf
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search(self, galaxy_client):
         """
@@ -106,7 +122,7 @@ class TestRM:
         result = search_collection(gc, search_string=artifact.name)
         logger.debug(result)
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_same_repo_diff_versions(self, galaxy_client):
         """
@@ -151,7 +167,7 @@ class TestRM:
         assert result["data"][0]["collection_version"]["version"] == "0.0.2"
         assert result["data"][0]["repository"]["name"] == test_repo_name
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_diff_repo_diff_versions(self, galaxy_client):
         """
@@ -204,8 +220,7 @@ class TestRM:
             if data["repo_name"] == test_repo_name_1:
                 assert data["cv_name"] == artifact_1.name
 
-
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_diff_repo_same_versions(self, galaxy_client):
         """
@@ -249,7 +264,7 @@ class TestRM:
             if data["repo_name"] == test_repo_name_1:
                 assert data["cv_name"] == artifact_1.name
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_same_repo_same_versions(self, galaxy_client):
         """
@@ -276,8 +291,7 @@ class TestRM:
             upload_test_artifact(gc, namespace_name, test_repo_name, artifact)
         assert ctx.value.response.status_code == 400
 
-
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_diff_collection_diff_namespaces(self, galaxy_client):
         """
@@ -321,7 +335,7 @@ class TestRM:
         new_results = edit_results_for_verification(results)
         logger.debug(new_results)
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_diff_repo_diff_versions_check_both_is_highest(self, galaxy_client):
         """
@@ -370,6 +384,11 @@ class TestRM:
         logger.debug(results)
         new_results = edit_results_for_verification(results)
         logger.debug(new_results)
+
+        expected = [{"repo_name": test_repo_name_1, "cv_name": artifact_1.name, "is_highest": True},
+                    {"repo_name": test_repo_name_2, "cv_name": artifact_1.name, "is_highest": True}]
+        verify_repo_data(expected, new_results)
+        '''
         for data in new_results:
             if data["repo_name"] == test_repo_name_1:
                 assert data["cv_name"] == artifact_1.name
@@ -377,12 +396,12 @@ class TestRM:
             if data["repo_name"] == test_repo_name_2:
                 assert data["cv_name"] == artifact_1.name
                 assert data["is_highest"] is True
-
+        '''
     @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_upload_same_collection_same_repo_diff_versions_delete_check_is_highest(self, galaxy_client):
         """
-        Verifies TODO
+        Verifies
         """
         test_repo_name = f"repo-test-{uuid4()}"
 
@@ -425,21 +444,25 @@ class TestRM:
         logger.debug(results)
         new_results = edit_results_for_verification(results)
         logger.debug(new_results)
+        '''
         for data in new_results:
             if data["cv_version"] == "0.0.2":
                 assert data["is_highest"] is True
             if data["cv_version"] == "0.0.1":
                 assert data["is_highest"] is False
+        '''
+
+        expected = [{"cv_version": "0.0.2", "is_highest": True}, {"cv_version": "0.0.1", "is_highest": False}]
+        assert verify_repo_data(expected, new_results)
         delete_collection(gc, namespace_name, artifact_1.name, version="0.0.2", repository=test_repo_name)
         results = search_collection(gc, search_string=artifact_1.name)
         logger.debug(results)
         new_results = edit_results_for_verification(results)
         logger.debug(new_results)
-        for data in new_results:
-            if data["cv_version"] == "0.0.1":
-                assert data["is_highest"] is True
+        expected = [{"cv_version": "0.0.1", "is_highest": True}]
+        assert verify_repo_data(expected, new_results)
 
-    @pytest.mark.rm
+    # @pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_delete_repo_with_contents(self, galaxy_client):
         """
@@ -472,8 +495,7 @@ class TestRM:
         results = search_collection(gc, search_string=artifact.name)
         logger.debug(results)
 
-
-    @pytest.mark.rm
+    #@pytest.mark.rm
     @pytest.mark.standalone_only
     def test_search_deprecate_collection(self, galaxy_client):
         """
@@ -506,8 +528,5 @@ class TestRM:
         results = search_collection(gc, repository=test_repo_name, search_string=artifact.name)
         logger.debug(results)
         new_results = edit_results_for_verification(results)
-        assert new_results[0]["is_deprecated"] is True
-
-# delete is_highest, check the other is_highest
-# is highest is in the context of the repo
-# what is repository version?
+        expected = {"repo_name": test_repo_name, "is_deprecated": True}
+        verify_repo_data(expected, new_results)
