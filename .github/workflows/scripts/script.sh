25a2e8fa123842d54f4fc0c9cd5ebb80a74c0093
Jiří Jeřábek <Jerabekjirka@email.cz>
2023-04-03 17:11:05 +0200

    Pin pulp_ansible version and update template_config (#1662)
    * update requirement files
    * update template_config
    * fix template_gitref
    No-Issue

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -8,11 +8,11 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -mveuo pipefail
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 REPO_ROOT="$PWD"
-
-set -mveuo pipefail
 
 source .github/workflows/scripts/utils.sh
 
@@ -44,21 +44,16 @@ if [[ "$TEST" = "docs" ]]; then
   exit
 fi
 
+REPORTED_STATUS="$(pulp status)"
+
 if [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
-  STATUS_ENDPOINT="${PULP_URL}${PULP_API_ROOT}api/v3/status/"
-  echo $STATUS_ENDPOINT
-  REPORTED_VERSION=$(http $STATUS_ENDPOINT | jq --arg plugin galaxy --arg legacy_plugin galaxy_ng -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+  REPORTED_VERSION="$(echo $REPORTED_STATUS | jq --arg plugin galaxy --arg legacy_plugin galaxy_ng -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')"
   response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/galaxy-ng/$REPORTED_VERSION/)
   if [ "$response" == "200" ];
   then
     echo "galaxy_ng $REPORTED_VERSION has already been released. Skipping running tests."
     exit
   fi
-fi
-
-if [[ "$TEST" == "plugin-from-pypi" ]]; then
-  COMPONENT_VERSION=$(http https://pypi.org/pypi/galaxy-ng/json | jq -r '.info.version')
-  git checkout ${COMPONENT_VERSION} -- galaxy_ng/tests/
 fi
 
 echo "machine pulp
@@ -68,26 +63,36 @@ password password
 # Some commands like ansible-galaxy specifically require 600
 cmd_user_stdin_prefix bash -c "chmod 600 ~pulp/.netrc"
 
+cd ../pulp-openapi-generator
+if [ "$(echo "$REPORTED_STATUS" | jq -r '.versions[0].package')" = "null" ]
+then
+  # We are on an old version of pulpcore without package in the status report
+  for app_label in $(echo "$REPORTED_STATUS" | jq -r '.versions[].component')
+  do
+    if [ "$app_label" = "core" ]
+    then
+      item=pulpcore
+    else
+      item="pulp_${app_label}"
+    fi
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+else
+  for item in $(echo "$REPORTED_STATUS" | jq -r '.versions[].package|sub("-"; "_")')
+  do
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+fi
+
+cd $REPO_ROOT
+
 cat unittest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/unittest_requirements.txt"
 cat functest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/functest_requirements.txt"
-cmd_prefix pip3 install -r /tmp/unittest_requirements.txt
-cmd_prefix pip3 install -r /tmp/functest_requirements.txt
-cmd_prefix pip3 install --upgrade ../pulp-smash
-
-cd ../pulp-openapi-generator
-./generate.sh galaxy_ng python
-cmd_prefix pip3 install /root/pulp-openapi-generator/galaxy_ng-client
-sudo rm -rf ./galaxy_ng-client
-./generate.sh pulpcore python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulpcore-client
-sudo rm -rf ./pulpcore-client
-./generate.sh pulp_ansible python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_ansible-client
-sudo rm -rf ./pulp_ansible-client
-./generate.sh pulp_container python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_container-client
-sudo rm -rf ./pulp_container-client
-cd $REPO_ROOT
+cmd_prefix pip3 install -r /tmp/unittest_requirements.txt -r /tmp/functest_requirements.txt
 
 CERTIFI=$(cmd_prefix python3 -c 'import certifi; print(certifi.where())')
 cmd_prefix bash -c "cat /etc/pulp/certs/pulp_webserver.crt  | tee -a "$CERTIFI" > /dev/null"
