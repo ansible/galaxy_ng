097537e2b4460475aa30ea582ec22acfaf7da051
Christian <ctorrens@redhat.com>
2023-04-11 22:06:43 +0200

    sync tests - WIP Issue: AAH-2125

--- galaxy_ng/tests/integration/api/test_custom_repo_sync.py
+++ galaxy_ng/tests/integration/api/test_custom_repo_sync.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import pytest
@@ -5,10 +6,15 @@ import pytest
 from galaxykit.remotes import create_remote
 from galaxykit.repositories import create_repository
 from galaxykit.utils import wait_for_task
+from .test_repo_management import create_test_namespace, upload_new_artifact, search_collection_endpoint, \
+    add_content_units, create_repo_and_dist, verify_repo_data
+from .test_sync_stage import start_sync
 from ..conftest import AnsibleConfigFixture, get_galaxy_client, get_ansible_config
 from ..utils import get_client, clear_certified, perform_sync, iterate_all, set_synclist
 from ..utils.iqe_utils import GalaxyKitClient
 from ..utils.tools import generate_random_string
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.mark.standalone_only
@@ -22,20 +28,40 @@ def test_basic_sync_custom_repo(galaxy_client):
     custom_config = {"url": url}
     galaxy_kit_client = GalaxyKitClient(custom_config)
     source_client = galaxy_kit_client.gen_authorized_client({"username": "notifications_admin", "password":"redhat"}, basic_token=True)
-    r = create_repository(source_client, test_repo_name_1)
+    pulp_href = create_repo_and_dist(source_client, test_repo_name_1)
+
+    namespace_name = create_test_namespace(source_client)
+    tags = ["application"]
+    artifact = upload_new_artifact(source_client, namespace_name, test_repo_name_1, "1.0.1", tags=tags)
+    collection_resp = source_client.get(f"pulp/api/v3/content/ansible/collection_versions/?name={artifact.name}")
+    content_units = [collection_resp["results"][0]["pulp_href"]]
+    add_content_units(source_client, content_units, pulp_href)
+    matches, results = search_collection_endpoint(source_client, name=artifact.name)
+    logger.debug(f"Results {results}!")
 
     # this is the standalone mode instance (destination local hub)
     gc = galaxy_client("iqe_admin")
     test_remote_name = f"remote-test-{generate_random_string()}"
     params = {"auth_url": "http://localhost:8080/auth/realms/redhat-external/protocol/openid-connect/token",
-              "token": "abcdefghijklmnopqrstuvwxyz1234567893"
+              "token": "abcdefghijklmnopqrstuvwxyz1234567893",
+              "requirements_file":
+                  f"---\ncollections:\n- {artifact.namespace}.{artifact.name}"
               }
-    respuesta = create_remote(gc, test_remote_name, url, params=params)
+    respuesta = create_remote(gc, test_remote_name, f"{url}content/{test_repo_name_1}/", params=params)
     remote_pulp_href = respuesta["pulp_href"]
     ansible_distribution_path = "/api/automation-hub/pulp/api/v3/distributions/ansible/ansible/"
     repo_res = create_repository(gc, test_repo_name_1, remote=remote_pulp_href)
     dist_data = {"base_path": test_repo_name_1, "name": test_repo_name_1, "repository": repo_res['pulp_href']}
     task_resp = gc.post(ansible_distribution_path, dist_data)
     wait_for_task(gc, task_resp)
+
+    sync_url = f"content/{test_repo_name_1}/v3/sync/"
+    r = gc.post(sync_url, body="")
+    wait_for_task(gc, r, task_id=r["task"])
+
+    matches, results = search_collection_endpoint(gc, name=artifact.name, limit=100)
+    logger.debug(f"Results {results}!")
+    expected = [{"repo_name": test_repo_name_1, "cv_version": "1.0.1", "is_highest": True, "cv_name": artifact.name}]
+    assert verify_repo_data(expected, results)
 
 
