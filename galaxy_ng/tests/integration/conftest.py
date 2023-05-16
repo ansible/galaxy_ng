e7dfbf3266ca3776e83b31b96231e317736e844e
Andrew Crosby <acrosby@redhat.com>
2023-05-16 11:43:53 -0400

    Test ci
    No-Issue

--- galaxy_ng/tests/integration/conftest.py
+++ galaxy_ng/tests/integration/conftest.py
@@ -68,6 +68,7 @@ rbac_repos: tests verifying rbac roles on custom repositories
 rm_sync: tests verifying syncing custom repositories
 x_repo_search: tests verifying cross-repo search endpoint
 repositories: tests verifying custom repositories
+test_ci: testing
 """
 
 
