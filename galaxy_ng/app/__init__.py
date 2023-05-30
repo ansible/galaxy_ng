632f48afe8056877b2a18b99cdd22aa1645f5033
Jiří Jeřábek (Jiri Jerabek) <Jerabekjirka@email.cz>
2023-05-30 15:47:26 +0200

    bump version to 4.6.6 (#1745)
    No-Issue

--- galaxy_ng/app/__init__.py
+++ galaxy_ng/app/__init__.py
@@ -6,7 +6,7 @@ class PulpGalaxyPluginAppConfig(PulpPluginAppConfig):
 
     name = "galaxy_ng.app"
     label = "galaxy"
-    version = "4.6.5"
+    version = "4.6.6"
     python_package_name = "galaxy-ng"
 
     def ready(self):
