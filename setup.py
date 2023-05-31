61c33e479269e0277fd9ecbd0f8fae8ae7b98ff2
Andrew Crosby <acrosby@redhat.com>
2023-05-31 13:11:18 -0400

    Decrease time for repo move tasks via pulp-ansible 0.17.2 (#1750)
    pulp-ansible 0.17.2 includes optimization for x-repo search index builds
    
    Issue: AAH-2364

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def _format_pulp_requirement(plugin, specifier=None, ref=None, gh_namespace="pul
 requirements = [
     "galaxy-importer>=0.4.10,<0.5.0",
     "pulpcore>=3.23.0,<3.24.0",
-    "pulp_ansible>=0.17.1,<0.18.0",
+    "pulp_ansible>=0.17.2,<0.18.0",
     "django-prometheus>=2.0.0",
     "drf-spectacular",
     "pulp-container>=2.14.5,<2.15.0",
