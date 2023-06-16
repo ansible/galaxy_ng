c5912788f6e30917cb865a88f11ede9e0ecf65d5
jerabekjiri <jerabekjirka@email.cz>
2023-06-16 15:47:33 +0200

    copy galaxy-importer cfg
    Issue: AAH-2382

--- profiles/community/init.sh
+++ profiles/community/init.sh
@@ -6,6 +6,8 @@ set -o pipefail
 
 source /src/galaxy_ng/profiles/base/init.sh
 
+cp -r /src/galaxy_ng/profiles/community/galaxy-importer /etc/galaxy-importer
+
 setup_collection_signing_service
 
 # collect rest_framewrok static files
