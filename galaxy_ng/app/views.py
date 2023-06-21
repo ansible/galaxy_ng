fb58fd81c85fa9c30ee43b9ae3c490596b5e5b4c
EamonTracey <etracey@redhat.com>
2023-06-21 15:01:45 -0400

    redirect to pulp openapi spec endpoints Issue: AAH-1548

--- galaxy_ng/app/views.py
+++ galaxy_ng/app/views.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from galaxy_ng.app.api import base as api_base
 
 
+PULP_PREFIX = settings.API_ROOT.strip('/')
+
+
 def health_view(request):
     return HttpResponse('OK')
 
@@ -24,3 +27,35 @@ class PulpAPIRedirectView(api_base.APIView):
         # Returning 308 instead of 302 since 308 requires that clients maintain the
         # same method as the original request.
         return HttpResponsePermanentRedirect308(url)
+
+
+class PulpOpenAPIJsonRedirectView(api_base.APIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        url = f"/{PULP_PREFIX}/api/v3/docs/api.json/"
+        return HttpResponsePermanentRedirect(url)
+
+
+class PulpOpenAPIYamlRedirectView(api_base.APIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        url = f"/{PULP_PREFIX}/api/v3/docs/api.yaml/"
+        return HttpResponsePermanentRedirect(url)
+
+
+class PulpRedocRedirectView(api_base.APIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        url = f"/{PULP_PREFIX}/api/v3/docs/"
+        return HttpResponsePermanentRedirect(url)
+
+
+class PulpSwaggerRedirectView(api_base.APIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        url = f"/{PULP_PREFIX}/api/v3/swagger/"
+        return HttpResponsePermanentRedirect(url)
