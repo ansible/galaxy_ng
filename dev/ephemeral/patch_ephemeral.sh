5c7249297b5dc6eb87acefb04005090159f11e45
Daniel Rodowicz <drodowic@redhat.com>
2023-10-12 10:15:59 -0400

    set proper CONTENT_ORIGIN for ephemeral pr checks (#1932)
    Issue: AAH-2775

--- dev/ephemeral/patch_ephemeral.sh
+++ dev/ephemeral/patch_ephemeral.sh
@@ -5,9 +5,14 @@ FE_ROUTE=$(oc get routes | awk '{print $1}' | grep front-end-aggregator)
 if [ -n "${FE_ROUTE}" ]; then
   CONTENT_ORIGIN=$(oc get route front-end-aggregator -o jsonpath='https://{.spec.host}{"\n"}')
 else
-  CONTENT_ORIGIN=$(bonfire namespace describe ${NAMESPACE} | grep "Frontend route" | awk '{print $3}')
+  CONTENT_ORIGIN=$(bonfire namespace describe ${NAMESPACE} | grep "Gateway route" | awk '{print $3}')
 fi
-echo "CONTENT_ORIGIN = ${CONTENT_ORIGIN}"
+if [ -z "${CONTENT_ORIGIN}" ]; then
+  echo "ERROR: unable to determine CONTENT_ORIGIN"
+  exit 1
+else
+  echo "CONTENT_ORIGIN = ${CONTENT_ORIGIN}"
+fi
 oc patch clowdapp automation-hub --type=json \
     -p '[{"op": "replace",
             "path": "/spec/deployments/1/podSpec/env/1/value",
