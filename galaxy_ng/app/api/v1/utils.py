728822bca69abd8386cc89ec6ccfc6fb170ad981
James Tanner <tanner.jc@gmail.com>
2023-10-26 15:43:20 -0400

    Try a list of keys to find the tag value.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/v1/utils.py
+++ galaxy_ng/app/api/v1/utils.py
@@ -15,5 +15,17 @@ def sort_versions(versions):
     """
     Use ansible-core's LooseVersion util to sort the version dicts by the tag key.
     """
+
+    def get_version_tag(version):
+        """
+        Necessary until we normalize all versions.
+        """
+        if version.get('tag'):
+            return version['tag']
+        elif version.get('name'):
+            return version['name']
+        return version.get('version')
+
     sorted_versions = sorted(versions, key=lambda x: LooseVersion(x['tag'].lower()))
+
     return sorted_versions
