5e58f0dae2264c3a05747f2f4bff2235886632dd
James Tanner <tanner.jc@gmail.com>
2023-11-04 23:31:10 -0400

    Add tests.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/community/test_v1_namespaces.py
+++ galaxy_ng/tests/integration/community/test_v1_namespaces.py
@@ -20,6 +20,7 @@ def test_social_auth_creates_v3_namespace_as_v1_provider(ansible_config):
     github_user = 'jctannerTEST'
     github_repo = 'role1'
     cleanup_social_user(github_user, ansible_config)
+    cleanup_social_user(github_user.lower(), ansible_config)
 
     cfg = ansible_config(github_user)
     with SocialGithubClient(config=cfg) as client:
@@ -49,3 +50,30 @@ def test_social_auth_creates_v3_namespace_as_v1_provider(ansible_config):
         role = res['results'][0]
         provider_namespace = role['summary_fields']['provider_namespace']
         assert provider_namespace['name'] == github_user.lower()
+
+        # set the provider namespace's avatar url
+        # PUT https://galaxy-dev.ansible.com/api/_ui/v1/my-namespaces/jctannertest/
+        provider_name = provider_namespace['name']
+        avatar_url = (
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/'
+            + 'NASA_logo.svg/918px-NASA_logo.svg.png'
+        )
+        rr = client.put(
+            f'_ui/v1/my-namespaces/{provider_name}/',
+            data={
+                'name': provider_name,
+                'avatar_url': avatar_url
+            }
+        )
+        assert rr.status_code == 200
+
+        # check the role's new avatar url ...
+        resp = client.get(f'v1/roles/?owner__username={github_user}&name={github_repo}')
+        res = resp.json()
+        role = res['results'][0]
+        assert role['summary_fields']['namespace']['avatar_url'] == avatar_url
+
+        # check the legacynamespace's avatar url ...
+        resp = client.get(f'v1/namespaces/?name={github_user}')
+        res = resp.json()
+        assert res['results'][0]['avatar_url'] == avatar_url
