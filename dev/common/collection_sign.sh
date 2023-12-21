533306cc61f8f1ff1bcae59edc82ab912a2783ee
Christian <ctorrens@redhat.com>
2023-12-21 15:58:34 +0100

    add debug all in gpg No-Issue

--- dev/common/collection_sign.sh
+++ dev/common/collection_sign.sh
@@ -7,7 +7,7 @@ ADMIN_ID="galaxy3@ansible.com"
 PASSWORD="Galaxy2022"
 
 # Create a detached signature
-gpg --lock-never --quiet --batch --pinentry-mode loopback --yes --passphrase \
+gpg --debug-all --lock-never --quiet --batch --pinentry-mode loopback --yes --passphrase \
    $PASSWORD --homedir ~/.gnupg/ --detach-sign --default-key $ADMIN_ID \
    --no-default-keyring --keyring ${KEYRING:-/etc/pulp/certs/galaxy.kbx} \
    --armor --output $SIGNATURE_PATH $FILE_PATH
