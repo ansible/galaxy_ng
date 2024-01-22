e92d750c693815a810f25e755dcdaef65239b1c4
Christian <ctorrens@redhat.com>
2024-01-22 16:01:00 +0100

    gw wip - use galaxykit No-Issue

--- galaxy_ng/tests/integration/api/test_container_push_update.py
+++ galaxy_ng/tests/integration/api/test_container_push_update.py
@@ -3,13 +3,12 @@
 See: https://issues.redhat.com/browse/AAH-2327
 """
 import subprocess
-import time
 from urllib.parse import urlparse
 
 import pytest
 
-from galaxy_ng.tests.integration.utils import get_client
 from galaxy_ng.tests.integration.utils.iqe_utils import pull_and_tag_test_image
+from galaxykit.utils import wait_for_task
 
 
 @pytest.mark.parametrize(
@@ -22,7 +21,7 @@ from galaxy_ng.tests.integration.utils.iqe_utils import pull_and_tag_test_image
 @pytest.mark.deployment_standalone
 @pytest.mark.min_hub_version("4.7.1")
 @pytest.mark.min_hub_version("4.6.6")
-def test_can_update_container_push(ansible_config, require_auth):
+def test_can_update_container_push(ansible_config, require_auth, galaxy_client):
     config = ansible_config("admin")
     container_engine = config["container_engine"]
     url = config['url']
@@ -45,25 +44,21 @@ def test_can_update_container_push(ansible_config, require_auth):
     subprocess.check_call(cmd)
 
     # Get an API client running with admin user credentials
-    client = get_client(
-        config=ansible_config("admin"),
-        request_token=True,
-        require_auth=require_auth
-    )
-    api_prefix = client.config.get("api_prefix").rstrip("/")
+    gc = galaxy_client("admin")
+    if not require_auth:
+        del gc.headers["Authorization"]
 
     # Get the pulp_href for the pushed repo
-    image = client(
-        f"{api_prefix}/pulp/api/v3/repositories/container/container-push/?name=alpine"
-    )
+    image = gc.get("pulp/api/v3/repositories/container/container-push/?name=alpine")
     container_href = image["results"][0]["pulp_href"]
 
     for value in (42, 1):
         # Make a Patch request changing the retain_repo_versions attribute to value
-        client(container_href, method="PATCH", args={"retain_repo_versions": value})
+        response = gc.patch(container_href, body={"retain_repo_versions": value})
 
-        # sleep 2 seconds waiting task to finish
-        time.sleep(2)
+        resp = wait_for_task(gc, response)
+        assert resp["state"] == "completed"
+
         # assert the change was persisted
-        repo = client(container_href)
+        repo = gc.get(container_href)
         assert repo["retain_repo_versions"] == value
