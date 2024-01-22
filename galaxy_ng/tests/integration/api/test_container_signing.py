1a3b9d47430d4fcdefa6bf5a81df25a3d4c2b844
Christian <ctorrens@redhat.com>
2024-01-22 16:40:37 +0100

    gw wip - use galaxykit No-Issue

--- galaxy_ng/tests/integration/api/test_container_signing.py
+++ galaxy_ng/tests/integration/api/test_container_signing.py
@@ -8,17 +8,15 @@ from urllib.parse import urlparse
 
 import pytest
 
-from galaxy_ng.tests.integration.utils import get_client
 from galaxy_ng.tests.integration.utils.iqe_utils import pull_and_tag_test_image
 from galaxykit.container_images import get_container
+from galaxykit.utils import wait_for_task
 
 
 @pytest.fixture(scope="function")
-def flags(ansible_config):
-    api_client = get_client(config=ansible_config("admin"), request_token=True,
-                            require_auth=True)
-    api_prefix = api_client.config.get("api_prefix").rstrip("/")
-    return api_client(f"{api_prefix}/_ui/v1/feature-flags/")
+def flags(galaxy_client):
+    gc = galaxy_client("admin")
+    return gc.get("_ui/v1/feature-flags/")
 
 
 @pytest.mark.parametrize(
@@ -58,40 +56,31 @@ def test_push_and_sign_a_container(ansible_config, flags, require_auth, galaxy_c
     subprocess.check_call(cmd)
 
     # Get an API client running with admin user credentials
-    client = get_client(
-        config=ansible_config("admin"),
-        request_token=True,
-        require_auth=require_auth
-    )
-    api_prefix = client.config.get("api_prefix").rstrip("/")
+    gc = galaxy_client("admin")
+    if not require_auth:
+        del gc.headers["Authorization"]
 
     # Get the pulp_href for the pushed image
-    image = client(
-        f"{api_prefix}/pulp/api/v3/repositories/container/container-push/?name=alpine"
-    )
+    image = gc.get("pulp/api/v3/repositories/container/container-push/?name=alpine")
     container_href = image["results"][0]["pulp_href"]
 
     # Get the pulp_href for signing service
-    signing_service = client(
-        f"{api_prefix}/pulp/api/v3/signing-services/?name=container-default"
-    )
+
+    signing_service = gc.get("pulp/api/v3/signing-services/?name=container-default")
     ss_href = signing_service["results"][0]["pulp_href"]
 
     # Sign the image
-    client(f"{container_href}/sign/", method="POST",
-           args={"manifest_signing_service": ss_href})
+    r = gc.post(f"{container_href}sign/", body={"manifest_signing_service": ss_href})
+    resp = wait_for_task(gc, r)
+    assert resp["state"] == "completed"
 
-    # sleep 2 second2
-    time.sleep(2)
-
-    repo = client(container_href)
+    repo = gc.get(container_href)
     latest_version_href = repo["latest_version_href"]
 
     # Check the image is signed on the latest version
-    latest_version = client(latest_version_href)
+    latest_version = gc.get(latest_version_href)
     assert latest_version["content_summary"]["added"]["container.signature"]["count"] > 0
 
-    gc = galaxy_client("admin")
     ee = get_container(gc, "alpine")
     # Check the sign state is set on the UI API
     assert ee["pulp"]["repository"]["sign_state"] == "signed"
