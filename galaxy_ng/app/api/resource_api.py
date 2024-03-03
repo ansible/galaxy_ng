a3b091aac674ddd9777f3a1149babde0c04b4376
David Newswanger <dnewswan@redhat.com>
2024-03-03 14:04:48 -0600

    Add support for validate-local-user api.
    No-Issue

--- galaxy_ng/app/api/resource_api.py
+++ galaxy_ng/app/api/resource_api.py
@@ -5,11 +5,42 @@ from ansible_base.resource_registry.registry import (
 )
 from ansible_base.resource_registry.shared_types import OrganizationType, TeamType, UserType
 
+
+from ansible_base.resource_registry.models import Resource, service_id
+
+from ansible_base.resource_registry.serializers import ValidateLocalUserSerializer
+
+from ansible_base.resource_registry.shared_types import UserType, TeamType
 from galaxy_ng.app import models
 
 
 class APIConfig(ServiceAPIConfig):
     service_type = "galaxy"
+
+    @staticmethod
+    def get_local_user_details(user) -> ValidateLocalUserSerializer:
+        user_resource = Resource.get_resource_for_object(user)
+        team_memberships = []
+
+        for group in user.groups.all():
+            resource = Resource.get_resource_for_object(group)
+            team_memberships.append(
+                {"ansible_id": resource.ansible_id, "membership_type": "member"}
+            )
+
+        data = {
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_superuser": user.is_superuser,
+            "service_id": user_resource.service_id,
+            "ansible_id": user_resource.ansible_id,
+            "organizations": [],
+            "teams": team_memberships,
+        }
+
+        return ValidateLocalUserSerializer(data)
 
 
 RESOURCE_LIST = (
@@ -20,7 +51,7 @@ RESOURCE_LIST = (
     ),
     ResourceConfig(
         models.Team,
-        shared_resource=SharedResource(serializer=TeamType, is_provider=True),
+        shared_resource=SharedResource(serializer=TeamType, is_provider=False),
         name_field="name",
     ),
     ResourceConfig(
