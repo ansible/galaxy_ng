4a1f1774f09179e9c4cc8e38e17ddd093d0f4491
James Tanner <tanner.jc@gmail.com>
2024-03-28 11:25:05 -0400

    Lint fix.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/api/test_aiindex.py
+++ galaxy_ng/tests/integration/api/test_aiindex.py
@@ -195,7 +195,7 @@ def test_namespace_add_list_remove_aiindex(ansible_config, namespace, pe_namespa
     # 7. Repeat step 2 with a basic user
     with UIClient(config=ansible_config("basic_user")) as uclient:
         # 8. Assert permission error raises
-        with pytest.raises(requests.exceptions.HTTPError, match=r'.*403.*permission_denied.*') as excinfo:
+        with pytest.raises(requests.exceptions.HTTPError, match=r'.*403.*permission_denied.*'):
             uclient.post(
                 "_ui/v1/ai_deny_index/namespace/",
                 payload={"reference": namespace}
