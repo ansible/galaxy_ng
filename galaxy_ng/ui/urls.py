6cee487499c8817ce3b87866bb3029f2c397374c
James Tanner <tanner.jc@gmail.com>
2024-05-02 14:25:59 -0400

    Try this.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/ui/urls.py
+++ galaxy_ng/ui/urls.py
@@ -4,4 +4,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url="/ui/")),
+    url('hub', RedirectView.as_view(url="/hub/")),
+    #url('hub/', RedirectView.as_view(url="/hub/index.html")),
+    #url(r'^hub', RedirectView.as_view(url="/static/galaxy_ng/index.html")),
 ]
