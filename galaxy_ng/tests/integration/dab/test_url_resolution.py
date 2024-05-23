fff8dd6a5dbcfa279463efe4d3be28a630db9faa
James Tanner <tanner.jc@gmail.com>
2024-05-23 11:27:57 -0400

    Lint fix.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/dab/test_url_resolution.py
+++ galaxy_ng/tests/integration/dab/test_url_resolution.py
@@ -2,13 +2,17 @@ import os
 import pytest
 
 
-@pytest.mark.skipif(not os.getenv("ENABLE_DAB_TESTS"), reason="Skipping test because ENABLE_DAB_TESTS is not set")
+@pytest.mark.skipif(
+    not os.getenv("ENABLE_DAB_TESTS"),
+    reason="Skipping test because ENABLE_DAB_TESTS is not set"
+)
 def test_dab_collection_download_url_hostnames(settings, galaxy_client, published):
     """
     We want the download url to point at the gateway
     """
     gc = galaxy_client("admin")
-    cv_url = f'v3/plugin/ansible/content/published/collections/index/{published.namespace}/{published.name}/versions/{published.version}'
+    cv_url = 'v3/plugin/ansible/content/published/collections/index/'
+    cv_url += f'{published.namespace}/{published.name}/versions/{published.version}'
     cv_info = gc.get(cv_url)
     download_url = cv_info['download_url']
     assert download_url.startswith(gc.galaxy_root)
