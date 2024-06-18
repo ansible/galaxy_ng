839ba6fb1771c6ca9285826d9be56e0f39439240
jctanner <tanner.jc@gmail.com>
2024-06-18 14:28:30 -0400

    Pin dab to a tag by default. Allow environment variable override for the branch. (#2180)
    Pin dab in the requirements files.
    No-Issue
    
    ---------
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- setup.py
+++ setup.py
@@ -110,6 +110,12 @@ def _format_pulp_requirement(plugin, specifier=None, ref=None, gh_namespace="pul
         )
 
 
+django_ansible_base_branch = os.getenv('DJANGO_ANSIBLE_BASE_BRANCH', '2024.6.11')
+django_ansible_base_dependency = (
+    'django-ansible-base[jwt_consumer] @ '
+    f'git+https://github.com/ansible/django-ansible-base@{django_ansible_base_branch}'
+)
+
 requirements = [
     "galaxy-importer>=0.4.21,<0.5.0",
     "pulpcore>=3.49.0,<3.50.0",
@@ -124,7 +130,7 @@ requirements = [
     "insights_analytics_collector>=0.3.0",
     "boto3",
     "distro",
-    "django-ansible-base[jwt_consumer] @ git+https://github.com/ansible/django-ansible-base@devel",  # noqa 501
+    django_ansible_base_dependency,  # noqa 501
     "django-crum==0.7.9",
     # From vendored automated_logging
     "marshmallow<4.0.0,>=3.6.1",
@@ -153,8 +159,9 @@ if unpin_requirements:
     ref: https://github.com/ansible/galaxy_ng/wiki/Development-Setup
          #steps-to-run-dev-environment-with-specific-upstream-branch
     """
+    DEFAULT = "pulpcore:pulp_ansible:pulp_container:galaxy_importer:django-ansible-base"
     DEV_SOURCE_PATH = os.getenv(
-        "DEV_SOURCE_PATH", default="pulpcore:pulp_ansible:pulp_container:galaxy_importer"
+        "DEV_SOURCE_PATH", default=DEFAULT
     ).split(":")
     DEV_SOURCE_PATH += [path.replace("_", "-") for path in DEV_SOURCE_PATH]
     requirements = [
