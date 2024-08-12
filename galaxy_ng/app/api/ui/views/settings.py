b3203f7aefa2e3d884439c6e2aa292a21770bc1a
James Tanner <tanner.jc@gmail.com>
2024-08-12 10:00:04 -0400

    Add more settings to the settings endpoint.
    We regularly come across these settings when debugging install issues.
    
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/ui/views/settings.py
+++ galaxy_ng/app/api/ui/views/settings.py
@@ -26,7 +26,16 @@ class SettingsView(api_base.APIView):
             "GALAXY_LDAP_MIRROR_ONLY_EXISTING_GROUPS",
             "GALAXY_LDAP_DISABLE_REFERRALS",
             "KEYCLOAK_URL",
+            "ANSIBLE_BASE_JWT_VALIDATE_CERT",
+            "ANSIBLE_BASE_JWT_KEY",
             "ALLOW_LOCAL_RESOURCE_MANAGEMENT",
+            "ANSIBLE_BASE_ROLES_REQUIRE_VIEW",
+            "DYNACONF_AFTER_GET_HOOKS",
+            "ANSIBLE_API_HOSTNAME",
+            "ANSIBLE_CONTENT_HOSTNAME",
+            "CONTENT_ORIGIN",
+            "TOKEN_SERVER",
+            "TOKEN_AUTH_DISABLED",
         ]
         settings_dict = settings.as_dict()
         data = {key: settings_dict.get(key, None) for key in keyset}
