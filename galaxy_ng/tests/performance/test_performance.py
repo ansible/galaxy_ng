d16bf117e0e52d46447c9f704304fc34d838a17e
Stevenson Michel <smichel@smichel-thinkpadp1gen4i.rmtusky.csb>
2024-08-14 15:43:39 -0400

    Created files for github actions

--- galaxy_ng/tests/performance/test_performance.py
+++ galaxy_ng/tests/performance/test_performance.py
@@ -12,39 +12,32 @@ from ..integration.utils import (
 )
 
 
-@pytest.mark.deployment_community
-def test_api_performance(ansible_config):
-    
-
+@pytest.fixture(scope="function")
+def api_client(ansible_config):
     config = ansible_config("admin")
     api_client = get_client(
         config=config,
         request_token=False,
         require_auth=False
     )
+    return api_client
+  
+        
+        
+        
+@pytest.mark.deployment_community
+@pytest.mark.parametrize("url,baseline", URLS.items())
+def test_api_performance(ansible_config, api_client, url, baseline):
     
     threshold = 0.25
-
-    resp = api_client('http://localhost:5001/api/_ui/v1/settings/', method='GET') 
-    print("Thhhhhhhe response is")
-    print (resp)
-    
     results = []
-
-    for url, baseline in URLS.items():
-       
+    cfg = ansible_config('basic_user')
+    with UIClient(config=cfg) as uclient:
+        
         start_time = time.time()
-        resp = api_client('http://localhost:5001' + url, method='GET')
+        resp = uclient.get(url)
         elapsed_time = time.time() - start_time
         
         difference = (elapsed_time - baseline) / baseline
-        if difference > threshold:
-            results.append(f"{url} exceeds threshold with {difference}")
-        else:
-            results.append(f"{url} does not exceed threshold and its difference is {difference}")
-            
-    print("httttttttttttthe result is")       
-    print(results)
-        
-    assert True
-        
+        results.append({url: difference})
+        assert difference < threshold
