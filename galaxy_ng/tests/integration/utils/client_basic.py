0c9ed42f07f84f2b087c105aa2e8c2fe7178d964
James Tanner <tanner.jc@gmail.com>
2024-08-14 16:38:49 -0400

    Test.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/tests/integration/utils/client_basic.py
+++ galaxy_ng/tests/integration/utils/client_basic.py
@@ -7,6 +7,8 @@ from galaxykit.utils import GalaxyClientError
 
 
 class BasicAuthClient(GalaxyClient):
+    '''Simplified basic auth through galaxykit.'''
+
     def __init__(self, galaxy_root, username, password):
         self.galaxy_root = galaxy_root
         self.username = username
@@ -17,6 +19,8 @@ class BasicAuthClient(GalaxyClient):
         return self._http(*args, **kwargs)
 
     def _http(self, method, path, *args, **kwargs):
+
+        # relative vs absolute url paths ...
         if not path.startswith('/'):
             url = urljoin(self.galaxy_root.rstrip("/") + "/", path)
         else:
@@ -27,14 +31,16 @@ class BasicAuthClient(GalaxyClient):
         kwargs['auth'] = self.auth
         kwargs['verify'] = False
 
+        # munge body back to json if possible ...
         if kwargs.get('body'):
             try:
                 kwargs['json'] = json.loads(kwargs['body'])
                 kwargs.pop('body')
-            except:
+            except Exception:
                 kwargs['data'] = kwargs['body']
                 kwargs.pop('body')
 
+        # not all endpoints return json
         want_json = True
         if 'want_json' in kwargs:
             want_json = kwargs['want_json']
@@ -47,5 +53,5 @@ class BasicAuthClient(GalaxyClient):
             if want_json:
                 return rr.json()
             return rr.text
-        except:
+        except Exception:
             raise GalaxyClientError(rr.text, rr.status_code)
