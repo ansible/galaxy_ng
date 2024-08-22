1ded60e09731308de6eb4bf0d44470522320a673
Jiří Jeřábek (Jiri Jerabek) <Jerabekjirka@email.cz>
2024-08-22 13:05:57 +0200

    Skip 2.5 tests in 2.4 pipelines (#2235)
    skip 2.5 tests in 2.4 pipelines
    
    No-Issue

--- galaxy_ng/tests/integration/dab/test_dab_rbac_pagination.py
+++ galaxy_ng/tests/integration/dab/test_dab_rbac_pagination.py
@@ -5,6 +5,7 @@ pytestmark = pytest.mark.qa  # noqa: F821
 
 
 @pytest.mark.deployment_standalone
+@pytest.mark.min_hub_version("4.10dev")
 def test_dab_rbac_pagination(galaxy_client):
     gc = galaxy_client("admin", ignore_cache=True)
     roledefs = gc.get('_ui/v2/role_definitions/?page_size=1')
