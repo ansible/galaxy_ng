ed1f9f61e7075a58e5109514718f436829691e3c
jctanner <tanner.jc@gmail.com>
2024-08-29 23:58:11 +0200

    More packages.
    No-Issue
    
    Signed-off-by: jctanner <tanner.jc@gmail.com>

--- dev/playbooks/docker/BUILD.sh
+++ dev/playbooks/docker/BUILD.sh
@@ -15,7 +15,7 @@ chmod +x pulp-oci-images/images/assets/switch_python
 cd pulp-oci-images
 git apply ../py311.patch
 git apply ../rootless.patch
-git apply ../xz.patch
+git apply ../packages.patch
 
 docker build --file images/Containerfile.core.base --tag pulp/base:latest .
 docker build --file images/pulp_ci_centos/Containerfile --tag pulp/pulp-ci-centos9:latest .
