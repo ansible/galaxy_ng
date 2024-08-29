3f9059e1211863ddbee1c427560de606803973a2
jctanner <tanner.jc@gmail.com>
2024-08-29 22:33:57 +0200

    Test.
    No-Issue
    
    Signed-off-by: jctanner <tanner.jc@gmail.com>

--- dev/playbooks/docker/BUILD.sh
+++ dev/playbooks/docker/BUILD.sh
@@ -9,11 +9,15 @@ cd pulp-oci-images
 git reset --hard
 cd ..
 
+# exit 1
+
 cp -f switch_python pulp-oci-images/images/assets/.
 chmod +x pulp-oci-images/images/assets/switch_python
 
 cd pulp-oci-images
 git apply ../py311.patch
+git apply ../rootless.patch
+git apply ../xz.patch
 
 docker build --file images/Containerfile.core.base --tag pulp/base:latest .
 docker build --file images/pulp_ci_centos/Containerfile --tag pulp/pulp-ci-centos9:latest .
