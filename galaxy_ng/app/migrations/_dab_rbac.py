2a83b25aab386306f56ba2d5f488cdda484e336b
James Tanner <tanner.jc@gmail.com>
2024-09-25 16:34:18 -0400

    Add some code comments.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/migrations/_dab_rbac.py
+++ galaxy_ng/app/migrations/_dab_rbac.py
@@ -38,6 +38,12 @@ def create_permissions_as_operation(apps, schema_editor):
 
 
 def split_pulp_roles(apps, schema_editor):
+    '''
+    For every user&group role that is tied to a specific content object,
+    split the role out into a new single content type role with permissions
+    that are only relevant to that content object. Afterwards, swap the
+    [User|Group]Role's .role with the new role.
+    '''
     Role = apps.get_model('core', 'Role')
     UserRole = apps.get_model('core', 'UserRole')
     GroupRole = apps.get_model('core', 'GroupRole')
@@ -48,12 +54,16 @@ def split_pulp_roles(apps, schema_editor):
             for pulp_assignment in assignment_cls.objects.filter(role=corerole, content_type__isnull=False):
                 if pulp_assignment.content_type_id not in split_roles:
  
+                    # Get all permissions relevant to this content model.
+                    # If any model (like synclist) hasn't been registered in the permission
+                    # system, it should not be split/recreated ...
                     cls = apps.get_model(pulp_assignment.content_type.app_label, pulp_assignment.content_type.model)
                     try:
                         ct_codenames = combine_values(permissions_allowed_for_role(cls))
                     except ValidationError:
                         continue
 
+                    # Make a new role for this special content model
                     new_data = {
                         'description': corerole.description,
                         'name': f'{corerole.name}_{pulp_assignment.content_type.model}'
@@ -61,7 +71,10 @@ def split_pulp_roles(apps, schema_editor):
                     new_role = Role(**new_data)
                     new_role.save()
 
+                    # Add the necesarry permissions to the new role ...
                     for perm in pulp_assignment.role.permissions.all():
+                        # The pulp role may have had permissions related to some other
+                        # content model we're not interested in, so we will skip adding those.
                         if ct_codenames and perm.codename not in ct_codenames:
                             continue
                         new_role.permissions.add(perm)
