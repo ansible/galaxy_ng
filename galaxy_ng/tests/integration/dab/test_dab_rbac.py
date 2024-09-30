0559724972fd44d8d2c771f95a4c4a57aa88bcf5
jerabekjiri <jerabekjirka@email.cz>
2024-09-30 12:33:08 +0200

    fix caps and skip if resource management false
    No-Issue

--- galaxy_ng/tests/integration/dab/test_dab_rbac.py
+++ galaxy_ng/tests/integration/dab/test_dab_rbac.py
@@ -56,7 +56,7 @@ def test_dab_rbac_repository_owner_by_user_or_team(
     random_username
 ):
 
-    if settings.get('allow_local_resource_management') is False:
+    if settings.get('ALLOW_LOCAL_RESOURCE_MANAGEMENT') is False:
         pytest.skip("this test relies on local resource creation")
 
     gc = galaxy_client("admin", ignore_cache=True)
@@ -169,7 +169,7 @@ def test_dab_rbac_namespace_owner_by_user_or_team(
       to view a private repository that includes their collection.
     """
 
-    if settings.get('allow_local_resource_management') is False:
+    if settings.get('ALLOW_LOCAL_RESOURCE_MANAGEMENT') is False:
         pytest.skip("this test relies on local resource creation")
 
     gc = galaxy_client("admin", ignore_cache=True)
