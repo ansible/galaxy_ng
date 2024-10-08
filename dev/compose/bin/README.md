f863ba435b258dc0b9f0e2ba327ab3bb040187d8
Bruno Rocha <rochacbruno@gmail.com>
2024-10-08 23:23:33 +0100

    New compose folder

---
+++ dev/compose/bin/README.md
@@ -0,0 +1,31 @@
+# Dev Binary Tools
+
+## Reloader
+
+A Script that splits `DEV_SOURCE_PATH` variable and list all files on its directories,
+then omit some unwanted files watching for `.py|yaml` only for changes.
+
+Then it calls `entr` to reload the passed command.
+
+## entr
+
+A tool to watch files for changes and reload
+This file is compiled specifically for the container in use.
+
+LINK: https://eradman.com/entrproject/
+
+In case recompilation is needed, run inside the container.
+
+```bash
+# Install entr so we can have reload on worker processes
+# the checkout is to fix a compilation error caused by this commit
+# https://github.com/eradman/entr/commit/f9ac92d17e42236fe6b5e8492e087620173c7b24
+RUN git clone https://github.com/eradman/entr && \
+    cd entr && \
+    git checkout 0d2d92d6052624a1e03a2a654e98e1c49f9955d9 && \
+    cp Makefile.linux Makefile && \
+    make
+```
+
+Then copy the generated `entr` binary.
+
---
+++ dev/compose/signing/setup_signing_services.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Collection
+HAS_COLLECTION_SIGNING=$(pulpcore-manager shell -c 'from pulpcore.app.models import SigningService;print(SigningService.objects.filter(name="ansible-default").count())' 2>/dev/null || true)
+if [[ "$HAS_COLLECTION_SIGNING" -eq "0" ]]; then
+    pulpcore-manager add-signing-service ansible-default /var/lib/pulp/scripts/collection_sign.sh F37575C52D4F16F3
+else
+    echo "Collection Signing Service Already exists"
+fi
+
+# Container
+HAS_CONTAINER_SIGNING=$(pulpcore-manager shell -c 'from pulpcore.app.models import SigningService;print(SigningService.objects.filter(name="container-default").count())' 2>/dev/null || true)
+if [[ "$HAS_CONTAINER_SIGNING" -eq "0" ]]; then
+    pulpcore-manager add-signing-service container-default /var/lib/pulp/scripts/container_sign.sh F37575C52D4F16F3 --class container:ManifestSigningService
+else
+    echo "Collection Signing Service Already exists"
+fi
