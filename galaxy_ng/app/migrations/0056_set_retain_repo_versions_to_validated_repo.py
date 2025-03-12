e9485de8d67ac08b569750098f3473ee8b0417d5
jerabekjiri <jerabekjirka@email.cz>
2025-03-12 18:11:35 +0100

    fix typo

--- galaxy_ng/app/migrations/0056_set_retain_repo_versions_to_validated_repo.py
+++ galaxy_ng/app/migrations/0056_set_retain_repo_versions_to_validated_repo.py
@@ -6,7 +6,7 @@ def set_retain_repo_versions_to_validated_repo(apps, schema_editor):
 
     db_alias = schema_editor.connection.alias
 
-    validated_repo = AnsibleRepository.objects.using(db_alias).filter(name="validateds").first()
+    validated_repo = AnsibleRepository.objects.using(db_alias).filter(name="validated").first()
     if validated_repo and validated_repo.retain_repo_versions is None:
         validated_repo.retain_repo_versions = 1
         validated_repo.save()
