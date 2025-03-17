2a9711d3aa17769a8a2718297203af5aeec9ca7b
jerabekjiri <jerabekjirka@email.cz>
2025-03-17 23:11:20 +0100

    bump version to 4.10.3

--- galaxy_ng/__init__.py
+++ galaxy_ng/__init__.py
@@ -3,6 +3,6 @@ from ._vendor import automated_logging
 
 sys.modules.setdefault("automated_logging", automated_logging)
 
-__version__ = "4.10.2"
+__version__ = "4.10.3"
 
 default_app_config = "galaxy_ng.app.PulpGalaxyPluginAppConfig"
