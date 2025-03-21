8d55fddc6eed0579b800572c153c439c7e3ab7e6
Bruno Rocha <rochacbruno@users.noreply.github.com>
2025-03-21 12:31:18 +0000

    Revert "backport stable4.10: Bump galaxy-importer to 0.4.29 (#2482)"
    This reverts commit 91af65834478ed6919de1a525f66ad3a1161c7c6.

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ django_ansible_base_dependency = (
 )
 
 requirements = [
-    "galaxy-importer>=0.4.29,<0.5.0",
+    "galaxy-importer>=0.4.21,<0.5.0",
     "pulpcore>=3.49.0,<3.50.0",
     "pulp_ansible>=0.21.0,<0.22.0",
     "pulp-container>=2.19.2,<2.20.0",
