76a6b373b109b4d4731881cae8be1d3e5f80a91b
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2025-03-21 21:08:16 +0000

    [stable-4.9] Use galaxy user instead of root (#2449) (#2492)
    Signed-off-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>
    
    (cherry picked from commit aec6a84c59c19649bf92c5bef1844d74a512e14f)
    Signed-off-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- dev/compose/signing/setup_repo_keyring.sh
+++ dev/compose/signing/setup_repo_keyring.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o pipefail
 
 for repo in staging published; do
-    pulpcore-manager set-repo-keyring --repository $repo --keyring /root/.gnupg/pubring.kbx -y;
+    pulpcore-manager set-repo-keyring --repository $repo --keyring /etc/pulp/gnupg/pubring.kbx -y;
 done
 
