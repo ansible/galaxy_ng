222d37a9a65ded5c683a9251242d61352c80f077
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2025-03-28 01:23:53 +0000

    Refactoring dev image (#2509)
    for making it compliant with DOCKER_BUILDKIT=0
    
    Signed-off-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- dev/compose/signing/setup_gpg_keys.sh
+++ dev/compose/signing/setup_gpg_keys.sh
@@ -5,8 +5,10 @@ set -o pipefail
 # ensure agent is running
 gpgconf --kill gpg-agent
 
+DB_ENCRYPTION_KEY=${DB_ENCRYPTION_KEY:-"/etc/pulp/certs/signing-secret.key"}
+
 # Import the key
-gpg --batch --no-default-keyring --import /etc/pulp/certs/signing-secret.key;
+gpg --batch --no-default-keyring --import ${DB_ENCRYPTION_KEY};
 
 # Set the key trust level
 (echo 5; echo y; echo save) | gpg --command-fd 0 --no-tty --no-greeting -q --edit-key 'FB8B3F2D24BCAF7EFDF793A9F37575C52D4F16F3' trust;
