51ef2f4516a9373a2ba40dd5b1b989433f79a59b
jerabekjiri <jerabekjirka@email.cz>
2025-05-27 15:52:58 +0200

    release 4.9.4

--- galaxy_ng/__init__.py
+++ galaxy_ng/__init__.py
@@ -3,6 +3,6 @@ from ._vendor import automated_logging
 
 sys.modules.setdefault("automated_logging", automated_logging)
 
-__version__ = "4.9.3"
+__version__ = "4.9.4"
 
 default_app_config = "galaxy_ng.app.PulpGalaxyPluginAppConfig"
--- galaxy_ng/app/__init__.py
+++ galaxy_ng/app/__init__.py
@@ -6,7 +6,7 @@ class PulpGalaxyPluginAppConfig(PulpPluginAppConfig):
 
     name = "galaxy_ng.app"
     label = "galaxy"
-    version = "4.9.3"
+    version = "4.9.4"
     python_package_name = "galaxy-ng"
 
     def ready(self):
