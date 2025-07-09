0438a76d0acc321930700328778b22e89bf15b7d
AlanCoding <arominge@redhat.com>
2025-07-08 22:42:57 -0400

    Use DAB RBAC branch for testing

--- setup.py
+++ setup.py
@@ -87,10 +87,10 @@ class BuildPyCommand(_BuildPyCommand):
         return super().run()
 
 
-django_ansible_base_branch = os.getenv('DJANGO_ANSIBLE_BASE_BRANCH', '2025.7.1')
+django_ansible_base_branch = os.getenv('DJANGO_ANSIBLE_BASE_BRANCH', 'remote_rbac')
 django_ansible_base_dependency = (
     'django-ansible-base[jwt-consumer,feature-flags] @ '
-    f'git+https://github.com/ansible/django-ansible-base@{django_ansible_base_branch}'
+    f'git+https://github.com/alancoding/django-ansible-base@{django_ansible_base_branch}'
 )
 
 requirements = [
