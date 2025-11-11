82666b826487699c201c239d3be42c54239687ea
Or Hochmann <orhochma@redhat.com>
2025-11-11 13:06:39 +0200

    bump to 4.9.6

--- galaxy_ng/app/__init__.py
+++ galaxy_ng/app/__init__.py
@@ -6,7 +6,7 @@ class PulpGalaxyPluginAppConfig(PulpPluginAppConfig):
 
     name = "galaxy_ng.app"
     label = "galaxy"
-    version = "4.9.5"
+    version = "4.9.6"
     python_package_name = "galaxy-ng"
 
     def ready(self):
