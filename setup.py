0bf63e62eab07b93b6db005899f4bf95dacd4b87
Or Hochmann <72911740+RedOrNot@users.noreply.github.com>
2026-04-15 05:16:50 +0300

    chore: update pyasn1  (#429)

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ requirements = [
     "pyopenssl>=25.3.0",  # bumped to allow cryptography>=46.0.5
     "black>=26.3.1",  # minimum version enforced for AAP-68431, AAP-68430, AAP-68421
     "ansible-lint>=26.1.1",  # minimum version enforced for AAP-68431, AAP-68430, AAP-68421
+    "pyasn1>=0.6.3",  # minimum version enforced to address AAP-69046, AAP-69045, AAP-69038
 ]
 
 
